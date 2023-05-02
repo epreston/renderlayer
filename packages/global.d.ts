07ca04985a31006a2486148249bbd3ea67362710
Ed Preston <epreston@prestonsoft.com>
2023-05-03 04:12:24 +1000

    Update global.d.ts
    type checking for expect extensions

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// for tests
+declare namespace jest {
+  interface Matchers<R, T> {
+    toHaveBeenWarned(): R
+    toHaveBeenWarnedLast(): R
+    toHaveBeenWarnedTimes(n: number): R
+  }
+}
