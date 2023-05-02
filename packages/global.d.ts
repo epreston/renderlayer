8b498315a3f31695b557fb34931a8bcd29953631
Ed Preston <epreston@prestonsoft.com>
2023-05-03 07:12:45 +1000

    Update global.d.ts
    draft declarations / notes

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+// Global compile-time constants
+declare var __DEV__: boolean
+declare var __TEST__: boolean
+declare var __BROWSER__: boolean
+declare var __ESM_BUNDLER__: boolean
+declare var __NODE_JS__: boolean
+declare var __COMMIT__: string
+declare var __VERSION__: string
+
 // for tests
 declare namespace jest {
   interface Matchers<R, T> {
@@ -7,4 +16,42 @@ declare namespace jest {
     toHaveBeenWarnedLast(): R
     toHaveBeenWarnedTimes(n: number): R
   }
+}
+
+declare interface String {
+  /**
+   * @deprecated Please use String.prototype.slice instead of String.prototype.substring in the repository.
+   */
+  substring(start: number, end?: number): string
+}
+
+declare interface ColorManagement {
+
+  /**
+   * @deprecated ColorManagement.legacyMode=false renamed to .enabled=true.
+   */
+  get legacyMode(): boolean
+
+  /**
+   * @deprecated ColorManagement.legacyMode=false renamed to .enabled=true.
+   */
+  set legacyMode(legacyMode: boolean)
+
+  /**
+   * @deprecated ColorManagement.workingColorSpace is readonly.
+   */
+  set workingColorSpace(colorSpace: string)
+}
+
+declare interface Triangle {
+
+  /**
+   * @deprecated Triangle.getUV() has been renamed to Triangle.getInterpolation().
+   */
+  static getUV( point, p1, p2, p3, uv1, uv2, uv3, target )
+
+  /**
+   * @deprecated Triangle.getUV() has been renamed to Triangle.getInterpolation().
+   */
+  getUV( point, uv1, uv2, uv3, target )
 }
