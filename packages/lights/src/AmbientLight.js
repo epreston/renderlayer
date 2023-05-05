d5d9d3a8e3257a631517d099606ace3fb2ec9ef4
Ed Preston <epreston@prestonsoft.com>
2023-05-05 19:43:22 +1000

    Create AmbientLight.js

---
+++ packages/lights/src/AmbientLight.js
@@ -0,0 +1,12 @@
+import { Light } from './Light.js';
+
+class AmbientLight extends Light {
+  constructor(color, intensity) {
+    super(color, intensity);
+
+    this.isAmbientLight = true;
+    this.type = 'AmbientLight';
+  }
+}
+
+export { AmbientLight };
