7823bcc24caeabbafc56392e4ca0fee10577cd7a
Ed Preston <epreston@prestonsoft.com>
2023-05-05 19:44:44 +1000

    Create index.js

---
+++ packages/lights/src/index.js
@@ -0,0 +1,5 @@
+export * from './AmbientLight.js';
+export * from './DirectionalLight.js';
+export * from './DirectionalLightShadow.js';
+export * from './Light.js';
+export * from './LightShadow.js';
