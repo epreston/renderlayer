045ad081139d779c9561d59c7d6dd72bfe99efcd
Ed Preston <epreston@prestonsoft.com>
2023-05-05 20:37:37 +1000

    Create Fog.js

---
+++ packages/scenes/src/Fog.js
@@ -0,0 +1,28 @@
+import { Color } from '@renderlayer/math';
+
+class Fog {
+  constructor(color, near = 1, far = 1000) {
+    this.isFog = true;
+    this.name = '';
+
+    this.color = new Color(color);
+
+    this.near = near;
+    this.far = far;
+  }
+
+  clone() {
+    return new Fog(this.color, this.near, this.far);
+  }
+
+  toJSON(/* meta */) {
+    return {
+      type: 'Fog',
+      color: this.color.getHex(),
+      near: this.near,
+      far: this.far,
+    };
+  }
+}
+
+export { Fog };
