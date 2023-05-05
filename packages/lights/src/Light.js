7d4edce9539cc020b81225c842ec6cca58192231
Ed Preston <epreston@prestonsoft.com>
2023-05-05 19:42:36 +1000

    Create Light.js

---
+++ packages/lights/src/Light.js
@@ -0,0 +1,47 @@
+import { Color } from '@renderlayer/math';
+import { Object3D } from '@renderlayer/core';
+
+class Light extends Object3D {
+  constructor(color, intensity = 1) {
+    super();
+
+    this.isLight = true;
+    this.type = 'Light';
+
+    this.color = new Color(color);
+    this.intensity = intensity;
+  }
+
+  dispose() {
+    // this.dispatchEvent( { type: 'dispose' } );
+  }
+
+  copy(source, recursive) {
+    super.copy(source, recursive);
+
+    this.color.copy(source.color);
+    this.intensity = source.intensity;
+
+    return this;
+  }
+
+  toJSON(meta) {
+    const data = super.toJSON(meta);
+
+    data.object.color = this.color.getHex();
+    data.object.intensity = this.intensity;
+
+    if (this.groundColor !== undefined) data.object.groundColor = this.groundColor.getHex();
+
+    if (this.distance !== undefined) data.object.distance = this.distance;
+    if (this.angle !== undefined) data.object.angle = this.angle;
+    if (this.decay !== undefined) data.object.decay = this.decay;
+    if (this.penumbra !== undefined) data.object.penumbra = this.penumbra;
+
+    if (this.shadow !== undefined) data.object.shadow = this.shadow.toJSON();
+
+    return data;
+  }
+}
+
+export { Light };
