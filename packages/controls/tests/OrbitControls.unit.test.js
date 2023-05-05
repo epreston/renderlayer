40ee1917c578e4865fe4b926a070208058da27eb
Ed Preston <epreston@prestonsoft.com>
2023-05-06 05:19:16 +1000

    Create OrbitControls.unit.test.js

---
+++ packages/controls/tests/OrbitControls.unit.test.js
@@ -0,0 +1,28 @@
+import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
+
+// import { OrbitControls } from '../src/OrbitControls.js';
+
+describe.todo('OrbitControls', () => {
+  // let instance;
+
+  // beforeAll(() => {
+  //   instance = new OrbitControls(object, domElement);
+  // });
+
+  // beforeEach(() => {
+  //   instance = new OrbitControls(object, domElement);
+  // });
+
+  // it('can be instantiated', () => {
+  //   expect(instance).toBeDefined();
+  //   expect(instance).toBeInstanceOf(Object);
+  // });
+
+  // it('should be an instance of OrbitControls', () => {
+  //   expect(instance).toBeInstanceOf(OrbitControls);
+  // });
+
+  // it('extends from EventDispatcher', () => {
+  //   expect(instance).toBeInstanceOf(EventDispatcher);
+  // });
+});
