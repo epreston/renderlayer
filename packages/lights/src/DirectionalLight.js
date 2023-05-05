7d454c2ef03dee6e3b82872f2c82224d4a2f5043
Ed Preston <epreston@prestonsoft.com>
2023-05-05 19:44:30 +1000

    Create DirectionalLight.js

---
+++ packages/lights/src/DirectionalLight.js
@@ -0,0 +1,34 @@
+import { DirectionalLightShadow } from './DirectionalLightShadow.js';
+import { Object3D } from '@renderlayer/core';
+import { Light } from './Light.js';
+
+class DirectionalLight extends Light {
+  constructor(color, intensity) {
+    super(color, intensity);
+
+    this.isDirectionalLight = true;
+    this.type = 'DirectionalLight';
+
+    this.position.copy(Object3D.DEFAULT_UP);
+    this.updateMatrix();
+
+    this.target = new Object3D();
+
+    this.shadow = new DirectionalLightShadow();
+  }
+
+  dispose() {
+    this.shadow.dispose();
+  }
+
+  copy(source) {
+    super.copy(source);
+
+    this.target = source.target.clone();
+    this.shadow = source.shadow.clone();
+
+    return this;
+  }
+}
+
+export { DirectionalLight };
