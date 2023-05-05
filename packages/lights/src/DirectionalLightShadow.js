307c854515432117f4a1086a02601401621874f4
Ed Preston <epreston@prestonsoft.com>
2023-05-05 19:43:50 +1000

    Create DirectionalLightShadow.js

---
+++ packages/lights/src/DirectionalLightShadow.js
@@ -0,0 +1,12 @@
+import { OrthographicCamera } from '@renderlayer/cameras';
+import { LightShadow } from './LightShadow.js';
+
+class DirectionalLightShadow extends LightShadow {
+  constructor() {
+    super(new OrthographicCamera(-5, 5, 5, -5, 0.5, 500));
+
+    this.isDirectionalLightShadow = true;
+  }
+}
+
+export { DirectionalLightShadow };
