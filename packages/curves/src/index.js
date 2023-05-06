17cb41ac5ce795854f853f02c41e6c814da43e50
Ed Preston <epreston@prestonsoft.com>
2023-05-07 01:22:28 +1000

    Create index.js

---
+++ packages/curves/src/index.js
@@ -0,0 +1,8 @@
+export * from './core/Curve.js';
+export * from './core/CurvePath.js';
+export * from './core/Interpolations.js';
+export * from './CatmullRomCurve3.js';
+export * from './CubicBezierCurve3.js';
+export * from './LineCurve.js';
+export * from './LineCurve3.js';
+
