2fa2e971e114df6a018b6c2f986e468fd36f6455
Ed Preston <epreston@prestonsoft.com>
2023-05-07 01:01:39 +1000

    Create Interpolations.unit.test.js

---
+++ packages/curves/tests/core/Interpolations.unit.test.js
@@ -0,0 +1,21 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { CatmullRom, QuadraticBezier, CubicBezier } from '../../src/core/Interpolations.js';
+
+describe('Curves', () => {
+  describe('Core', () => {
+    describe('Interpolations', () => {
+      test.todo('CatmullRom', () => {
+        // implement
+      });
+
+      test.todo('QuadraticBezier', () => {
+        // implement
+      });
+
+      test.todo('CubicBezier', () => {
+        // implement
+      });
+    });
+  });
+});
