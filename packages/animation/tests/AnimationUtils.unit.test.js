d3905870d975e262dfc7b24d6d565d995d414653
Ed Preston <epreston@prestonsoft.com>
2023-05-06 21:32:00 +1000

    Create AnimationUtils.unit.test.js

---
+++ packages/animation/tests/AnimationUtils.unit.test.js
@@ -0,0 +1,39 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+// import * as AnimationUtils from '../src/AnimationUtils.js';
+
+describe('Animation', () => {
+  describe('AnimationUtils', () => {
+    test.todo('arraySlice', () => {
+      // implement
+    });
+
+    test.todo('convertArray', () => {
+      // implement
+    });
+
+    test.todo('isTypedArray', () => {
+      // implement
+    });
+
+    test.todo('getKeyframeOrder', () => {
+      // implement
+    });
+
+    test.todo('sortedArray', () => {
+      // implement
+    });
+
+    test.todo('flattenJSON', () => {
+      // implement
+    });
+
+    test.todo('subclip', () => {
+      // implement
+    });
+
+    test.todo('makeClipAdditive', () => {
+      // implement
+    });
+  });
+});
