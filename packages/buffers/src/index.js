ea4b3edfdf30dbd16a8c90eb46f68bc2223b03d9
Ed Preston <epreston@prestonsoft.com>
2023-05-07 19:10:30 +1000

    Create index.js

---
+++ packages/buffers/src/index.js
@@ -0,0 +1,10 @@
+export * from './BufferAttribute.js';
+export * from './BufferAttributeUtils.js';
+export * from './BufferGeometry.js';
+export * from './BufferGeometryUtils.js';
+export * from './InstancedBufferAttribute.js';
+export * from './InstancedBufferGeometry.js';
+export * from './InstancedInterleavedBuffer.js';
+export * from './InterleavedBuffer.js';
+export * from './InterleavedBufferAttribute.js';
+
