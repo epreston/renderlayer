aa2cb8ab64cabc47cc02d1900b37f13843d04f3d
Ed Preston <epreston@prestonsoft.com>
2023-05-08 23:20:38 +1000

    Create WebGLRenderTarget.unit.test.js

---
+++ packages/targets/tests/WebGLRenderTarget.unit.test.js
@@ -0,0 +1,85 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { EventDispatcher } from '@renderlayer/core';
+import { WebGLRenderTarget } from '../src/WebGLRenderTarget.js';
+
+describe('Renderers', () => {
+  describe('WebGLRenderTarget', () => {
+    test('Instancing', () => {
+      const object = new WebGLRenderTarget();
+      expect(object).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new WebGLRenderTarget();
+      expect(object).toBeInstanceOf(EventDispatcher);
+    });
+
+    test.todo('width', () => {
+      // implement
+    });
+
+    test.todo('height', () => {
+      // implement
+    });
+
+    test.todo('depth', () => {
+      // implement
+    });
+
+    test.todo('scissor', () => {
+      // implement
+    });
+
+    test.todo('scissorTest', () => {
+      // implement
+    });
+
+    test.todo('viewport', () => {
+      // implement
+    });
+
+    test.todo('texture', () => {
+      // implement
+    });
+
+    test.todo('depthBuffer', () => {
+      // implement
+    });
+
+    test.todo('stencilBuffer', () => {
+      // implement
+    });
+
+    test.todo('depthTexture', () => {
+      // implement
+    });
+
+    test.todo('samples', () => {
+      // implement
+    });
+
+    test.todo('isWebGLRenderTarget', () => {
+      // implement
+    });
+
+    test.todo('setSize', () => {
+      // implement
+    });
+
+    test.todo('clone', () => {
+      // implement
+    });
+
+    test.todo('copy', () => {
+      // implement
+    });
+
+    test('dispose', () => {
+      const object = new WebGLRenderTarget();
+      object.dispose();
+
+      expect(object).toBeDefined();
+    });
+  });
+});
