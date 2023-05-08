c6aa166f99d45bfaeae8e5cf2b695c245135fcf8
Ed Preston <epreston@prestonsoft.com>
2023-05-09 05:18:40 +1000

    Create keyframes.esm-bundler.js
    0.0.1

---
+++ packages/keyframes/dist/keyframes.esm-bundler.js
@@ -0,0 +1,291 @@
+import { InterpolateSmooth, InterpolateLinear, InterpolateDiscrete } from '@renderlayer/shared';
+import { DiscreteInterpolant, LinearInterpolant, CubicInterpolant, QuaternionLinearInterpolant } from '@renderlayer/interpolants';
+
+function arraySlice(array, from, to) {
+  if (isTypedArray(array)) {
+    return new array.constructor(array.subarray(from, to !== void 0 ? to : array.length));
+  }
+  return array.slice(from, to);
+}
+function convertArray(array, type, forceClone) {
+  if (!array || // let 'undefined' and 'null' pass
+  !forceClone && array.constructor === type)
+    return array;
+  if (typeof type.BYTES_PER_ELEMENT === "number") {
+    return new type(array);
+  }
+  return Array.prototype.slice.call(array);
+}
+function isTypedArray(object) {
+  return ArrayBuffer.isView(object) && !(object instanceof DataView);
+}
+
+class KeyframeTrack {
+  constructor(name, times, values, interpolation) {
+    if (name === void 0)
+      throw new Error("KeyframeTrack: track name is undefined");
+    if (times === void 0 || times.length === 0)
+      throw new Error("KeyframeTrack: no keyframes in track named " + name);
+    this.name = name;
+    this.times = convertArray(times, this.TimeBufferType);
+    this.values = convertArray(values, this.ValueBufferType);
+    this.setInterpolation(interpolation || this.DefaultInterpolation);
+  }
+  // Serialization (in static context, because of constructor invocation
+  // and automatic invocation of .toJSON):
+  static toJSON(track) {
+    const trackType = track.constructor;
+    let json;
+    if (trackType.toJSON !== this.toJSON) {
+      json = trackType.toJSON(track);
+    } else {
+      json = {
+        name: track.name,
+        times: convertArray(track.times, Array),
+        values: convertArray(track.values, Array)
+      };
+      const interpolation = track.getInterpolation();
+      if (interpolation !== track.DefaultInterpolation) {
+        json.interpolation = interpolation;
+      }
+    }
+    json.type = track.ValueTypeName;
+    return json;
+  }
+  InterpolantFactoryMethodDiscrete(result) {
+    return new DiscreteInterpolant(this.times, this.values, this.getValueSize(), result);
+  }
+  InterpolantFactoryMethodLinear(result) {
+    return new LinearInterpolant(this.times, this.values, this.getValueSize(), result);
+  }
+  InterpolantFactoryMethodSmooth(result) {
+    return new CubicInterpolant(this.times, this.values, this.getValueSize(), result);
+  }
+  setInterpolation(interpolation) {
+    let factoryMethod;
+    switch (interpolation) {
+      case InterpolateDiscrete:
+        factoryMethod = this.InterpolantFactoryMethodDiscrete;
+        break;
+      case InterpolateLinear:
+        factoryMethod = this.InterpolantFactoryMethodLinear;
+        break;
+      case InterpolateSmooth:
+        factoryMethod = this.InterpolantFactoryMethodSmooth;
+        break;
+    }
+    if (factoryMethod === void 0) {
+      const message = "unsupported interpolation for " + this.ValueTypeName + " keyframe track named " + this.name;
+      if (this.createInterpolant === void 0) {
+        if (interpolation !== this.DefaultInterpolation) {
+          this.setInterpolation(this.DefaultInterpolation);
+        } else {
+          throw new Error(message);
+        }
+      }
+      console.warn("KeyframeTrack:", message);
+      return this;
+    }
+    this.createInterpolant = factoryMethod;
+    return this;
+  }
+  getInterpolation() {
+    switch (this.createInterpolant) {
+      case this.InterpolantFactoryMethodDiscrete:
+        return InterpolateDiscrete;
+      case this.InterpolantFactoryMethodLinear:
+        return InterpolateLinear;
+      case this.InterpolantFactoryMethodSmooth:
+        return InterpolateSmooth;
+    }
+  }
+  getValueSize() {
+    return this.values.length / this.times.length;
+  }
+  // move all keyframes either forwards or backwards in time
+  shift(timeOffset) {
+    if (timeOffset !== 0) {
+      const times = this.times;
+      for (let i = 0, n = times.length; i !== n; ++i) {
+        times[i] += timeOffset;
+      }
+    }
+    return this;
+  }
+  // scale all keyframe times by a factor (useful for frame <-> seconds conversions)
+  scale(timeScale) {
+    if (timeScale !== 1) {
+      const times = this.times;
+      for (let i = 0, n = times.length; i !== n; ++i) {
+        times[i] *= timeScale;
+      }
+    }
+    return this;
+  }
+  // removes keyframes before and after animation without changing any values within
+  // the range [startTime, endTime].
+  // IMPORTANT: We do not shift around keys to the start of the track time, because
+  // for interpolated keys this will change their values
+  trim(startTime, endTime) {
+    const times = this.times, nKeys = times.length;
+    let from = 0, to = nKeys - 1;
+    while (from !== nKeys && times[from] < startTime) {
+      ++from;
+    }
+    while (to !== -1 && times[to] > endTime) {
+      --to;
+    }
+    ++to;
+    if (from !== 0 || to !== nKeys) {
+      if (from >= to) {
+        to = Math.max(to, 1);
+        from = to - 1;
+      }
+      const stride = this.getValueSize();
+      this.times = arraySlice(times, from, to);
+      this.values = arraySlice(this.values, from * stride, to * stride);
+    }
+    return this;
+  }
+  // ensure we do not get a GarbageInGarbageOut situation, make sure tracks are at least minimally viable
+  validate() {
+    let valid = true;
+    const valueSize = this.getValueSize();
+    if (valueSize - Math.floor(valueSize) !== 0) {
+      console.warn("KeyframeTrack: Invalid value size in track.", this);
+      valid = false;
+    }
+    const times = this.times, values = this.values, nKeys = times.length;
+    if (nKeys === 0) {
+      console.warn("KeyframeTrack: Track is empty.", this);
+      valid = false;
+    }
+    let prevTime = null;
+    for (let i = 0; i !== nKeys; i++) {
+      const currTime = times[i];
+      if (typeof currTime === "number" && isNaN(currTime)) {
+        console.warn("KeyframeTrack: Time is not a valid number.", this, i, currTime);
+        valid = false;
+        break;
+      }
+      if (prevTime !== null && prevTime > currTime) {
+        console.warn("KeyframeTrack: Out of order keys.", this, i, currTime, prevTime);
+        valid = false;
+        break;
+      }
+      prevTime = currTime;
+    }
+    if (values !== void 0) {
+      if (isTypedArray(values)) {
+        for (let i = 0, n = values.length; i !== n; ++i) {
+          const value = values[i];
+          if (isNaN(value)) {
+            console.warn("KeyframeTrack: Value is not a valid number.", this, i, value);
+            valid = false;
+            break;
+          }
+        }
+      }
+    }
+    return valid;
+  }
+  // removes equivalent sequential keys as common in morph target sequences
+  // (0,0,0,0,1,1,1,0,0,0,0,0,0,0) --> (0,0,1,1,0,0)
+  optimize() {
+    const times = arraySlice(this.times), values = arraySlice(this.values), stride = this.getValueSize(), smoothInterpolation = this.getInterpolation() === InterpolateSmooth, lastIndex = times.length - 1;
+    let writeIndex = 1;
+    for (let i = 1; i < lastIndex; ++i) {
+      let keep = false;
+      const time = times[i];
+      const timeNext = times[i + 1];
+      if (time !== timeNext && (i !== 1 || time !== times[0])) {
+        if (!smoothInterpolation) {
+          const offset = i * stride, offsetP = offset - stride, offsetN = offset + stride;
+          for (let j = 0; j !== stride; ++j) {
+            const value = values[offset + j];
+            if (value !== values[offsetP + j] || value !== values[offsetN + j]) {
+              keep = true;
+              break;
+            }
+          }
+        } else {
+          keep = true;
+        }
+      }
+      if (keep) {
+        if (i !== writeIndex) {
+          times[writeIndex] = times[i];
+          const readOffset = i * stride, writeOffset = writeIndex * stride;
+          for (let j = 0; j !== stride; ++j) {
+            values[writeOffset + j] = values[readOffset + j];
+          }
+        }
+        ++writeIndex;
+      }
+    }
+    if (lastIndex > 0) {
+      times[writeIndex] = times[lastIndex];
+      for (let readOffset = lastIndex * stride, writeOffset = writeIndex * stride, j = 0; j !== stride; ++j) {
+        values[writeOffset + j] = values[readOffset + j];
+      }
+      ++writeIndex;
+    }
+    if (writeIndex !== times.length) {
+      this.times = arraySlice(times, 0, writeIndex);
+      this.values = arraySlice(values, 0, writeIndex * stride);
+    } else {
+      this.times = times;
+      this.values = values;
+    }
+    return this;
+  }
+  clone() {
+    const times = arraySlice(this.times, 0);
+    const values = arraySlice(this.values, 0);
+    const track = new this.constructor(this.name, times, values);
+    track.createInterpolant = this.createInterpolant;
+    return track;
+  }
+}
+KeyframeTrack.prototype.TimeBufferType = Float32Array;
+KeyframeTrack.prototype.ValueBufferType = Float32Array;
+KeyframeTrack.prototype.DefaultInterpolation = InterpolateLinear;
+
+class BooleanKeyframeTrack extends KeyframeTrack {
+}
+BooleanKeyframeTrack.prototype.ValueTypeName = "bool";
+BooleanKeyframeTrack.prototype.ValueBufferType = Array;
+BooleanKeyframeTrack.prototype.DefaultInterpolation = InterpolateDiscrete;
+BooleanKeyframeTrack.prototype.InterpolantFactoryMethodLinear = void 0;
+BooleanKeyframeTrack.prototype.InterpolantFactoryMethodSmooth = void 0;
+
+class ColorKeyframeTrack extends KeyframeTrack {
+}
+ColorKeyframeTrack.prototype.ValueTypeName = "color";
+
+class NumberKeyframeTrack extends KeyframeTrack {
+}
+NumberKeyframeTrack.prototype.ValueTypeName = "number";
+
+class QuaternionKeyframeTrack extends KeyframeTrack {
+  InterpolantFactoryMethodLinear(result) {
+    return new QuaternionLinearInterpolant(this.times, this.values, this.getValueSize(), result);
+  }
+}
+QuaternionKeyframeTrack.prototype.ValueTypeName = "quaternion";
+QuaternionKeyframeTrack.prototype.DefaultInterpolation = InterpolateLinear;
+QuaternionKeyframeTrack.prototype.InterpolantFactoryMethodSmooth = void 0;
+
+class StringKeyframeTrack extends KeyframeTrack {
+}
+StringKeyframeTrack.prototype.ValueTypeName = "string";
+StringKeyframeTrack.prototype.ValueBufferType = Array;
+StringKeyframeTrack.prototype.DefaultInterpolation = InterpolateDiscrete;
+StringKeyframeTrack.prototype.InterpolantFactoryMethodLinear = void 0;
+StringKeyframeTrack.prototype.InterpolantFactoryMethodSmooth = void 0;
+
+class VectorKeyframeTrack extends KeyframeTrack {
+}
+VectorKeyframeTrack.prototype.ValueTypeName = "vector";
+
+export { BooleanKeyframeTrack, ColorKeyframeTrack, KeyframeTrack, NumberKeyframeTrack, QuaternionKeyframeTrack, StringKeyframeTrack, VectorKeyframeTrack };
