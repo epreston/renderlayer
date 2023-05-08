df34c8b58fb77ab522469564ff722316efa8b4a8
Ed Preston <epreston@prestonsoft.com>
2023-05-08 13:45:49 +1000

    Create InstancedInterleavedBuffer.unit.test.js

---
+++ packages/buffers/tests/InstancedInterleavedBuffer.unit.test.js
@@ -0,0 +1,45 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { InterleavedBuffer } from '../src/InterleavedBuffer.js';
+import { InstancedInterleavedBuffer } from '../src/InstancedInterleavedBuffer.js';
+
+describe('Buffers', () => {
+  describe('InstancedInterleavedBuffer', () => {
+    test('Instancing', () => {
+      const array = new Float32Array([1, 2, 3, 7, 8, 9]);
+      const instance = new InstancedInterleavedBuffer(array, 3);
+
+      expect(instance.meshPerAttribute === 1).toBeTruthy();
+    });
+
+    test('Extending', () => {
+      const object = new InstancedInterleavedBuffer();
+      expect(object).toBeInstanceOf(InterleavedBuffer);
+    });
+
+    test.todo('meshPerAttribute', () => {
+      // implement
+    });
+
+    test('isInstancedInterleavedBuffer', () => {
+      const object = new InstancedInterleavedBuffer();
+      expect(object.isInstancedInterleavedBuffer).toBeTruthy();
+    });
+
+    test('copy', () => {
+      const array = new Float32Array([1, 2, 3, 7, 8, 9]);
+      const instance = new InstancedInterleavedBuffer(array, 3);
+      const copiedInstance = instance.copy(instance);
+
+      expect(copiedInstance.meshPerAttribute === 1).toBeTruthy();
+    });
+
+    test.todo('clone', () => {
+      // implement
+    });
+
+    test.todo('toJSON', () => {
+      // implement
+    });
+  });
+});
