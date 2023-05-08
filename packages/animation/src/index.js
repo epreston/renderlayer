b1ea3c6c4590cda0a0114c3d417640f36e913e13
Ed Preston <epreston@prestonsoft.com>
2023-05-09 07:08:38 +1000

    Create index.js

---
+++ packages/animation/src/index.js
@@ -0,0 +1,6 @@
+export * from './AnimationAction.js';
+export * from './AnimationClip.js';
+export * from './AnimationMixer.js';
+export * from './AnimationUtils.js';
+export * from './PropertyBinding.js';
+export * from './PropertyMixer.js';
