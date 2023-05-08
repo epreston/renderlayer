2c8d4a61e8a84d8987255e80a10d81a666fcbdb3
Ed Preston <epreston@prestonsoft.com>
2023-05-09 06:31:49 +1000

    Create AnimationClip.unit.test.js

---
+++ packages/animation/tests/AnimationClip.unit.test.js
@@ -0,0 +1,83 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { AnimationClip } from '../src/AnimationClip.js';
+
+describe('Animation', () => {
+  describe('AnimationClip', () => {
+    test('Instancing', () => {
+      const clip = new AnimationClip('clip1', 1000, [{}]);
+      expect(clip).toBeTruthy();
+    });
+
+    test('name', () => {
+      const clip = new AnimationClip('clip1', 1000, [{}]);
+      expect(clip.name === 'clip1').toBeTruthy();
+    });
+
+    test.todo('tracks', () => {
+      // implement
+    });
+
+    test.todo('duration', () => {
+      // implement
+    });
+
+    test.todo('blendMode', () => {
+      // implement
+    });
+
+    test.todo('uuid', () => {
+      // implement
+    });
+
+    test.todo('parse', () => {
+      // implement
+    });
+
+    test.todo('toJSON', () => {
+      // static toJSON
+      // implement
+    });
+
+    test.todo('CreateFromMorphTargetSequence', () => {
+      // implement
+    });
+
+    test.todo('findByName', () => {
+      // implement
+    });
+
+    test.todo('CreateClipsFromMorphTargetSequences', () => {
+      // implement
+    });
+
+    test.todo('parseAnimation', () => {
+      // implement
+    });
+
+    test.todo('resetDuration', () => {
+      // implement
+    });
+
+    test.todo('trim', () => {
+      // implement
+    });
+
+    test.todo('validate', () => {
+      // implement
+    });
+
+    test.todo('optimize', () => {
+      // implement
+    });
+
+    test.todo('clone', () => {
+      // implement
+    });
+
+    test.todo('toJSON', () => {
+      // member method toJSON
+      // implement
+    });
+  });
+});
