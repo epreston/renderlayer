26852e0977a560289f654148c41f736001d30312
Ed Preston <epreston@prestonsoft.com>
2023-05-08 12:00:10 +1000

    Create ImageUtils.unit.test.js

---
+++ packages/shared/tests/ImageUtils.unit.test.js
@@ -0,0 +1,17 @@
+// @vitest-environment jsdom
+
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { ImageUtils } from '../src/ImageUtils.js';
+
+describe('Shared', () => {
+  describe('ImageUtils', () => {
+    test.todo('getDataURL', () => {
+      // implement
+    });
+
+    test.todo('sRGBToLinear', () => {
+      // implement
+    });
+  });
+});
