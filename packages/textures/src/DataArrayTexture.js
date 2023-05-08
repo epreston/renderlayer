6b5a03b739157de0b95283fbea8fcd82906cb953
Ed Preston <epreston@prestonsoft.com>
2023-05-08 22:12:54 +1000

    Create DataArrayTexture.js

---
+++ packages/textures/src/DataArrayTexture.js
@@ -0,0 +1,24 @@
+import { ClampToEdgeWrapping, NearestFilter } from '@renderlayer/shared';
+import { Texture } from './Texture.js';
+
+class DataArrayTexture extends Texture {
+  constructor(data = null, width = 1, height = 1, depth = 1) {
+    super(null);
+
+    this.isDataArrayTexture = true;
+
+    this.image = { data, width, height, depth };
+
+    this.magFilter = NearestFilter;
+    this.minFilter = NearestFilter;
+
+    this.wrapR = ClampToEdgeWrapping;
+
+    this.generateMipmaps = false;
+    this.flipY = false;
+    this.unpackAlignment = 1;
+  }
+}
+
+export { DataArrayTexture };
+
