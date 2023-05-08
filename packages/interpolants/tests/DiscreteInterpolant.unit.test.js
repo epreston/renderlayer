f0eb3a814b854737c8bb4096d339da4c0ab1dc7d
Ed Preston <epreston@prestonsoft.com>
2023-05-09 04:15:13 +1000

    Create DiscreteInterpolant.unit.test.js

---
+++ packages/interpolants/tests/DiscreteInterpolant.unit.test.js
@@ -0,0 +1,26 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { Interpolant } from '../src/Interpolant.js';
+import { DiscreteInterpolant } from '../src/DiscreteInterpolant.js';
+
+describe('Interpolants', () => {
+  describe('DiscreteInterpolant', () => {
+    test('Extending', () => {
+      const object = new DiscreteInterpolant(null, [1, 11, 2, 22, 3, 33], 2, []);
+      expect(object).toBeInstanceOf(Interpolant);
+    });
+
+    test('Instancing', () => {
+      // parameterPositions, sampleValues, sampleSize, resultBuffer
+      const object = new DiscreteInterpolant(null, [1, 11, 2, 22, 3, 33], 2, []);
+      expect(object).toBeDefined();
+    });
+
+    test.todo('interpolate_', () => {
+      // private
+      // interpolate_( i1 /*, t0, t, t1 */ )
+      // return equal to base class Interpolant.resultBuffer after call
+      // implement
+    });
+  });
+});
