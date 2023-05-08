1c0a4ec4a0f2c226ecf212804eefe8472b19eef7
Ed Preston <epreston@prestonsoft.com>
2023-05-08 11:02:08 +1000

    Create shaders.esm-bundler.js
    0.0.1

---
+++ packages/shaders/dist/shaders.esm-bundler.js
@@ -0,0 +1,500 @@
+import { Color, Matrix3, Vector2, Vector3 } from '@renderlayer/math';
+import { LinearSRGBColorSpace } from '@renderlayer/shared';
+
+const ShaderChunk = {};
+
+const UniformsLib = {
+  common: {
+    diffuse: { value: /* @__PURE__ */ new Color(16777215) },
+    opacity: { value: 1 },
+    map: { value: null },
+    mapTransform: { value: /* @__PURE__ */ new Matrix3() },
+    alphaMap: { value: null },
+    alphaMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+    alphaTest: { value: 0 }
+  },
+  specularmap: {
+    specularMap: { value: null },
+    specularMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  envmap: {
+    envMap: { value: null },
+    flipEnvMap: { value: -1 },
+    reflectivity: { value: 1 },
+    // basic, lambert, phong
+    ior: { value: 1.5 },
+    // physical
+    refractionRatio: { value: 0.98 }
+    // basic, lambert, phong
+  },
+  aomap: {
+    aoMap: { value: null },
+    aoMapIntensity: { value: 1 },
+    aoMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  lightmap: {
+    lightMap: { value: null },
+    lightMapIntensity: { value: 1 },
+    lightMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  bumpmap: {
+    bumpMap: { value: null },
+    bumpMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+    bumpScale: { value: 1 }
+  },
+  normalmap: {
+    normalMap: { value: null },
+    normalMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+    normalScale: { value: /* @__PURE__ */ new Vector2(1, 1) }
+  },
+  displacementmap: {
+    displacementMap: { value: null },
+    displacementMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+    displacementScale: { value: 1 },
+    displacementBias: { value: 0 }
+  },
+  emissivemap: {
+    emissiveMap: { value: null },
+    emissiveMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  metalnessmap: {
+    metalnessMap: { value: null },
+    metalnessMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  roughnessmap: {
+    roughnessMap: { value: null },
+    roughnessMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  gradientmap: {
+    gradientMap: { value: null }
+  },
+  fog: {
+    fogDensity: { value: 25e-5 },
+    fogNear: { value: 1 },
+    fogFar: { value: 2e3 },
+    fogColor: { value: /* @__PURE__ */ new Color(16777215) }
+  },
+  lights: {
+    ambientLightColor: { value: [] },
+    lightProbe: { value: [] },
+    directionalLights: {
+      value: [],
+      properties: {
+        direction: {},
+        color: {}
+      }
+    },
+    directionalLightShadows: {
+      value: [],
+      properties: {
+        shadowBias: {},
+        shadowNormalBias: {},
+        shadowRadius: {},
+        shadowMapSize: {}
+      }
+    },
+    directionalShadowMap: { value: [] },
+    directionalShadowMatrix: { value: [] },
+    spotLights: {
+      value: [],
+      properties: {
+        color: {},
+        position: {},
+        direction: {},
+        distance: {},
+        coneCos: {},
+        penumbraCos: {},
+        decay: {}
+      }
+    },
+    spotLightShadows: {
+      value: [],
+      properties: {
+        shadowBias: {},
+        shadowNormalBias: {},
+        shadowRadius: {},
+        shadowMapSize: {}
+      }
+    },
+    spotLightMap: { value: [] },
+    spotShadowMap: { value: [] },
+    spotLightMatrix: { value: [] },
+    pointLights: {
+      value: [],
+      properties: {
+        color: {},
+        position: {},
+        decay: {},
+        distance: {}
+      }
+    },
+    pointLightShadows: {
+      value: [],
+      properties: {
+        shadowBias: {},
+        shadowNormalBias: {},
+        shadowRadius: {},
+        shadowMapSize: {},
+        shadowCameraNear: {},
+        shadowCameraFar: {}
+      }
+    },
+    pointShadowMap: { value: [] },
+    pointShadowMatrix: { value: [] },
+    hemisphereLights: {
+      value: [],
+      properties: {
+        direction: {},
+        skyColor: {},
+        groundColor: {}
+      }
+    },
+    // TODO: RectAreaLight BRDF data needs to be moved from example to main src
+    rectAreaLights: {
+      value: [],
+      properties: {
+        color: {},
+        position: {},
+        width: {},
+        height: {}
+      }
+    },
+    ltc_1: { value: null },
+    ltc_2: { value: null }
+  },
+  points: {
+    diffuse: { value: /* @__PURE__ */ new Color(16777215) },
+    opacity: { value: 1 },
+    size: { value: 1 },
+    scale: { value: 1 },
+    map: { value: null },
+    alphaMap: { value: null },
+    alphaTest: { value: 0 },
+    uvTransform: { value: /* @__PURE__ */ new Matrix3() }
+  },
+  sprite: {
+    diffuse: { value: /* @__PURE__ */ new Color(16777215) },
+    opacity: { value: 1 },
+    center: { value: /* @__PURE__ */ new Vector2(0.5, 0.5) },
+    rotation: { value: 0 },
+    map: { value: null },
+    mapTransform: { value: /* @__PURE__ */ new Matrix3() },
+    alphaMap: { value: null },
+    alphaTest: { value: 0 }
+  }
+};
+
+function cloneUniforms(src) {
+  const dst = {};
+  for (const u in src) {
+    dst[u] = {};
+    for (const p in src[u]) {
+      const property = src[u][p];
+      if (property && (property.isColor || property.isMatrix3 || property.isMatrix4 || property.isVector2 || property.isVector3 || property.isVector4 || property.isTexture || property.isQuaternion)) {
+        if (property.isRenderTargetTexture) {
+          console.warn(
+            "UniformsUtils: Textures of render targets cannot be cloned via cloneUniforms() or mergeUniforms()."
+          );
+          dst[u][p] = null;
+        } else {
+          dst[u][p] = property.clone();
+        }
+      } else if (Array.isArray(property)) {
+        dst[u][p] = property.slice();
+      } else {
+        dst[u][p] = property;
+      }
+    }
+  }
+  return dst;
+}
+function mergeUniforms(uniforms) {
+  const merged = {};
+  for (let u = 0; u < uniforms.length; u++) {
+    const tmp = cloneUniforms(uniforms[u]);
+    for (const p in tmp) {
+      merged[p] = tmp[p];
+    }
+  }
+  return merged;
+}
+function cloneUniformsGroups(src) {
+  const dst = [];
+  for (let u = 0; u < src.length; u++) {
+    dst.push(src[u].clone());
+  }
+  return dst;
+}
+function getUnlitUniformColorSpace(renderer) {
+  if (renderer.getRenderTarget() === null) {
+    return renderer.outputColorSpace;
+  }
+  return LinearSRGBColorSpace;
+}
+
+const ShaderLib = {
+  basic: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.specularmap,
+      UniformsLib.envmap,
+      UniformsLib.aomap,
+      UniformsLib.lightmap,
+      UniformsLib.fog
+    ]),
+    vertexShader: ShaderChunk.meshbasic_vert,
+    fragmentShader: ShaderChunk.meshbasic_frag
+  },
+  lambert: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.specularmap,
+      UniformsLib.envmap,
+      UniformsLib.aomap,
+      UniformsLib.lightmap,
+      UniformsLib.emissivemap,
+      UniformsLib.bumpmap,
+      UniformsLib.normalmap,
+      UniformsLib.displacementmap,
+      UniformsLib.fog,
+      UniformsLib.lights,
+      {
+        emissive: { value: /* @__PURE__ */ new Color(0) }
+      }
+    ]),
+    vertexShader: ShaderChunk.meshlambert_vert,
+    fragmentShader: ShaderChunk.meshlambert_frag
+  },
+  phong: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.specularmap,
+      UniformsLib.envmap,
+      UniformsLib.aomap,
+      UniformsLib.lightmap,
+      UniformsLib.emissivemap,
+      UniformsLib.bumpmap,
+      UniformsLib.normalmap,
+      UniformsLib.displacementmap,
+      UniformsLib.fog,
+      UniformsLib.lights,
+      {
+        emissive: { value: /* @__PURE__ */ new Color(0) },
+        specular: { value: /* @__PURE__ */ new Color(1118481) },
+        shininess: { value: 30 }
+      }
+    ]),
+    vertexShader: ShaderChunk.meshphong_vert,
+    fragmentShader: ShaderChunk.meshphong_frag
+  },
+  standard: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.envmap,
+      UniformsLib.aomap,
+      UniformsLib.lightmap,
+      UniformsLib.emissivemap,
+      UniformsLib.bumpmap,
+      UniformsLib.normalmap,
+      UniformsLib.displacementmap,
+      UniformsLib.roughnessmap,
+      UniformsLib.metalnessmap,
+      UniformsLib.fog,
+      UniformsLib.lights,
+      {
+        emissive: { value: /* @__PURE__ */ new Color(0) },
+        roughness: { value: 1 },
+        metalness: { value: 0 },
+        envMapIntensity: { value: 1 }
+        // temporary
+      }
+    ]),
+    vertexShader: ShaderChunk.meshphysical_vert,
+    fragmentShader: ShaderChunk.meshphysical_frag
+  },
+  toon: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.aomap,
+      UniformsLib.lightmap,
+      UniformsLib.emissivemap,
+      UniformsLib.bumpmap,
+      UniformsLib.normalmap,
+      UniformsLib.displacementmap,
+      UniformsLib.gradientmap,
+      UniformsLib.fog,
+      UniformsLib.lights,
+      {
+        emissive: { value: /* @__PURE__ */ new Color(0) }
+      }
+    ]),
+    vertexShader: ShaderChunk.meshtoon_vert,
+    fragmentShader: ShaderChunk.meshtoon_frag
+  },
+  matcap: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.bumpmap,
+      UniformsLib.normalmap,
+      UniformsLib.displacementmap,
+      UniformsLib.fog,
+      {
+        matcap: { value: null }
+      }
+    ]),
+    vertexShader: ShaderChunk.meshmatcap_vert,
+    fragmentShader: ShaderChunk.meshmatcap_frag
+  },
+  points: {
+    uniforms: /* @__PURE__ */ mergeUniforms([UniformsLib.points, UniformsLib.fog]),
+    vertexShader: ShaderChunk.points_vert,
+    fragmentShader: ShaderChunk.points_frag
+  },
+  dashed: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.fog,
+      {
+        scale: { value: 1 },
+        dashSize: { value: 1 },
+        totalSize: { value: 2 }
+      }
+    ]),
+    vertexShader: ShaderChunk.linedashed_vert,
+    fragmentShader: ShaderChunk.linedashed_frag
+  },
+  depth: {
+    uniforms: /* @__PURE__ */ mergeUniforms([UniformsLib.common, UniformsLib.displacementmap]),
+    vertexShader: ShaderChunk.depth_vert,
+    fragmentShader: ShaderChunk.depth_frag
+  },
+  normal: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.bumpmap,
+      UniformsLib.normalmap,
+      UniformsLib.displacementmap,
+      {
+        opacity: { value: 1 }
+      }
+    ]),
+    vertexShader: ShaderChunk.meshnormal_vert,
+    fragmentShader: ShaderChunk.meshnormal_frag
+  },
+  sprite: {
+    uniforms: /* @__PURE__ */ mergeUniforms([UniformsLib.sprite, UniformsLib.fog]),
+    vertexShader: ShaderChunk.sprite_vert,
+    fragmentShader: ShaderChunk.sprite_frag
+  },
+  background: {
+    uniforms: {
+      uvTransform: { value: /* @__PURE__ */ new Matrix3() },
+      t2D: { value: null },
+      backgroundIntensity: { value: 1 }
+    },
+    vertexShader: ShaderChunk.background_vert,
+    fragmentShader: ShaderChunk.background_frag
+  },
+  backgroundCube: {
+    uniforms: {
+      envMap: { value: null },
+      flipEnvMap: { value: -1 },
+      backgroundBlurriness: { value: 0 },
+      backgroundIntensity: { value: 1 }
+    },
+    vertexShader: ShaderChunk.backgroundCube_vert,
+    fragmentShader: ShaderChunk.backgroundCube_frag
+  },
+  cube: {
+    uniforms: {
+      tCube: { value: null },
+      tFlip: { value: -1 },
+      opacity: { value: 1 }
+    },
+    vertexShader: ShaderChunk.cube_vert,
+    fragmentShader: ShaderChunk.cube_frag
+  },
+  equirect: {
+    uniforms: {
+      tEquirect: { value: null }
+    },
+    vertexShader: ShaderChunk.equirect_vert,
+    fragmentShader: ShaderChunk.equirect_frag
+  },
+  distanceRGBA: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.common,
+      UniformsLib.displacementmap,
+      {
+        referencePosition: { value: /* @__PURE__ */ new Vector3() },
+        nearDistance: { value: 1 },
+        farDistance: { value: 1e3 }
+      }
+    ]),
+    vertexShader: ShaderChunk.distanceRGBA_vert,
+    fragmentShader: ShaderChunk.distanceRGBA_frag
+  },
+  shadow: {
+    uniforms: /* @__PURE__ */ mergeUniforms([
+      UniformsLib.lights,
+      UniformsLib.fog,
+      {
+        color: { value: /* @__PURE__ */ new Color(0) },
+        opacity: { value: 1 }
+      }
+    ]),
+    vertexShader: ShaderChunk.shadow_vert,
+    fragmentShader: ShaderChunk.shadow_frag
+  }
+};
+ShaderLib.physical = {
+  uniforms: /* @__PURE__ */ mergeUniforms([
+    ShaderLib.standard.uniforms,
+    {
+      clearcoat: { value: 0 },
+      clearcoatMap: { value: null },
+      clearcoatMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      clearcoatNormalMap: { value: null },
+      clearcoatNormalMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      clearcoatNormalScale: { value: /* @__PURE__ */ new Vector2(1, 1) },
+      clearcoatRoughness: { value: 0 },
+      clearcoatRoughnessMap: { value: null },
+      clearcoatRoughnessMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      iridescence: { value: 0 },
+      iridescenceMap: { value: null },
+      iridescenceMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      iridescenceIOR: { value: 1.3 },
+      iridescenceThicknessMinimum: { value: 100 },
+      iridescenceThicknessMaximum: { value: 400 },
+      iridescenceThicknessMap: { value: null },
+      iridescenceThicknessMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      sheen: { value: 0 },
+      sheenColor: { value: /* @__PURE__ */ new Color(0) },
+      sheenColorMap: { value: null },
+      sheenColorMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      sheenRoughness: { value: 1 },
+      sheenRoughnessMap: { value: null },
+      sheenRoughnessMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      transmission: { value: 0 },
+      transmissionMap: { value: null },
+      transmissionMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      transmissionSamplerSize: { value: /* @__PURE__ */ new Vector2() },
+      transmissionSamplerMap: { value: null },
+      thickness: { value: 0 },
+      thicknessMap: { value: null },
+      thicknessMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      attenuationDistance: { value: 0 },
+      attenuationColor: { value: /* @__PURE__ */ new Color(0) },
+      specularColor: { value: /* @__PURE__ */ new Color(1, 1, 1) },
+      specularColorMap: { value: null },
+      specularColorMapTransform: { value: /* @__PURE__ */ new Matrix3() },
+      specularIntensity: { value: 1 },
+      specularIntensityMap: { value: null },
+      specularIntensityMapTransform: { value: /* @__PURE__ */ new Matrix3() }
+    }
+  ]),
+  vertexShader: ShaderChunk.meshphysical_vert,
+  fragmentShader: ShaderChunk.meshphysical_frag
+};
+
+export { ShaderChunk, ShaderLib, UniformsLib, cloneUniforms, cloneUniformsGroups, getUnlitUniformColorSpace, mergeUniforms };
