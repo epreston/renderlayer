9b1d2716bbe2adc1611dd636de94e8db3f45abf1
Ed Preston <epreston@prestonsoft.com>
2023-05-09 05:19:15 +1000

    Create BooleanKeyframeTrack.unit.test.js

---
+++ packages/keyframes/tests/BooleanKeyframeTrack.unit.test.js
@@ -0,0 +1,35 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { KeyframeTrack } from '../src/KeyframeTrack.js';
+import { BooleanKeyframeTrack } from '../src/BooleanKeyframeTrack.js';
+
+describe('Keyframes', () => {
+  describe('BooleanKeyframeTrack', () => {
+    const parameters = {
+      name: '.visible',
+      times: [0, 1],
+      values: [true, false],
+      interpolation: BooleanKeyframeTrack.DefaultInterpolation,
+    };
+
+    test('Instancing', () => {
+      // name, times, values
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object).toBeDefined();
+
+      // name, times, values, interpolation
+      const object_all = new BooleanKeyframeTrack(
+        parameters.name,
+        parameters.times,
+        parameters.values,
+        parameters.interpolation
+      );
+      expect(object_all).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object).toBeInstanceOf(KeyframeTrack);
+    });
+  });
+});
