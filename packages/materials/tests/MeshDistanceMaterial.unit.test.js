1197f72365106fbeca45298de45552d08933d6d2
Ed Preston <epreston@prestonsoft.com>
2023-05-09 01:29:08 +1000

    Create MeshDistanceMaterial.unit.test.js

---
+++ packages/materials/tests/MeshDistanceMaterial.unit.test.js
@@ -0,0 +1,52 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { Material } from '../src/Material.js';
+import { MeshDistanceMaterial } from '../src/MeshDistanceMaterial.js';
+
+describe('Materials', () => {
+  describe('MeshDistanceMaterial', () => {
+    test('Instancing', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object ).toBeInstanceOf( Material);
+    });
+
+    test('type', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.type === 'MeshDistanceMaterial').toBeTruthy();
+    });
+
+    test.todo('map', () => {
+      // implement
+    });
+
+    test.todo('alphaMap', () => {
+      // implement
+    });
+
+    test.todo('displacementMap', () => {
+      // implement
+    });
+
+    test.todo('displacementScale', () => {
+      // implement
+    });
+
+    test.todo('displacementBias', () => {
+      // implement
+    });
+
+    test('isMeshDistanceMaterial', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.isMeshDistanceMaterial).toBeTruthy();
+    });
+
+    test.todo('copy', () => {
+      // implement
+    });
+  });
+});
