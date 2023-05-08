e0ace703c358d3096456e2eb81310576def99808
Ed Preston <epreston@prestonsoft.com>
2023-05-09 05:19:26 +1000

    Create QuaternionKeyframeTrack.unit.test.js

---
+++ packages/keyframes/tests/QuaternionKeyframeTrack.unit.test.js
@@ -0,0 +1,35 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { KeyframeTrack } from '../src/KeyframeTrack.js';
+import { QuaternionKeyframeTrack } from '../src/QuaternionKeyframeTrack.js';
+
+describe('Keyframes', () => {
+  describe('QuaternionKeyframeTrack', () => {
+    const parameters = {
+      name: '.rotation',
+      times: [0],
+      values: [0.5, 0.5, 0.5, 1],
+      interpolation: QuaternionKeyframeTrack.DefaultInterpolation,
+    };
+
+    test('Instancing', () => {
+      // name, times, values
+      const object = new QuaternionKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object).toBeDefined();
+
+      // name, times, values, interpolation
+      const object_all = new QuaternionKeyframeTrack(
+        parameters.name,
+        parameters.times,
+        parameters.values,
+        parameters.interpolation
+      );
+      expect(object_all).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new QuaternionKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object).toBeInstanceOf(KeyframeTrack);
+    });
+  });
+});
