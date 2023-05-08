ba3e312c2d7d9b0e95b794dd564f34c64d7b0260
Ed Preston <epreston@prestonsoft.com>
2023-05-08 11:03:43 +1000

    Create RawShaderMaterial.js

---
+++ packages/materials/src/RawShaderMaterial.js
@@ -0,0 +1,12 @@
+import { ShaderMaterial } from './ShaderMaterial.js';
+
+class RawShaderMaterial extends ShaderMaterial {
+  constructor(parameters) {
+    super(parameters);
+
+    this.isRawShaderMaterial = true;
+    this.type = 'RawShaderMaterial';
+  }
+}
+
+export { RawShaderMaterial };
