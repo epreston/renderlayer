9a45891d74e17ce66bd9f16a0a7a392e25c17008
Ed Preston <epreston@prestonsoft.com>
2023-05-09 06:28:58 +1000

    Create PropertyMixer.unit.test.js

---
+++ packages/animation/tests/PropertyMixer.unit.test.js
@@ -0,0 +1,60 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { PropertyMixer } from '../src/PropertyMixer.js';
+
+describe('Animation', () => {
+  describe('PropertyMixer', () => {
+    test('Instancing', () => {
+      const object = new PropertyMixer();
+      expect(object).toBeDefined();
+    });
+
+    test.todo('binding', () => {
+      // implement
+    });
+
+    test.todo('valueSize', () => {
+      // implement
+    });
+
+    test.todo('buffer', () => {
+      // implement
+    });
+
+    test.todo('cumulativeWeight', () => {
+      // implement
+    });
+
+    test.todo('cumulativeWeightAdditive', () => {
+      // implement
+    });
+
+    test.todo('useCount', () => {
+      // implement
+    });
+
+    test.todo('referenceCount', () => {
+      // implement
+    });
+
+    test.todo('accumulate', () => {
+      // implement
+    });
+
+    test.todo('accumulateAdditive', () => {
+      // implement
+    });
+
+    test.todo('apply', () => {
+      // implement
+    });
+
+    test.todo('saveOriginalState', () => {
+      // implement
+    });
+
+    test.todo('restoreOriginalState', () => {
+      // implement
+    });
+  });
+});
