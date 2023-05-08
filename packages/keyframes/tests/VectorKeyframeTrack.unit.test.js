3d08454c5e8668129b0857f964a3fd937b22b1ce
Ed Preston <epreston@prestonsoft.com>
2023-05-09 05:19:35 +1000

    Create VectorKeyframeTrack.unit.test.js

---
+++ packages/keyframes/tests/VectorKeyframeTrack.unit.test.js
@@ -0,0 +1,35 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { KeyframeTrack } from '../src/KeyframeTrack.js';
+import { VectorKeyframeTrack } from '../src/VectorKeyframeTrack.js';
+
+describe('Keyframes', () => {
+  describe('VectorKeyframeTrack', () => {
+    const parameters = {
+      name: '.force',
+      times: [0],
+      values: [0.5, 0.5, 0.5],
+      interpolation: VectorKeyframeTrack.DefaultInterpolation,
+    };
+
+    test('Instancing', () => {
+      // name, times, values
+      const object = new VectorKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object).toBeDefined();
+
+      // name, times, values, interpolation
+      const object_all = new VectorKeyframeTrack(
+        parameters.name,
+        parameters.times,
+        parameters.values,
+        parameters.interpolation
+      );
+      expect(object_all).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new VectorKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object).toBeInstanceOf(KeyframeTrack);
+    });
+  });
+});
