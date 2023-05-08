3b030caf9eb65dc11abfa6c1bf9c8127edb040c5
Ed Preston <epreston@prestonsoft.com>
2023-05-08 23:20:50 +1000

    Create targets.esm-bundler.js

---
+++ packages/targets/dist/targets.esm-bundler.js
@@ -0,0 +1,179 @@
+import { LinearFilter, BackSide, NoBlending, LinearMipmapLinearFilter } from '@renderlayer/shared';
+import { BoxGeometry } from '@renderlayer/geometries';
+import { ShaderMaterial } from '@renderlayer/materials';
+import { Mesh } from '@renderlayer/objects';
+import { cloneUniforms } from '@renderlayer/shaders';
+import { CubeCamera } from '@renderlayer/cameras';
+import { Texture, Source, CubeTexture } from '@renderlayer/textures';
+import { EventDispatcher } from '@renderlayer/core';
+import { Vector4 } from '@renderlayer/math';
+
+class WebGLRenderTarget extends EventDispatcher {
+  constructor(width = 1, height = 1, options = {}) {
+    super();
+    this.isWebGLRenderTarget = true;
+    this.width = width;
+    this.height = height;
+    this.depth = 1;
+    this.scissor = new Vector4(0, 0, width, height);
+    this.scissorTest = false;
+    this.viewport = new Vector4(0, 0, width, height);
+    const image = { width, height, depth: 1 };
+    this.texture = new Texture(
+      image,
+      options.mapping,
+      options.wrapS,
+      options.wrapT,
+      options.magFilter,
+      options.minFilter,
+      options.format,
+      options.type,
+      options.anisotropy,
+      options.colorSpace
+    );
+    this.texture.isRenderTargetTexture = true;
+    this.texture.flipY = false;
+    this.texture.generateMipmaps = options.generateMipmaps !== void 0 ? options.generateMipmaps : false;
+    this.texture.internalFormat = options.internalFormat !== void 0 ? options.internalFormat : null;
+    this.texture.minFilter = options.minFilter !== void 0 ? options.minFilter : LinearFilter;
+    this.depthBuffer = options.depthBuffer !== void 0 ? options.depthBuffer : true;
+    this.stencilBuffer = options.stencilBuffer !== void 0 ? options.stencilBuffer : false;
+    this.depthTexture = options.depthTexture !== void 0 ? options.depthTexture : null;
+    this.samples = options.samples !== void 0 ? options.samples : 0;
+  }
+  setSize(width, height, depth = 1) {
+    if (this.width !== width || this.height !== height || this.depth !== depth) {
+      this.width = width;
+      this.height = height;
+      this.depth = depth;
+      this.texture.image.width = width;
+      this.texture.image.height = height;
+      this.texture.image.depth = depth;
+      this.dispose();
+    }
+    this.viewport.set(0, 0, width, height);
+    this.scissor.set(0, 0, width, height);
+  }
+  clone() {
+    return new this.constructor().copy(this);
+  }
+  copy(source) {
+    this.width = source.width;
+    this.height = source.height;
+    this.depth = source.depth;
+    this.scissor.copy(source.scissor);
+    this.scissorTest = source.scissorTest;
+    this.viewport.copy(source.viewport);
+    this.texture = source.texture.clone();
+    this.texture.isRenderTargetTexture = true;
+    const image = Object.assign({}, source.texture.image);
+    this.texture.source = new Source(image);
+    this.depthBuffer = source.depthBuffer;
+    this.stencilBuffer = source.stencilBuffer;
+    if (source.depthTexture !== null)
+      this.depthTexture = source.depthTexture.clone();
+    this.samples = source.samples;
+    return this;
+  }
+  dispose() {
+    this.dispatchEvent({ type: "dispose" });
+  }
+}
+
+class WebGLCubeRenderTarget extends WebGLRenderTarget {
+  constructor(size = 1, options = {}) {
+    super(size, size, options);
+    this.isWebGLCubeRenderTarget = true;
+    const image = { width: size, height: size, depth: 1 };
+    const images = [image, image, image, image, image, image];
+    this.texture = new CubeTexture(
+      images,
+      options.mapping,
+      options.wrapS,
+      options.wrapT,
+      options.magFilter,
+      options.minFilter,
+      options.format,
+      options.type,
+      options.anisotropy,
+      options.colorSpace
+    );
+    this.texture.isRenderTargetTexture = true;
+    this.texture.generateMipmaps = options.generateMipmaps !== void 0 ? options.generateMipmaps : false;
+    this.texture.minFilter = options.minFilter !== void 0 ? options.minFilter : LinearFilter;
+  }
+  fromEquirectangularTexture(renderer, texture) {
+    this.texture.type = texture.type;
+    this.texture.colorSpace = texture.colorSpace;
+    this.texture.generateMipmaps = texture.generateMipmaps;
+    this.texture.minFilter = texture.minFilter;
+    this.texture.magFilter = texture.magFilter;
+    const shader = {
+      uniforms: {
+        tEquirect: { value: null }
+      },
+      vertexShader: (
+        /* glsl */
+        `
+				varying vec3 vWorldDirection;
+
+				vec3 transformDirection( in vec3 dir, in mat4 matrix ) {
+					return normalize( ( matrix * vec4( dir, 0.0 ) ).xyz );
+				}
+
+				void main() {
+					vWorldDirection = transformDirection( position, modelMatrix );
+
+					#include <begin_vertex>
+					#include <project_vertex>
+				}
+			`
+      ),
+      fragmentShader: (
+        /* glsl */
+        `
+				uniform sampler2D tEquirect;
+				varying vec3 vWorldDirection;
+
+				#include <common>
+
+				void main() {
+					vec3 direction = normalize( vWorldDirection );
+					vec2 sampleUV = equirectUv( direction );
+					gl_FragColor = texture2D( tEquirect, sampleUV );
+				}
+			`
+      )
+    };
+    const geometry = new BoxGeometry(5, 5, 5);
+    const material = new ShaderMaterial({
+      name: "CubemapFromEquirect",
+      uniforms: cloneUniforms(shader.uniforms),
+      vertexShader: shader.vertexShader,
+      fragmentShader: shader.fragmentShader,
+      side: BackSide,
+      blending: NoBlending
+    });
+    material.uniforms.tEquirect.value = texture;
+    const mesh = new Mesh(geometry, material);
+    const currentMinFilter = texture.minFilter;
+    if (texture.minFilter === LinearMipmapLinearFilter)
+      texture.minFilter = LinearFilter;
+    const camera = new CubeCamera(1, 10, this);
+    camera.update(renderer, mesh);
+    texture.minFilter = currentMinFilter;
+    mesh.geometry.dispose();
+    mesh.material.dispose();
+    return this;
+  }
+  clear(renderer, color, depth, stencil) {
+    const currentRenderTarget = renderer.getRenderTarget();
+    for (let i = 0; i < 6; i++) {
+      renderer.setRenderTarget(this, i);
+      renderer.clear(color, depth, stencil);
+    }
+    renderer.setRenderTarget(currentRenderTarget);
+  }
+}
+
+export { WebGLCubeRenderTarget, WebGLRenderTarget };
