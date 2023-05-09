315e067a0d2e8dee2c624a437b8787a3391c0905
Ed Preston <epreston@prestonsoft.com>
2023-05-10 00:11:27 +1000

    Create WebGLBackground.unit.test.js

---
+++ packages/webgl/tests/WebGLBackground.unit.test.js
@@ -0,0 +1,35 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLBackground } from '../src/WebGLBackground.js';
+
+describe('WebGL', () => {
+  describe('WebGLBackground', () => {
+    it('should expose a function', () => {
+      expect(WebGLBackground).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('getClearColor', () => {
+      // implement
+    });
+
+    test.todo('setClearColor', () => {
+      // implement
+    });
+
+    test.todo('getClearAlpha', () => {
+      // implement
+    });
+
+    test.todo('setClearAlpha', () => {
+      // implement
+    });
+
+    test.todo('render', () => {
+      // implement
+    });
+  });
+});
