e1d9fbea0a5b120d7ee5b16d2dcef51e464a9cf4
Ed Preston <epreston@prestonsoft.com>
2023-05-09 22:45:59 +1000

    Create WebGLProperties.unit.test.js

---
+++ packages/webgl/tests/WebGLProperties.unit.test.js
@@ -0,0 +1,31 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLProperties } from '../src/WebGLProperties.js';
+
+describe('WebGL', () => {
+  describe('WebGLProperties', () => {
+    it('should expose a function', () => {
+      expect(WebGLProperties).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('get', () => {
+      // implement
+    });
+
+    test.todo('remove', () => {
+      // implement
+    });
+
+    test.todo('update', () => {
+      // implement
+    });
+
+    test.todo('dispose', () => {
+      // implement
+    });
+  });
+});
