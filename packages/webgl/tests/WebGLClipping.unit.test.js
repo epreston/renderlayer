77939ea5a8d15f0bcc3c766f80ebf5ddae788f8c
Ed Preston <epreston@prestonsoft.com>
2023-05-09 22:39:27 +1000

    Create WebGLClipping.unit.test.js

---
+++ packages/webgl/tests/WebGLClipping.unit.test.js
@@ -0,0 +1,31 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLClipping } from '../src/WebGLClipping.js';
+
+describe('WebGL', () => {
+  describe('WebGLClipping', () => {
+    it('should expose a function', () => {
+      expect(WebGLClipping).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('init', () => {
+      // implement
+    });
+
+    test.todo('beginShadows', () => {
+      // implement
+    });
+
+    test.todo('endShadows', () => {
+      // implement
+    });
+
+    test.todo('setState', () => {
+      // implement
+    });
+  });
+});
