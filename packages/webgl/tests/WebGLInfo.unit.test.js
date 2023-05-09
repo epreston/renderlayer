4acf56c26e791e4277d904c484aadd3d7868ac7a
Ed Preston <epreston@prestonsoft.com>
2023-05-09 22:44:07 +1000

    Create WebGLInfo.unit.test.js

---
+++ packages/webgl/tests/WebGLInfo.unit.test.js
@@ -0,0 +1,39 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLInfo } from '../src/WebGLInfo.js';
+
+describe('WebGL', () => {
+  describe('WebGLInfo', () => {
+    it('should expose a function', () => {
+      expect(WebGLInfo).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('memory', () => {
+      // implement
+    });
+
+    test.todo('render', () => {
+      // implement
+    });
+
+    test.todo('programs', () => {
+      // implement
+    });
+
+    test.todo('autoReset', () => {
+      // implement
+    });
+
+    test.todo('reset', () => {
+      // implement
+    });
+
+    test.todo('update', () => {
+      // implement
+    });
+  });
+});
