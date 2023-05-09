c2733e6b954a7eb3abaaf350f7321ee51b62bd3d
Ed Preston <epreston@prestonsoft.com>
2023-05-09 22:07:53 +1000

    Create WebGLBufferRenderer.unit.test.js

---
+++ packages/webgl/tests/WebGLBufferRenderer.unit.test.js
@@ -0,0 +1,27 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLBufferRenderer } from '../src/WebGLBufferRenderer.js';
+
+describe('WebGL', () => {
+  describe('WebGLBufferRenderer', () => {
+    it('should expose a function', () => {
+      expect(WebGLBufferRenderer).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('setMode', () => {
+      // implement
+    });
+
+    test.todo('render', () => {
+      // implement
+    });
+
+    test.todo('renderInstances', () => {
+      // implement
+    });
+  });
+});
