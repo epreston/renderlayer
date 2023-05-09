b359d5db18a5f2f5b0307a8e47867fdec40abf07
Ed Preston <epreston@prestonsoft.com>
2023-05-09 23:17:36 +1000

    Create WebGLGeometries.unit.test.js

---
+++ packages/webgl/tests/WebGLGeometries.unit.test.js
@@ -0,0 +1,27 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLGeometries } from '../src/WebGLGeometries.js';
+
+describe('WebGL', () => {
+  describe('WebGLGeometries', () => {
+    it('should expose a function', () => {
+      expect(WebGLGeometries).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('get', () => {
+      // implement
+    });
+
+    test.todo('update', () => {
+      // implement
+    });
+
+    test.todo('getWireframeAttribute', () => {
+      // implement
+    });
+  });
+});
