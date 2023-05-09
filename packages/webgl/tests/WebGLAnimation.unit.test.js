aa975e166106d3a00f34e8d83c4516e4aa443f9e
Ed Preston <epreston@prestonsoft.com>
2023-05-09 21:56:58 +1000

    Create WebGLAnimation.unit.test.js

---
+++ packages/webgl/tests/WebGLAnimation.unit.test.js
@@ -0,0 +1,31 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLAnimation } from '../src/WebGLAnimation.js';
+
+describe('WebGL', () => {
+  describe('WebGLAnimation', () => {
+    it('should expose a function', () => {
+      expect(WebGLAnimation).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('start', () => {
+      // implement
+    });
+
+    test.todo('stop', () => {
+      // implement
+    });
+
+    test.todo('setAnimationLoop', () => {
+      // implement
+    });
+
+    test.todo('setContext', () => {
+      // implement
+    });
+  });
+});
