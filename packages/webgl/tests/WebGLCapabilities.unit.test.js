096c0aa67b04be2ae54faee4a728cddcdb736f30
Ed Preston <epreston@prestonsoft.com>
2023-05-10 00:14:24 +1000

    Create WebGLCapabilities.unit.test.js

---
+++ packages/webgl/tests/WebGLCapabilities.unit.test.js
@@ -0,0 +1,83 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLCapabilities } from '../src/WebGLCapabilities.js';
+
+describe('WebGL', () => {
+  describe('WebGLCapabilities', () => {
+    it('should expose a function', () => {
+      expect(WebGLCapabilities).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('isWebGL2', () => {
+      // implement
+    });
+
+    test.todo('drawBuffers', () => {
+      // implement
+    });
+
+    test.todo('getMaxAnisotropy', () => {
+      // implement
+    });
+
+    test.todo('getMaxPrecision', () => {
+      // implement
+    });
+
+    test.todo('precision', () => {
+      // implement
+    });
+
+    test.todo('logarithmicDepthBuffer', () => {
+      // implement
+    });
+
+    test.todo('maxTextures', () => {
+      // implement
+    });
+
+    test.todo('maxVertexTextures', () => {
+      // implement
+    });
+
+    test.todo('maxTextureSize', () => {
+      // implement
+    });
+
+    test.todo('maxCubemapSize', () => {
+      // implement
+    });
+
+    test.todo('maxAttributes', () => {
+      // implement
+    });
+
+    test.todo('maxVertexUniforms', () => {
+      // implement
+    });
+
+    test.todo('maxVaryings', () => {
+      // implement
+    });
+
+    test.todo('maxFragmentUniforms', () => {
+      // implement
+    });
+
+    test.todo('vertexTextures', () => {
+      // implement
+    });
+
+    test.todo('floatFragmentTextures', () => {
+      // implement
+    });
+
+    test.todo('floatVertexTextures', () => {
+      // implement
+    });
+  });
+});
