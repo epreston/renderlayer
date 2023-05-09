7d3fd5bb56696bc7a8a9bb7345e002774a3932e7
Ed Preston <epreston@prestonsoft.com>
2023-05-09 23:36:25 +1000

    Create WebGLBindingStates.unit.test.js

---
+++ packages/webgl/tests/WebGLBindingStates.unit.test.js
@@ -0,0 +1,51 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLBindingStates } from '../src/WebGLBindingStates.js';
+
+describe('WebGL', () => {
+  describe('WebGLBindingStates', () => {
+    it('should expose a function', () => {
+      expect(WebGLBindingStates).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('setup', () => {
+      // implement
+    });
+
+    test.todo('reset', () => {
+      // implement
+    });
+
+    test.todo('resetDefaultState', () => {
+      // implement
+    });
+
+    test.todo('dispose', () => {
+      // implement
+    });
+
+    test.todo('releaseStatesOfGeometry', () => {
+      // implement
+    });
+
+    test.todo('releaseStatesOfProgram', () => {
+      // implement
+    });
+
+    test.todo('initAttributes', () => {
+      // implement
+    });
+
+    test.todo('enableAttribute', () => {
+      // implement
+    });
+
+    test.todo('disableUnusedAttributes', () => {
+      // implement
+    });
+  });
+});
