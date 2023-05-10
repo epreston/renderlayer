de5f6ae355c38cdf674a661dee8e95dca884ff90
Ed Preston <epreston@prestonsoft.com>
2023-05-10 18:23:28 +1000

    Create WebGLRenderer.unit.test.js

---
+++ packages/renderers/tests/WebGLRenderer.unit.test.js
@@ -0,0 +1,255 @@
+// @vitest-environment jsdom
+
+// TODO: prep for browser testing, enable and test in browser manually
+
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLRenderer } from '../src/WebGLRenderer.js';
+
+describe('Renderers', () => {
+  describe.todo('WebGLRenderer', () => {
+    test('Instancing', () => {
+      const renderer = new WebGLRenderer();
+      expect(renderer).toBeDefined();
+    });
+
+    test.todo('domElement', () => {
+      // implement
+    });
+
+    test.todo('debug', () => {
+      // implement
+    });
+
+    test.todo('autoClear', () => {
+      // implement
+    });
+
+    test.todo('autoClearColor', () => {
+      // implement
+    });
+
+    test.todo('autoClearDepth', () => {
+      // implement
+    });
+
+    test.todo('autoClearStencil', () => {
+      // implement
+    });
+
+    test.todo('sortObjects', () => {
+      // implement
+    });
+
+    test.todo('clippingPlanes', () => {
+      // implement
+    });
+
+    test.todo('localClippingEnabled', () => {
+      // implement
+    });
+
+    test.todo('outputColorSpace', () => {
+      // implement
+    });
+
+    test.todo('physicallyCorrectLights', () => {
+      // implement
+    });
+
+    test.todo('toneMapping', () => {
+      // implement
+    });
+
+    test.todo('toneMappingExposure', () => {
+      // implement
+    });
+
+    test.todo('isWebGLRenderer', () => {
+      // implement
+    });
+
+    test.todo('getContext', () => {
+      // implement
+    });
+
+    test.todo('getContextAttributes', () => {
+      // implement
+    });
+
+    test.todo('forceContextLoss', () => {
+      // implement
+    });
+
+    test.todo('forceContextRestore', () => {
+      // implement
+    });
+
+    test.todo('getPixelRatio', () => {
+      // implement
+    });
+
+    test.todo('setPixelRatio', () => {
+      // implement
+    });
+
+    test.todo('getSize', () => {
+      // implement
+    });
+
+    test.todo('setSize', () => {
+      // implement
+    });
+
+    test.todo('getDrawingBufferSize', () => {
+      // implement
+    });
+
+    test.todo('setDrawingBufferSize', () => {
+      // implement
+    });
+
+    test.todo('getCurrentViewport', () => {
+      // implement
+    });
+
+    test.todo('getViewport', () => {
+      // implement
+    });
+
+    test.todo('setViewport', () => {
+      // implement
+    });
+
+    test.todo('getScissor', () => {
+      // implement
+    });
+
+    test.todo('setScissor', () => {
+      // implement
+    });
+
+    test.todo('getScissorTest', () => {
+      // implement
+    });
+
+    test.todo('setScissorTest', () => {
+      // implement
+    });
+
+    test.todo('setOpaqueSort', () => {
+      // implement
+    });
+
+    test.todo('setTransparentSort', () => {
+      // implement
+    });
+
+    test.todo('getClearColor', () => {
+      // implement
+    });
+
+    test.todo('setClearColor', () => {
+      // implement
+    });
+
+    test.todo('getClearAlpha', () => {
+      // implement
+    });
+
+    test.todo('setClearAlpha', () => {
+      // implement
+    });
+
+    test.todo('clear', () => {
+      // implement
+    });
+
+    test.todo('clearColor', () => {
+      // implement
+    });
+
+    test.todo('clearDepth', () => {
+      // implement
+    });
+
+    test.todo('clearStencil', () => {
+      // implement
+    });
+
+    test('dispose', () => {
+      const object = new WebGLRenderer();
+      object.dispose();
+
+      expect(object).toBeDefined();
+    });
+
+    test.todo('renderBufferDirect', () => {
+      // implement
+    });
+
+    test.todo('compile', () => {
+      // implement
+    });
+
+    test.todo('setAnimationLoop', () => {
+      // implement
+    });
+
+    test.todo('render', () => {
+      // implement
+    });
+
+    test.todo('getActiveCubeFace', () => {
+      // implement
+    });
+
+    test.todo('getActiveMipmapLevel', () => {
+      // implement
+    });
+
+    test.todo('getRenderTarget', () => {
+      // implement
+    });
+
+    test.todo('getRenderTarget', () => {
+      // implement
+    });
+
+    test.todo('setRenderTargetTextures', () => {
+      // implement
+    });
+
+    test.todo('setRenderTargetFramebuffer', () => {
+      // implement
+    });
+
+    test.todo('setRenderTarget', () => {
+      // implement
+    });
+
+    test.todo('readRenderTargetPixels', () => {
+      // implement
+    });
+
+    test.todo('copyFramebufferToTexture', () => {
+      // implement
+    });
+
+    test.todo('copyTextureToTexture', () => {
+      // implement
+    });
+
+    test.todo('copyTextureToTexture3D', () => {
+      // implement
+    });
+
+    test.todo('initTexture', () => {
+      // implement
+    });
+
+    test.todo('resetState', () => {
+      // implement
+    });
+  });
+});
