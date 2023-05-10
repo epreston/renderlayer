3d6cee18ecd4aa2b467fa5b8732d51a15f57fd15
Ed Preston <epreston@prestonsoft.com>
2023-05-10 15:36:34 +1000

    Create WebGLState.unit.test.js

---
+++ packages/webgl/tests/WebGLState.unit.test.js
@@ -0,0 +1,139 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLState } from '../src/WebGLState.js';
+
+describe('WebGL', () => {
+  describe('WebGLState', () => {
+    it('should expose a function', () => {
+      expect(WebGLState).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('buffers', () => {
+      // implement
+    });
+
+    test.todo('enable', () => {
+      // implement
+    });
+
+    test.todo('disable', () => {
+      // implement
+    });
+
+    test.todo('bindFramebuffer', () => {
+      // implement
+    });
+
+    test.todo('drawBuffers', () => {
+      // implement
+    });
+
+    test.todo('useProgram', () => {
+      // implement
+    });
+
+    test.todo('setBlending', () => {
+      // implement
+    });
+
+    test.todo('setMaterial', () => {
+      // implement
+    });
+
+    test.todo('setFlipSided', () => {
+      // implement
+    });
+
+    test.todo('setCullFace', () => {
+      // implement
+    });
+
+    test.todo('setLineWidth', () => {
+      // implement
+    });
+
+    test.todo('setPolygonOffset', () => {
+      // implement
+    });
+
+    test.todo('setScissorTest', () => {
+      // implement
+    });
+
+    test.todo('activeTexture', () => {
+      // implement
+    });
+
+    test.todo('bindTexture', () => {
+      // implement
+    });
+
+    test.todo('unbindTexture', () => {
+      // implement
+    });
+
+    test.todo('compressedTexImage2D', () => {
+      // implement
+    });
+
+    test.todo('compressedTexImage3D', () => {
+      // implement
+    });
+
+    test.todo('texImage2D', () => {
+      // implement
+    });
+
+    test.todo('texImage3D', () => {
+      // implement
+    });
+
+    test.todo('updateUBOMapping', () => {
+      // implement
+    });
+
+    test.todo('uniformBlockBinding', () => {
+      // implement
+    });
+
+    test.todo('texStorage2D', () => {
+      // implement
+    });
+
+    test.todo('texStorage3D', () => {
+      // implement
+    });
+
+    test.todo('texSubImage2D', () => {
+      // implement
+    });
+
+    test.todo('texSubImage3D', () => {
+      // implement
+    });
+
+    test.todo('compressedTexSubImage2D', () => {
+      // implement
+    });
+
+    test.todo('compressedTexSubImage3D', () => {
+      // implement
+    });
+
+    test.todo('scissor', () => {
+      // implement
+    });
+
+    test.todo('viewport', () => {
+      // implement
+    });
+
+    test.todo('reset', () => {
+      // implement
+    });
+  });
+});
