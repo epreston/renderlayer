a85920900e8a43b533bb3802a2bdb23261b18bc1
Ed Preston <epreston@prestonsoft.com>
2023-05-10 15:59:47 +1000

    Create index.js

---
+++ packages/webgl/src/index.js
@@ -0,0 +1,30 @@
+export * from './WebGLAnimation.js';
+export * from './WebGLAttributes.js';
+export * from './WebGLBackground.js';
+export * from './WebGLBindingStates.js';
+export * from './WebGLBufferRenderer.js';
+export * from './WebGLCapabilities.js';
+export * from './WebGLClipping.js';
+export * from './WebGLCubeMaps.js';
+// export * from './WebGLCubeUVMaps.js'; // excluded from build
+export * from './WebGLExtensions.js';
+export * from './WebGLGeometries.js';
+export * from './WebGLIndexedBufferRenderer.js';
+export * from './WebGLInfo.js';
+// export * from './WebGLLights.js'; // not exported
+export * from './WebGLMaterials.js';
+export * from './WebGLMorphtargets.js';
+export * from './WebGLObjects.js';
+// export * from './WebGLProgram.js'; // not exported
+export * from './WebGLPrograms.js';
+export * from './WebGLProperties.js';
+export * from './WebGLRenderLists.js';
+export * from './WebGLRenderStates.js';
+// export * from './WebGLShader.js'; // not exported
+// export * from './WebGLShaderCache.js'; // not exported
+export * from './WebGLShadowMap.js';
+export * from './WebGLState.js';
+export * from './WebGLTextures.js';
+export * from './WebGLUniforms.js';
+export * from './WebGLUniformsGroups.js';
+export * from './WebGLUtils.js';
