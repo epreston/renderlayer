9c5bd05c64209a99de806e1b8fe13275858b7c20
Ed Preston <epreston@prestonsoft.com>
2023-05-10 18:48:33 +1000

    Create loaders.esm-bundler.js
    0.0.1

---
+++ packages/loaders/dist/loaders.esm-bundler.js
@@ -0,0 +1,117 @@
+class LoadingManager {
+  constructor(onLoad, onProgress, onError) {
+    const scope = this;
+    let isLoading = false;
+    let itemsLoaded = 0;
+    let itemsTotal = 0;
+    let urlModifier = void 0;
+    const handlers = [];
+    this.onStart = void 0;
+    this.onLoad = onLoad;
+    this.onProgress = onProgress;
+    this.onError = onError;
+    this.itemStart = function(url) {
+      itemsTotal++;
+      if (isLoading === false) {
+        if (scope.onStart !== void 0) {
+          scope.onStart(url, itemsLoaded, itemsTotal);
+        }
+      }
+      isLoading = true;
+    };
+    this.itemEnd = function(url) {
+      itemsLoaded++;
+      if (scope.onProgress !== void 0) {
+        scope.onProgress(url, itemsLoaded, itemsTotal);
+      }
+      if (itemsLoaded === itemsTotal) {
+        isLoading = false;
+        if (scope.onLoad !== void 0) {
+          scope.onLoad();
+        }
+      }
+    };
+    this.itemError = function(url) {
+      if (scope.onError !== void 0) {
+        scope.onError(url);
+      }
+    };
+    this.resolveURL = function(url) {
+      if (urlModifier) {
+        return urlModifier(url);
+      }
+      return url;
+    };
+    this.setURLModifier = function(transform) {
+      urlModifier = transform;
+      return this;
+    };
+    this.addHandler = function(regex, loader) {
+      handlers.push(regex, loader);
+      return this;
+    };
+    this.removeHandler = function(regex) {
+      const index = handlers.indexOf(regex);
+      if (index !== -1) {
+        handlers.splice(index, 2);
+      }
+      return this;
+    };
+    this.getHandler = function(file) {
+      for (let i = 0, l = handlers.length; i < l; i += 2) {
+        const regex = handlers[i];
+        const loader = handlers[i + 1];
+        if (regex.global)
+          regex.lastIndex = 0;
+        if (regex.test(file)) {
+          return loader;
+        }
+      }
+      return null;
+    };
+  }
+}
+const DefaultLoadingManager = /* @__PURE__ */ new LoadingManager();
+
+class Loader {
+  constructor(manager) {
+    this.manager = manager !== void 0 ? manager : DefaultLoadingManager;
+    this.crossOrigin = "anonymous";
+    this.withCredentials = false;
+    this.path = "";
+    this.resourcePath = "";
+    this.requestHeader = {};
+  }
+  load() {
+  }
+  loadAsync(url, onProgress) {
+    const scope = this;
+    return new Promise(function(resolve, reject) {
+      scope.load(url, resolve, onProgress, reject);
+    });
+  }
+  parse() {
+  }
+  setCrossOrigin(crossOrigin) {
+    this.crossOrigin = crossOrigin;
+    return this;
+  }
+  setWithCredentials(value) {
+    this.withCredentials = value;
+    return this;
+  }
+  setPath(path) {
+    this.path = path;
+    return this;
+  }
+  setResourcePath(resourcePath) {
+    this.resourcePath = resourcePath;
+    return this;
+  }
+  setRequestHeader(requestHeader) {
+    this.requestHeader = requestHeader;
+    return this;
+  }
+}
+
+export { DefaultLoadingManager, Loader, LoadingManager };
