6b1d083529fe254b3c697300969bfbbd66cebdb2
Ed Preston <epreston@prestonsoft.com>
2023-05-10 13:47:00 +1000

    Create WebGLMorphtargets.unit.test.js

---
+++ packages/webgl/tests/WebGLMorphtargets.unit.test.js
@@ -0,0 +1,19 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLMorphtargets } from '../src/WebGLMorphtargets.js';
+
+describe('WebGL', () => {
+  describe('WebGLMorphtargets', () => {
+    it('should expose a function', () => {
+      expect(WebGLMorphtargets).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('update', () => {
+      // implement
+    });
+  });
+});
