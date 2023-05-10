e5e75d132a42130dbcdde2e0aae395b83dbf676e
Ed Preston <epreston@prestonsoft.com>
2023-05-10 14:47:53 +1000

    Create WebGLTextures.unit.test.js

---
+++ packages/webgl/tests/WebGLTextures.unit.test.js
@@ -0,0 +1,67 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLTextures } from '../src/WebGLTextures.js';
+
+describe('WebGL', () => {
+  describe('WebGLTextures', () => {
+    it('should expose a function', () => {
+      expect(WebGLTextures).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('allocateTextureUnit', () => {
+      // implement
+    });
+
+    test.todo('resetTextureUnits', () => {
+      // implement
+    });
+
+    test.todo('setTexture2D', () => {
+      // implement
+    });
+
+    test.todo('setTexture2DArray', () => {
+      // implement
+    });
+
+    test.todo('setTexture3D', () => {
+      // implement
+    });
+
+    test.todo('setTextureCube', () => {
+      // implement
+    });
+
+    test.todo('rebindTextures', () => {
+      // implement
+    });
+
+    test.todo('setupRenderTarget', () => {
+      // implement
+    });
+
+    test.todo('updateRenderTargetMipmap', () => {
+      // implement
+    });
+
+    test.todo('updateMultisampleRenderTarget', () => {
+      // implement
+    });
+
+    test.todo('setupDepthRenderbuffer', () => {
+      // implement
+    });
+
+    test.todo('setupFrameBufferTexture', () => {
+      // implement
+    });
+
+    test.todo('useMultisampledRTT', () => {
+      // implement
+    });
+  });
+});
