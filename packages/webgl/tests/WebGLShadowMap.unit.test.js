b262507ceb490a1e4b267d30639931ea3a0c02c3
Ed Preston <epreston@prestonsoft.com>
2023-05-10 15:51:12 +1000

    Create WebGLShadowMap.unit.test.js

---
+++ packages/webgl/tests/WebGLShadowMap.unit.test.js
@@ -0,0 +1,19 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLShadowMap } from '../src/WebGLShadowMap.js';
+
+describe('WebGL', () => {
+  describe('WebGLShadowMap', () => {
+    it('should expose a function', () => {
+      expect(WebGLShadowMap).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('render', () => {
+      // implement
+    });
+  });
+});
