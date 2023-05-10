a6adf2474298315e805e24b4ee18a64ded536f50
Ed Preston <epreston@prestonsoft.com>
2023-05-10 13:45:04 +1000

    Create WebGLMaterials.unit.test.js

---
+++ packages/webgl/tests/WebGLMaterials.unit.test.js
@@ -0,0 +1,27 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+// import { BackSide } from '@renderlayer/shared';
+// import { getUnlitUniformColorSpace } from '@renderlayer/shaders';
+
+import { WebGLMaterials } from '../src/WebGLMaterials.js';
+
+// vi.mock('@renderlayer/shared');
+// vi.mock('@renderlayer/shaders');
+
+describe('WebGLMaterials', () => {
+  it('should expose a function', () => {
+		expect(WebGLMaterials).toBeDefined();
+	});
+
+  test.todo('Instancing', () => {
+    // implement
+  });
+
+  test.todo('refreshFogUniforms', () => {
+    // implement
+  });
+
+  test.todo('refreshMaterialUniforms', () => {
+    // implement
+  });
+});
