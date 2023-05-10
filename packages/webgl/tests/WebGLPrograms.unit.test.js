f95f2b53aaf3cc6a1cdba551717760a6213af079
Ed Preston <epreston@prestonsoft.com>
2023-05-10 15:16:45 +1000

    Create WebGLPrograms.unit.test.js

---
+++ packages/webgl/tests/WebGLPrograms.unit.test.js
@@ -0,0 +1,47 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLPrograms } from '../src/WebGLPrograms.js';
+
+describe('WebGL', () => {
+  describe('WebGLPrograms', () => {
+    it('should expose a function', () => {
+      expect(WebGLPrograms).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('getParameters', () => {
+      // implement
+    });
+
+    test.todo('getProgramCacheKey', () => {
+      // implement
+    });
+
+    test.todo('getUniforms', () => {
+      // implement
+    });
+
+    test.todo('acquireProgram', () => {
+      // implement
+    });
+
+    test.todo('releaseProgram', () => {
+      // implement
+    });
+
+    test.todo('releaseShaderCache', () => {
+      // implement
+    });
+
+    test.todo('programs', () => {
+      // implement
+    });
+
+    test.todo('dispose', () => {
+      // implement
+    });
+  });
+});
