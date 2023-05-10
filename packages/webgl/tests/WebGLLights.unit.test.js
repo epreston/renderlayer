813a165bb71c5c1f87cc7f5f821559cda0621910
Ed Preston <epreston@prestonsoft.com>
2023-05-10 13:18:14 +1000

    Create WebGLLights.unit.test.js

---
+++ packages/webgl/tests/WebGLLights.unit.test.js
@@ -0,0 +1,27 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLLights } from '../src/WebGLLights.js';
+
+describe('WebGL', () => {
+  describe('WebGLLights', () => {
+    it('should expose a function', () => {
+      expect(WebGLLights).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('setup', () => {
+      // implement
+    });
+
+    test.todo('setupView', () => {
+      // implement
+    });
+
+    test.todo('state', () => {
+      // implement
+    });
+  });
+});
