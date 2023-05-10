9052902beb6ae19a0da43cfeaee451ca56cbccb3
Ed Preston <epreston@prestonsoft.com>
2023-05-10 13:55:24 +1000

    Create WebGLUniforms.unit.test.js

---
+++ packages/webgl/tests/WebGLUniforms.unit.test.js
@@ -0,0 +1,31 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLUniforms } from '../src/WebGLUniforms.js';
+
+describe('WebGL', () => {
+  describe('WebGLUniforms', () => {
+    it('should expose a function', () => {
+      expect(WebGLUniforms).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('setValue', () => {
+      // implement
+    });
+
+    test.todo('setOptional', () => {
+      // implement
+    });
+
+    test.todo('upload', () => {
+      // implement
+    });
+
+    test.todo('seqWithValue', () => {
+      // implement
+    });
+  });
+});
