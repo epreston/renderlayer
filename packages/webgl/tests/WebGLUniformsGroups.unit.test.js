bb044617d799f07550d625156a83a970113ae014
Ed Preston <epreston@prestonsoft.com>
2023-05-10 14:00:58 +1000

    Create WebGLUniformsGroups.unit.test.js

---
+++ packages/webgl/tests/WebGLUniformsGroups.unit.test.js
@@ -0,0 +1,31 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { WebGLUniformsGroups } from '../src/WebGLUniformsGroups.js';
+
+describe('WebGL', () => {
+  describe('WebGLUniformsGroups', () => {
+    it('should expose a function', () => {
+      expect(WebGLUniformsGroups).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('setValue', () => {
+      // implement
+    });
+
+    test.todo('setOptional', () => {
+      // implement
+    });
+
+    test.todo('upload', () => {
+      // implement
+    });
+
+    test.todo('seqWithValue', () => {
+      // implement
+    });
+  });
+});
