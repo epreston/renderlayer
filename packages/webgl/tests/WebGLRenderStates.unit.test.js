0afed78cc30e6546816488233c36a3860e21bcde
Ed Preston <epreston@prestonsoft.com>
2023-05-10 14:14:11 +1000

    Create WebGLRenderStates.unit.test.js

---
+++ packages/webgl/tests/WebGLRenderStates.unit.test.js
@@ -0,0 +1,61 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+// import { WebGLLights } from '../src/WebGLLights.js';
+
+import { WebGLRenderStates, WebGLRenderState } from '../src/WebGLRenderStates.js';
+
+// vi.mock('./WebGLLights.js');
+
+describe('WebGL', () => {
+  describe('WebGLRenderState', () => {
+    it('should expose a function', () => {
+      expect(WebGLRenderState).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('init', () => {
+      // implement
+    });
+
+    test.todo('state', () => {
+      // implement
+    });
+
+    test.todo('setupLights', () => {
+      // implement
+    });
+
+    test.todo('setupLightsView', () => {
+      // implement
+    });
+
+    test.todo('pushLight', () => {
+      // implement
+    });
+
+    test.todo('pushShadow', () => {
+      // implement
+    });
+  });
+
+  describe('WebGLRenderStates', () => {
+    it('should expose a function', () => {
+      expect(WebGLRenderStates).toBeDefined();
+    });
+
+    test.todo('Instancing', () => {
+      // implement
+    });
+
+    test.todo('get', () => {
+      // implement
+    });
+
+    test.todo('dispose', () => {
+      // implement
+    });
+  });
+});
