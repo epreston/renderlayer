be99bcc2b9fe8fd4639a4d9b693462eeb8c5a152
Ed Preston <epreston@prestonsoft.com>
2023-05-17 22:14:00 +1000

    Update core.esm-bundler.js
    0.0.2

--- packages/core/dist/core.esm-bundler.js
+++ packages/core/dist/core.esm-bundler.js
@@ -1,4 +1,4 @@
-import { generateUUID, Vector3, Euler, Quaternion, Matrix4, Matrix3 } from '@renderlayer/math';
+import { generateUUID, Vector3, Euler, Quaternion, Matrix4, Matrix3, Ray } from '@renderlayer/math';
 
 class EventDispatcher {
   addEventListener(type, listener) {
@@ -615,4 +615,63 @@ Object3D.DEFAULT_UP = /* @__PURE__ */ new Vector3(0, 1, 0);
 Object3D.DEFAULT_MATRIX_AUTO_UPDATE = true;
 Object3D.DEFAULT_MATRIX_WORLD_AUTO_UPDATE = true;
 
-export { EventDispatcher, Layers, Object3D };
+class Raycaster {
+  constructor(origin, direction, near = 0, far = Infinity) {
+    this.ray = new Ray(origin, direction);
+    this.near = near;
+    this.far = far;
+    this.camera = null;
+    this.layers = new Layers();
+    this.params = {
+      Mesh: {},
+      Line: { threshold: 1 },
+      LOD: {},
+      Points: { threshold: 1 },
+      Sprite: {}
+    };
+  }
+  set(origin, direction) {
+    this.ray.set(origin, direction);
+  }
+  setFromCamera(coords, camera) {
+    if (camera.isPerspectiveCamera) {
+      this.ray.origin.setFromMatrixPosition(camera.matrixWorld);
+      this.ray.direction.set(coords.x, coords.y, 0.5).unproject(camera).sub(this.ray.origin).normalize();
+      this.camera = camera;
+    } else if (camera.isOrthographicCamera) {
+      this.ray.origin.set(coords.x, coords.y, (camera.near + camera.far) / (camera.near - camera.far)).unproject(camera);
+      this.ray.direction.set(0, 0, -1).transformDirection(camera.matrixWorld);
+      this.camera = camera;
+    } else {
+      console.error("Raycaster: Unsupported camera type: " + camera.type);
+    }
+  }
+  intersectObject(object, recursive = true, intersects = []) {
+    intersectObject(object, this, intersects, recursive);
+    intersects.sort(ascSort);
+    return intersects;
+  }
+  intersectObjects(objects, recursive = true, intersects = []) {
+    for (let i = 0, l = objects.length; i < l; i++) {
+      intersectObject(objects[i], this, intersects, recursive);
+    }
+    intersects.sort(ascSort);
+    return intersects;
+  }
+}
+function ascSort(a, b) {
+  return a.distance - b.distance;
+}
+function intersectObject(object, raycaster, intersects, recursive) {
+  if (object.layers.test(raycaster.layers)) {
+    object.raycast(raycaster, intersects);
+  }
+  if (recursive === true) {
+    const children = object.children;
+    for (let i = 0, l = children.length; i < l; i++) {
+      intersectObject(children[i], raycaster, intersects, true);
+    }
+  }
+}
+
+export { EventDispatcher, Layers, Object3D, Raycaster };
