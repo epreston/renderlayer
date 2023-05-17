e95ca20ff98b5182f37e018fb9cc27eb0239e32a
Ed Preston <epreston@prestonsoft.com>
2023-05-18 00:03:43 +1000

    Update index.js

--- packages/loaders/src/index.js
+++ packages/loaders/src/index.js
@@ -1,3 +1,5 @@
+export * from './Cache.js';
+export * from './FileLoader.js';
 export * from './Loader.js';
 export * from './LoadingManager.js';
 // export * from './ObjectLoader.js'; // execlude from build
