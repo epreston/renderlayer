fb85e33baf9863bd3be9f41e7cebf201f6e5b0d1
Ed Preston <epreston@prestonsoft.com>
2023-05-17 22:13:23 +1000

    Update index.js
    Add raycaster to core for now

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,4 @@
-// export * from './Clock.js';
 export * from './EventDispatcher.js';
 export * from './Layers.js';
 export * from './Object3D.js';
-// export * from './Raycaster.js';
+export * from './Raycaster.js';
