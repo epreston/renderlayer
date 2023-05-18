f638e3ccdfb43e3618ce5d797f9bc40f3958cf46
Ed Preston <epreston@prestonsoft.com>
2023-05-18 13:15:21 +1000

    Update index.js
    add LoaderUtils to build

--- packages/loaders/src/index.js
+++ packages/loaders/src/index.js
@@ -1,5 +1,6 @@
 export * from './Cache.js';
 export * from './FileLoader.js';
 export * from './Loader.js';
+export * from './LoaderUtils.js';
 export * from './LoadingManager.js';
 // export * from './ObjectLoader.js'; // execlude from build
