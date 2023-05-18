00473b842e766699e690c926caef0e0f133f9383
Ed Preston <epreston@prestonsoft.com>
2023-05-18 18:53:04 +1000

    Create LineLoop.js

---
+++ packages/objects/src/LineLoop.js
@@ -0,0 +1,12 @@
+import { Line } from './Line.js';
+
+class LineLoop extends Line {
+  constructor(geometry, material) {
+    super(geometry, material);
+
+    this.isLineLoop = true;
+    this.type = 'LineLoop';
+  }
+}
+
+export { LineLoop };
