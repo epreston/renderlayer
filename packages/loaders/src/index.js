aa293683febc0844e4384155b08d57fd429c4336
Ed Preston <epreston@prestonsoft.com>
2023-05-18 15:06:05 +1000

    Update index.js
    add loaders to build

--- packages/loaders/src/index.js
+++ packages/loaders/src/index.js
@@ -1,6 +1,9 @@
 export * from './Cache.js';
 export * from './FileLoader.js';
+export * from './ImageBitmapLoader.js';
+export * from './ImageLoader.js';
 export * from './Loader.js';
 export * from './LoaderUtils.js';
 export * from './LoadingManager.js';
 // export * from './ObjectLoader.js'; // execlude from build
+export * from './TextureLoader.js';
