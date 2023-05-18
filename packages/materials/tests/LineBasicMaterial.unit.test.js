fca59eabb7c71ea5fb628673c91f571d3ff7e6e3
Ed Preston <epreston@prestonsoft.com>
2023-05-18 18:17:38 +1000

    Create LineBasicMaterial.unit.test.js

---
+++ packages/materials/tests/LineBasicMaterial.unit.test.js
@@ -0,0 +1,52 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { Material } from '../src/Material.js';
+import { LineBasicMaterial } from '../src/LineBasicMaterial.js';
+
+describe('Materials', () => {
+  describe('LineBasicMaterial', () => {
+    test('Instancing', () => {
+      const object = new LineBasicMaterial();
+      expect(object).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new LineBasicMaterial();
+      expect(object).toBeInstanceOf(Material);
+    });
+
+    test('type', () => {
+      const object = new LineBasicMaterial();
+      expect(object.type).toBe('LineBasicMaterial');
+    });
+
+    test.todo('color', () => {
+      // implement
+    });
+
+    test.todo('linewidth', () => {
+      // implement
+    });
+
+    test.todo('linecap', () => {
+      // implement
+    });
+
+    test.todo('linejoin', () => {
+      // implement
+    });
+
+    test.todo('fog', () => {
+      // implement
+    });
+
+    test('isLineBasicMaterial', () => {
+      const object = new LineBasicMaterial();
+      expect(object.isLineBasicMaterial).toBeTruthy();
+    });
+
+    test.todo('copy', () => {
+      // implement
+    });
+  });
+});
