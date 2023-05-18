b80659289466f7a8a14fbb83dfa9a6ff8868d52e
Ed Preston <epreston@prestonsoft.com>
2023-05-18 18:58:14 +1000

    Create DataTexture.unit.test.js

---
+++ packages/textures/tests/DataTexture.unit.test.js
@@ -0,0 +1,39 @@
+import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+
+import { Texture } from '../src/Texture.js';
+import { DataTexture } from '../src/DataTexture.js';
+
+describe('Textures', () => {
+  describe('DataTexture', () => {
+    test('Instancing', () => {
+      const object = new DataTexture();
+      expect(object).toBeDefined();
+    });
+
+    test('Extending', () => {
+      const object = new DataTexture();
+      expect(object).toBeInstanceOf(Texture);
+    });
+
+    test.todo('image', () => {
+      // implement
+    });
+
+    test.todo('generateMipmaps', () => {
+      // implement
+    });
+
+    test.todo('flipY', () => {
+      // implement
+    });
+
+    test.todo('unpackAlignment', () => {
+      // implement
+    });
+
+    test('isDataTexture', () => {
+      const object = new DataTexture();
+      expect(object.isDataTexture).toBeTruthy();
+    });
+  });
+});
