bccfe5b243f6d670041d3f2bd9446ed9509a5595
Ed Preston <epreston@prestonsoft.com>
2023-05-18 18:58:57 +1000

    Update index.js
    add data texture to build

--- packages/textures/src/index.js
+++ packages/textures/src/index.js
@@ -1,5 +1,6 @@
 export * from './CubeTexture.js';
 export * from './Data3DTexture.js';
 export * from './DataArrayTexture.js';
+export * from './DataTexture.js';
 export * from './Source.js';
 export * from './Texture.js';
