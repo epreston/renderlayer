a401b1d8dd5a404b18c775d34ca21b2c5d1a8dfe
Ed Preston <epreston@prestonsoft.com>
2023-05-18 17:59:51 +1000

    Create SpotLightShadow.js

---
+++ packages/lights/src/SpotLightShadow.js
@@ -0,0 +1,40 @@
+import { PerspectiveCamera } from '@renderlayer/cameras';
+import { RAD2DEG } from '@renderlayer/math';
+import { LightShadow } from './LightShadow.js';
+
+class SpotLightShadow extends LightShadow {
+  constructor() {
+    super(new PerspectiveCamera(50, 1, 0.5, 500));
+
+    this.isSpotLightShadow = true;
+
+    this.focus = 1;
+  }
+
+  updateMatrices(light) {
+    const camera = this.camera;
+
+    const fov = RAD2DEG * 2 * light.angle * this.focus;
+    const aspect = this.mapSize.width / this.mapSize.height;
+    const far = light.distance || camera.far;
+
+    if (fov !== camera.fov || aspect !== camera.aspect || far !== camera.far) {
+      camera.fov = fov;
+      camera.aspect = aspect;
+      camera.far = far;
+      camera.updateProjectionMatrix();
+    }
+
+    super.updateMatrices(light);
+  }
+
+  copy(source) {
+    super.copy(source);
+
+    this.focus = source.focus;
+
+    return this;
+  }
+}
+
+export { SpotLightShadow };
