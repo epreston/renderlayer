5415f85e68c610129bbf6b856fdbc480fabc5fc4
Ed Preston <epreston@prestonsoft.com>
2023-05-18 18:03:26 +1000

    Update lights.esm-bundler.js
    0.0.2

--- packages/lights/dist/lights.esm-bundler.js
+++ packages/lights/dist/lights.esm-bundler.js
@@ -1,6 +1,6 @@
-import { Color, Vector2, Matrix4, Frustum, Vector4, Vector3 } from '@renderlayer/math';
+import { Color, Vector2, Matrix4, Frustum, Vector4, Vector3, RAD2DEG } from '@renderlayer/math';
 import { Object3D } from '@renderlayer/core';
-import { OrthographicCamera } from '@renderlayer/cameras';
+import { OrthographicCamera, PerspectiveCamera } from '@renderlayer/cameras';
 
 class Light extends Object3D {
   constructor(color, intensity = 1) {
@@ -46,9 +46,9 @@ class AmbientLight extends Light {
   }
 }
 
-const _projScreenMatrix = /* @__PURE__ */ new Matrix4();
-const _lightPositionWorld = /* @__PURE__ */ new Vector3();
-const _lookTarget = /* @__PURE__ */ new Vector3();
+const _projScreenMatrix$1 = /* @__PURE__ */ new Matrix4();
+const _lightPositionWorld$1 = /* @__PURE__ */ new Vector3();
+const _lookTarget$1 = /* @__PURE__ */ new Vector3();
 class LightShadow {
   constructor(camera) {
     this.camera = camera;
@@ -76,13 +76,13 @@ class LightShadow {
   updateMatrices(light) {
     const shadowCamera = this.camera;
     const shadowMatrix = this.matrix;
-    _lightPositionWorld.setFromMatrixPosition(light.matrixWorld);
-    shadowCamera.position.copy(_lightPositionWorld);
-    _lookTarget.setFromMatrixPosition(light.target.matrixWorld);
-    shadowCamera.lookAt(_lookTarget);
+    _lightPositionWorld$1.setFromMatrixPosition(light.matrixWorld);
+    shadowCamera.position.copy(_lightPositionWorld$1);
+    _lookTarget$1.setFromMatrixPosition(light.target.matrixWorld);
+    shadowCamera.lookAt(_lookTarget$1);
     shadowCamera.updateMatrixWorld();
-    _projScreenMatrix.multiplyMatrices(shadowCamera.projectionMatrix, shadowCamera.matrixWorldInverse);
-    this._frustum.setFromProjectionMatrix(_projScreenMatrix);
+    _projScreenMatrix$1.multiplyMatrices(shadowCamera.projectionMatrix, shadowCamera.matrixWorldInverse);
+    this._frustum.setFromProjectionMatrix(_projScreenMatrix$1);
     shadowMatrix.set(
       0.5,
       0,
@@ -101,7 +101,7 @@ class LightShadow {
       0,
       1
     );
-    shadowMatrix.multiply(_projScreenMatrix);
+    shadowMatrix.multiply(_projScreenMatrix$1);
   }
   getViewport(viewportIndex) {
     return this._viewports[viewportIndex];
@@ -171,4 +171,166 @@ class DirectionalLight extends Light {
   }
 }
 
-export { AmbientLight, DirectionalLight, DirectionalLightShadow, Light, LightShadow };
+const _projScreenMatrix = /* @__PURE__ */ new Matrix4();
+const _lightPositionWorld = /* @__PURE__ */ new Vector3();
+const _lookTarget = /* @__PURE__ */ new Vector3();
+class PointLightShadow extends LightShadow {
+  constructor() {
+    super(new PerspectiveCamera(90, 1, 0.5, 500));
+    this.isPointLightShadow = true;
+    this._frameExtents = new Vector2(4, 2);
+    this._viewportCount = 6;
+    this._viewports = [
+      // These viewports map a cube-map onto a 2D texture with the
+      // following orientation:
+      //
+      //  xzXZ
+      //   y Y
+      //
+      // X - Positive x direction
+      // x - Negative x direction
+      // Y - Positive y direction
+      // y - Negative y direction
+      // Z - Positive z direction
+      // z - Negative z direction
+      // positive X
+      new Vector4(2, 1, 1, 1),
+      // negative X
+      new Vector4(0, 1, 1, 1),
+      // positive Z
+      new Vector4(3, 1, 1, 1),
+      // negative Z
+      new Vector4(1, 1, 1, 1),
+      // positive Y
+      new Vector4(3, 0, 1, 1),
+      // negative Y
+      new Vector4(1, 0, 1, 1)
+    ];
+    this._cubeDirections = [
+      new Vector3(1, 0, 0),
+      new Vector3(-1, 0, 0),
+      new Vector3(0, 0, 1),
+      new Vector3(0, 0, -1),
+      new Vector3(0, 1, 0),
+      new Vector3(0, -1, 0)
+    ];
+    this._cubeUps = [
+      new Vector3(0, 1, 0),
+      new Vector3(0, 1, 0),
+      new Vector3(0, 1, 0),
+      new Vector3(0, 1, 0),
+      new Vector3(0, 0, 1),
+      new Vector3(0, 0, -1)
+    ];
+  }
+  updateMatrices(light, viewportIndex = 0) {
+    const camera = this.camera;
+    const shadowMatrix = this.matrix;
+    const far = light.distance || camera.far;
+    if (far !== camera.far) {
+      camera.far = far;
+      camera.updateProjectionMatrix();
+    }
+    _lightPositionWorld.setFromMatrixPosition(light.matrixWorld);
+    camera.position.copy(_lightPositionWorld);
+    _lookTarget.copy(camera.position);
+    _lookTarget.add(this._cubeDirections[viewportIndex]);
+    camera.up.copy(this._cubeUps[viewportIndex]);
+    camera.lookAt(_lookTarget);
+    camera.updateMatrixWorld();
+    shadowMatrix.makeTranslation(-_lightPositionWorld.x, -_lightPositionWorld.y, -_lightPositionWorld.z);
+    _projScreenMatrix.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse);
+    this._frustum.setFromProjectionMatrix(_projScreenMatrix);
+  }
+}
+
+class PointLight extends Light {
+  constructor(color, intensity, distance = 0, decay = 2) {
+    super(color, intensity);
+    this.isPointLight = true;
+    this.type = "PointLight";
+    this.distance = distance;
+    this.decay = decay;
+    this.shadow = new PointLightShadow();
+  }
+  get power() {
+    return this.intensity * 4 * Math.PI;
+  }
+  set power(power) {
+    this.intensity = power / (4 * Math.PI);
+  }
+  dispose() {
+    this.shadow.dispose();
+  }
+  copy(source, recursive) {
+    super.copy(source, recursive);
+    this.distance = source.distance;
+    this.decay = source.decay;
+    this.shadow = source.shadow.clone();
+    return this;
+  }
+}
+
+class SpotLightShadow extends LightShadow {
+  constructor() {
+    super(new PerspectiveCamera(50, 1, 0.5, 500));
+    this.isSpotLightShadow = true;
+    this.focus = 1;
+  }
+  updateMatrices(light) {
+    const camera = this.camera;
+    const fov = RAD2DEG * 2 * light.angle * this.focus;
+    const aspect = this.mapSize.width / this.mapSize.height;
+    const far = light.distance || camera.far;
+    if (fov !== camera.fov || aspect !== camera.aspect || far !== camera.far) {
+      camera.fov = fov;
+      camera.aspect = aspect;
+      camera.far = far;
+      camera.updateProjectionMatrix();
+    }
+    super.updateMatrices(light);
+  }
+  copy(source) {
+    super.copy(source);
+    this.focus = source.focus;
+    return this;
+  }
+}
+
+class SpotLight extends Light {
+  constructor(color, intensity, distance = 0, angle = Math.PI / 3, penumbra = 0, decay = 2) {
+    super(color, intensity);
+    this.isSpotLight = true;
+    this.type = "SpotLight";
+    this.position.copy(Object3D.DEFAULT_UP);
+    this.updateMatrix();
+    this.target = new Object3D();
+    this.distance = distance;
+    this.angle = angle;
+    this.penumbra = penumbra;
+    this.decay = decay;
+    this.map = null;
+    this.shadow = new SpotLightShadow();
+  }
+  get power() {
+    return this.intensity * Math.PI;
+  }
+  set power(power) {
+    this.intensity = power / Math.PI;
+  }
+  dispose() {
+    this.shadow.dispose();
+  }
+  copy(source, recursive) {
+    super.copy(source, recursive);
+    this.distance = source.distance;
+    this.angle = source.angle;
+    this.penumbra = source.penumbra;
+    this.decay = source.decay;
+    this.target = source.target.clone();
+    this.shadow = source.shadow.clone();
+    return this;
+  }
+}
+
+export { AmbientLight, DirectionalLight, DirectionalLightShadow, Light, LightShadow, PointLight, PointLightShadow, SpotLight, SpotLightShadow };
