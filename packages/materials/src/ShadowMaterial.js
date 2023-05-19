abda9ebad75396df2e3c78cc8a7e51813471450f
Ed Preston <epreston@prestonsoft.com>
2023-05-19 14:13:12 +1000

    Create ShadowMaterial.js

---
+++ packages/materials/src/ShadowMaterial.js
@@ -0,0 +1,30 @@
+import { Color } from '@renderlayer/math';
+import { Material } from './Material.js';
+
+class ShadowMaterial extends Material {
+  constructor(parameters) {
+    super();
+
+    this.isShadowMaterial = true;
+    this.type = 'ShadowMaterial';
+
+    this.color = new Color(0x000000);
+    this.transparent = true;
+
+    this.fog = true;
+
+    this.setValues(parameters);
+  }
+
+  copy(source) {
+    super.copy(source);
+
+    this.color.copy(source.color);
+
+    this.fog = source.fog;
+
+    return this;
+  }
+}
+
+export { ShadowMaterial };
