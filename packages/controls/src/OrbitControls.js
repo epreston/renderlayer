bc1cec2cf95120c277a60396e2cb0cff5074b05e
Ed Preston <epreston@prestonsoft.com>
2023-08-07 07:58:43 +1000

    Update OrbitControls.js
    correct import

--- packages/controls/src/OrbitControls.js
+++ packages/controls/src/OrbitControls.js
@@ -1,6 +1,6 @@
 import { EventDispatcher } from '@renderlayer/core';
 import { Quaternion, Vector2, Vector3 } from '@renderlayer/math';
-import { Spherical } from '../src/Spherical.js';
+import { Spherical } from './Spherical.js';
 
 export const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
 export const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
