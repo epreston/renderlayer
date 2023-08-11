b53cd2dd805fec2279b923aa2f3a0c6820351f30
Ed Preston <epreston@prestonsoft.com>
2023-08-12 04:57:42 +1000

    Correct build time globals

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,16 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __DEV__: true,
+    __TEST__: true,
+    __VERSION__: '"test"',
+    __BROWSER__: false,
+    __GLOBAL__: false,
+    __ESM_BUNDLER__: true,
+    __ESM_BROWSER__: false,
+    __NODE_JS__: true
+  },
   plugins: [visualizer()],
   build: {
     target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4'],
