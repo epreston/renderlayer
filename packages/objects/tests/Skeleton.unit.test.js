ba43dd3dbafb20ad357a369da965ed2dd9f8009b
Ed Preston <epreston@prestonsoft.com>
2023-08-14 10:06:44 +1000

    Update Skeleton.unit.test.js
    add unit test

--- packages/objects/tests/Skeleton.unit.test.js
+++ packages/objects/tests/Skeleton.unit.test.js
@@ -9,8 +9,10 @@ describe('Objects', () => {
       expect(object).toBeDefined();
     });
 
-    test.todo('uuid', () => {
-      // implement
+    test('uuid', () => {
+      const object = new Skeleton();
+      expect(object.uuid).to.be.a('string');
+      expect(object.uuid).to.have.length(36);
     });
 
     test.todo('bones', () => {
