a99ade872bc6548f86e6354288798810444b8e89
Ed Preston <epreston@prestonsoft.com>
2023-08-14 10:07:47 +1000

    Update AnimationClip.unit.test.js
    add unit test

--- packages/animation/tests/AnimationClip.unit.test.js
+++ packages/animation/tests/AnimationClip.unit.test.js
@@ -26,8 +26,11 @@ describe('Animation', () => {
       // implement
     });
 
-    test.todo('uuid', () => {
-      // implement
+    test('uuid', () => {
+      const object = new AnimationClip('clip1', 1000, [{}]);
+
+      expect(object.uuid).to.be.a('string');
+      expect(object.uuid).to.have.length(36);
     });
 
     test.todo('parse', () => {
