3b13a4837dbe6dac9a6c852925ff48361c9d72c5
Ed Preston <epreston@prestonsoft.com>
2023-08-23 20:27:34 +1000

    Update global.d.ts
    add module declaration for glsl imports

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+declare module '*.glsl' {
+  const src: string
+  export default src
+}
+
 // Global compile-time constants
 declare var __DEV__: boolean;
 declare var __TEST__: boolean;
