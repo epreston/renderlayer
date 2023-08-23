29714a55af80d35ec1a69197d47e57a4b97ee601
Ed Preston <epreston@prestonsoft.com>
2023-08-23 20:28:15 +1000

    Update math.esm-bundler.js
    refresh build with notes

--- packages/math/dist/math.esm-bundler.js
+++ packages/math/dist/math.esm-bundler.js
@@ -1288,27 +1288,27 @@ class Vector3 {
     this.z = radius * Math.cos(theta);
     return this;
   }
-  setFromMatrixPosition(m) {
-    const e = m.elements;
+  setFromMatrixPosition(mat4) {
+    const e = mat4.elements;
     this.x = e[12];
     this.y = e[13];
     this.z = e[14];
     return this;
   }
-  setFromMatrixScale(m) {
-    const sx = this.setFromMatrixColumn(m, 0).length();
-    const sy = this.setFromMatrixColumn(m, 1).length();
-    const sz = this.setFromMatrixColumn(m, 2).length();
+  setFromMatrixScale(mat4) {
+    const sx = this.setFromMatrixColumn(mat4, 0).length();
+    const sy = this.setFromMatrixColumn(mat4, 1).length();
+    const sz = this.setFromMatrixColumn(mat4, 2).length();
     this.x = sx;
     this.y = sy;
     this.z = sz;
     return this;
   }
-  setFromMatrixColumn(m, index) {
-    return this.fromArray(m.elements, index * 4);
+  setFromMatrixColumn(mat4, index) {
+    return this.fromArray(mat4.elements, index * 4);
   }
-  setFromMatrix3Column(m, index) {
-    return this.fromArray(m.elements, index * 3);
+  setFromMatrix3Column(mat3, index) {
+    return this.fromArray(mat3.elements, index * 3);
   }
   setFromEuler(e) {
     this.x = e._x;
