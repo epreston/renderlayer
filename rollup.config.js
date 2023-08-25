9b81a7640f17723a72af9d735e497de849d57082
Ed Preston <epreston@prestonsoft.com>
2023-08-25 12:14:03 +1000

    Update rollup.config.js
    disable unused output configs

--- rollup.config.js
+++ rollup.config.js
@@ -51,20 +51,20 @@ const outputConfigs = {
   'global': {
     file: resolve(`dist/${name}.global.js`),
     format: `iife`
-  },
-  // runtime-only builds, for main "vue" package only
-  'esm-bundler-runtime': {
-    file: resolve(`dist/${name}.runtime.esm-bundler.js`),
-    format: `es`
-  },
-  'esm-browser-runtime': {
-    file: resolve(`dist/${name}.runtime.esm-browser.js`),
-    format: 'es'
-  },
-  'global-runtime': {
-    file: resolve(`dist/${name}.runtime.global.js`),
-    format: 'iife'
   }
+  // runtime-only builds, for main "vue" package only
+  // 'esm-bundler-runtime': {
+  //   file: resolve(`dist/${name}.runtime.esm-bundler.js`),
+  //   format: `es`
+  // },
+  // 'esm-browser-runtime': {
+  //   file: resolve(`dist/${name}.runtime.esm-browser.js`),
+  //   format: 'es'
+  // },
+  // 'global-runtime': {
+  //   file: resolve(`dist/${name}.runtime.global.js`),
+  //   format: 'iife'
+  // }
 };
 
 // const defaultFormats = ['esm-bundler', 'cjs'];
