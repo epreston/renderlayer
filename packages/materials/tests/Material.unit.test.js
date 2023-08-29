80ef81a118f807c413dceb5d8f96c92dd54d6270
Ed Preston <epreston@prestonsoft.com>
2023-08-30 02:25:28 +1000

    Update Material.unit.test.js
    cleanup

--- packages/materials/tests/Material.unit.test.js
+++ packages/materials/tests/Material.unit.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
 
 import { EventDispatcher } from '@renderlayer/core';
 import { Material } from '../src/Material.js';
