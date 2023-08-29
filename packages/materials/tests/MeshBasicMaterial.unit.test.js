e03185b5b2bcbeb5d8d7dc7177877b947b6e6a3c
Ed Preston <epreston@prestonsoft.com>
2023-08-30 01:32:25 +1000

    Update MeshBasicMaterial.unit.test.js
    add unit tests

--- packages/materials/tests/MeshBasicMaterial.unit.test.js
+++ packages/materials/tests/MeshBasicMaterial.unit.test.js
@@ -1,4 +1,8 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
+
+import { Color } from '@renderlayer/math';
+import { MixOperation, MultiplyOperation } from '@renderlayer/shared';
+import { Texture } from '@renderlayer/textures';
 
 import { Material } from '../src/Material.js';
 import { MeshBasicMaterial } from '../src/MeshBasicMaterial.js';
@@ -15,86 +19,137 @@ describe('Materials', () => {
       expect(object).toBeInstanceOf(Material);
     });
 
-    test('type', () => {
-      const object = new MeshBasicMaterial();
-      expect(object.type === 'MeshBasicMaterial').toBeTruthy();
-    });
-
-    test.todo('color', () => {
-      // implement
-    });
-
-    test.todo('map', () => {
-      // implement
-    });
-
-    test.todo('lightMap', () => {
-      // implement
-    });
-
-    test.todo('lightMapIntensity', () => {
-      // implement
-    });
-
-    test.todo('aoMap', () => {
-      // implement
-    });
-
-    test.todo('aoMapIntensity', () => {
-      // implement
-    });
-
-    test.todo('specularMap', () => {
-      // implement
-    });
-
-    test.todo('alphaMap', () => {
-      // implement
-    });
-
-    test.todo('envMap', () => {
-      // implement
-    });
-
-    test.todo('combine', () => {
-      // implement
-    });
-
-    test.todo('reflectivity', () => {
-      // implement
-    });
-
-    test.todo('refractionRatio', () => {
-      // implement
-    });
-
-    test.todo('wireframe', () => {
-      // implement
-    });
-
-    test.todo('wireframeLinewidth', () => {
-      // implement
-    });
-
-    test.todo('wireframeLinecap', () => {
-      // implement
-    });
-
-    test.todo('wireframeLinejoin', () => {
-      // implement
-    });
-
-    test.todo('fog', () => {
-      // implement
-    });
-
     test('isMeshBasicMaterial', () => {
       const object = new MeshBasicMaterial();
       expect(object.isMeshBasicMaterial).toBeTruthy();
     });
 
-    test.todo('copy', () => {
-      // implement
+    test('type', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.type).toBe('MeshBasicMaterial');
+    });
+
+    test('color', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.color.equals(new Color(0xffffff))).toBeTruthy();
+    });
+
+    test('map', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.map).toBeNull();
+    });
+
+    test('lightMap', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.lightMap).toBeNull();
+    });
+
+    test('lightMapIntensity', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.lightMapIntensity).toBe(1);
+    });
+
+    test('aoMap', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.aoMap).toBeNull();
+    });
+
+    test('aoMapIntensity', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.aoMapIntensity).toBe(1);
+    });
+
+    test('specularMap', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.specularMap).toBeNull();
+    });
+
+    test('alphaMap', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.alphaMap).toBeNull();
+    });
+
+    test('envMap', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.envMap).toBeNull();
+    });
+
+    test('combine', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.combine).toBe(MultiplyOperation);
+    });
+
+    test('reflectivity', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.reflectivity).toBe(1);
+    });
+
+    test('refractionRatio', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.refractionRatio).toBeCloseTo(0.98);
+    });
+
+    test('wireframe', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.wireframe).toBe(false);
+    });
+
+    test('wireframeLinewidth', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.wireframeLinewidth).toBe(1);
+    });
+
+    test('wireframeLinecap', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.wireframeLinecap).toBe('round');
+    });
+
+    test('wireframeLinejoin', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.wireframeLinejoin).toBe('round');
+    });
+
+    test('fog', () => {
+      const object = new MeshBasicMaterial();
+      expect(object.fog).toBe(true);
+    });
+
+    test('copy', () => {
+      const object = new MeshBasicMaterial();
+      const object2 = new MeshBasicMaterial();
+
+      object2.color = new Color(0x3d3d3d);
+      object2.map = new Texture();
+
+      object2.lightMap = new Texture();
+      object2.lightMapIntensity = 0.5;
+
+      object2.alphaMap = new Texture();
+      object2.aoMapIntensity = 0.5;
+
+      object2.specularMap = new Texture();
+      object2.alphaMap = new Texture();
+
+      object2.envMap = new Texture();
+      object2.combine = MixOperation;
+
+      object2.reflectivity = 0.5;
+      object2.refractionRatio = 0.88;
+
+      object2.wireframe = true;
+      object2.wireframeLinewidth = 2;
+      object2.wireframeLinecap = 'square';
+      object2.wireframeLinejoin = 'square';
+
+      object2.fog = false;
+
+      object.copy(object2);
+
+      // uuid will be different
+      object.uuid = object2.uuid;
+
+      expect(object).not.toBe(object2);
+      expect(object).toStrictEqual(object2);
     });
   });
 });
