e4a01abeba189fc70ff2137b0ba40e9d11d521e3
Ed Preston <epreston@prestonsoft.com>
2023-08-30 02:10:35 +1000

    Update MeshDistanceMaterial.unit.test.js
    add unit tests

--- packages/materials/tests/MeshDistanceMaterial.unit.test.js
+++ packages/materials/tests/MeshDistanceMaterial.unit.test.js
@@ -1,4 +1,6 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
+
+import { Texture } from '@renderlayer/textures';
 
 import { Material } from '../src/Material.js';
 import { MeshDistanceMaterial } from '../src/MeshDistanceMaterial.js';
@@ -15,38 +17,59 @@ describe('Materials', () => {
       expect(object).toBeInstanceOf(Material);
     });
 
-    test('type', () => {
-      const object = new MeshDistanceMaterial();
-      expect(object.type === 'MeshDistanceMaterial').toBeTruthy();
-    });
-
-    test.todo('map', () => {
-      // implement
-    });
-
-    test.todo('alphaMap', () => {
-      // implement
-    });
-
-    test.todo('displacementMap', () => {
-      // implement
-    });
-
-    test.todo('displacementScale', () => {
-      // implement
-    });
-
-    test.todo('displacementBias', () => {
-      // implement
-    });
-
     test('isMeshDistanceMaterial', () => {
       const object = new MeshDistanceMaterial();
       expect(object.isMeshDistanceMaterial).toBeTruthy();
     });
 
-    test.todo('copy', () => {
-      // implement
+    test('type', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.type === 'MeshDistanceMaterial').toBeTruthy();
+    });
+
+    test('map', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.map).toBeNull();
+    });
+
+    test('alphaMap', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.alphaMap).toBeNull();
+    });
+
+    test('displacementMap', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.displacementMap).toBeNull();
+    });
+
+    test('displacementScale', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.displacementScale).toBe(1);
+    });
+
+    test('displacementBias', () => {
+      const object = new MeshDistanceMaterial();
+      expect(object.displacementBias).toBe(0);
+    });
+
+    test('copy', () => {
+      const object = new MeshDistanceMaterial();
+      const object2 = new MeshDistanceMaterial();
+
+      object2.map = new Texture();
+      object2.alphaMap = new Texture();
+
+      object2.displacementMap = new Texture();
+      object2.displacementScale = 0.8;
+      object2.displacementBias = 0.2;
+
+      object.copy(object2);
+
+      // uuid will be different
+      object.uuid = object2.uuid;
+
+      expect(object).not.toBe(object2);
+      expect(object).toStrictEqual(object2);
     });
   });
 });
