6519c66c30fc7b4e6a66d99600bef40911f297c9
Ed Preston <epreston@prestonsoft.com>
2023-09-01 18:12:00 +1000

    Update CubicInterpolant.unit.test.js
    add unit tests

--- packages/interpolants/tests/CubicInterpolant.unit.test.js
+++ packages/interpolants/tests/CubicInterpolant.unit.test.js
@@ -3,6 +3,10 @@ import { describe, expect, it, test, vi } from 'vitest';
 import { Interpolant } from '../src/Interpolant.js';
 import { CubicInterpolant } from '../src/CubicInterpolant.js';
 
+function createCubicInterpolant() {
+  return new CubicInterpolant(new Float32Array(2), new Float32Array(2), 1, new Float32Array(1));
+}
+
 describe('Interpolants', () => {
   describe('CubicInterpolant', () => {
     test('constructor', () => {
@@ -16,17 +20,30 @@ describe('Interpolants', () => {
       expect(object).toBeInstanceOf(Interpolant);
     });
 
-    test.todo('intervalChanged_', () => {
-      // private
-      // intervalChanged_( i1, t0, t1 )
-      // implement
+    test('intervalChanged_', () => {
+      const object = createCubicInterpolant();
+      const evalResult = object.evaluate(0.5);
+      expect(evalResult.length).toBe(1);
+      expect(evalResult[0]).toBe(0);
+
+      object.intervalChanged_(1, 11, 22);
+
+      expect(object._weightPrev).toBe(-0.5);
+      expect(object._weightNext).toBe(-0.5);
+      expect(object._offsetPrev).toBe(1);
+      expect(object._offsetNext).toBe(0);
     });
 
-    test.todo('interpolate_', () => {
-      // private
-      // interpolate_( i1, t0, t, t1 )
-      // return equal to base class Interpolant.resultBuffer after call
-      // implement
+    test('interpolate_', () => {
+      const object = createCubicInterpolant();
+      const evalResult = object.evaluate(0.5);
+      expect(evalResult.length).toBe(1);
+      expect(evalResult[0]).toBe(0);
+
+      const result = object.interpolate_(1, 11, 11, 22);
+
+      expect(result.length).toBe(1);
+      expect(result[0]).toBe(0);
     });
   });
 });
