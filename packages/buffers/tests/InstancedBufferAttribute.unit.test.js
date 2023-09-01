3f8d41974ba53e6b5c7d3587edfd34ed3ef09b80
Ed Preston <epreston@prestonsoft.com>
2023-09-02 07:06:20 +1000

    Update InstancedBufferAttribute.unit.test.js
    add unit tests

--- packages/buffers/tests/InstancedBufferAttribute.unit.test.js
+++ packages/buffers/tests/InstancedBufferAttribute.unit.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
 
 import { BufferAttribute } from '../src/BufferAttribute.js';
 import { InstancedBufferAttribute } from '../src/InstancedBufferAttribute.js';
@@ -8,11 +8,11 @@ describe('Buffers', () => {
     test('constructor', () => {
       // array, itemSize
       let instance = new InstancedBufferAttribute(new Float32Array(10), 2);
-      expect(instance.meshPerAttribute === 1).toBeTruthy();
+      expect(instance.meshPerAttribute).toBe(1);
 
       // array, itemSize, normalized, meshPerAttribute
       instance = new InstancedBufferAttribute(new Float32Array(10), 2, false, 123);
-      expect(instance.meshPerAttribute === 123).toBeTruthy();
+      expect(instance.meshPerAttribute).toBe(123);
     });
 
     test('extends', () => {
@@ -20,13 +20,16 @@ describe('Buffers', () => {
       expect(object).toBeInstanceOf(BufferAttribute);
     });
 
-    test.todo('meshPerAttribute', () => {
-      // implement
-    });
-
     test('isInstancedBufferAttribute', () => {
       const object = new InstancedBufferAttribute();
       expect(object.isInstancedBufferAttribute).toBeTruthy();
+    });
+
+    test('meshPerAttribute', () => {
+      const array = new Float32Array([1, 2, 3, 7, 8, 9]);
+      const instance = new InstancedBufferAttribute(array, 2, true, 123);
+
+      expect(instance.meshPerAttribute).toBe(123);
     });
 
     test('copy', () => {
@@ -35,17 +38,36 @@ describe('Buffers', () => {
       const copiedInstance = instance.copy(instance);
 
       expect(copiedInstance).toBeInstanceOf(InstancedBufferAttribute);
-      expect(copiedInstance.itemSize === 2).toBeTruthy();
+      expect(copiedInstance.itemSize).toBe(2);
       expect(copiedInstance.normalized).toBeTruthy();
-      expect(copiedInstance.meshPerAttribute === 123).toBeTruthy();
+      expect(copiedInstance.meshPerAttribute).toBe(123);
 
       for (let i = 0; i < array.length; i++) {
-        expect(copiedInstance.array[i] === array[i]).toBeTruthy();
+        expect(copiedInstance.array[i]).toBe(array[i]);
       }
     });
 
-    test.todo('toJSON', () => {
-      // implement
+    test('toJSON', () => {
+      const array = new Float32Array([1, 2, 3, 7, 8, 9]);
+      const instance = new InstancedBufferAttribute(array, 2, true, 123);
+
+      expect(instance).toMatchInlineSnapshot(`
+        {
+          "array": [
+            1,
+            2,
+            3,
+            7,
+            8,
+            9,
+          ],
+          "isInstancedBufferAttribute": true,
+          "itemSize": 2,
+          "meshPerAttribute": 123,
+          "normalized": true,
+          "type": "Float32Array",
+        }
+      `);
     });
   });
 });
