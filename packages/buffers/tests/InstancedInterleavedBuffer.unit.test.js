7c408623d33c154449d7c920983a51f990b9f4f2
Ed Preston <epreston@prestonsoft.com>
2023-09-02 07:01:23 +1000

    Update InstancedInterleavedBuffer.unit.test.js
    add unit test

--- packages/buffers/tests/InstancedInterleavedBuffer.unit.test.js
+++ packages/buffers/tests/InstancedInterleavedBuffer.unit.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
 
 import { InterleavedBuffer } from '../src/InterleavedBuffer.js';
 import { InstancedInterleavedBuffer } from '../src/InstancedInterleavedBuffer.js';
@@ -17,13 +17,13 @@ describe('Buffers', () => {
       expect(object).toBeInstanceOf(InterleavedBuffer);
     });
 
-    test.todo('meshPerAttribute', () => {
-      // implement
-    });
-
     test('isInstancedInterleavedBuffer', () => {
       const object = new InstancedInterleavedBuffer();
       expect(object.isInstancedInterleavedBuffer).toBeTruthy();
+    });
+
+    test.todo('meshPerAttribute', () => {
+      // implement
     });
 
     test('copy', () => {
@@ -38,8 +38,24 @@ describe('Buffers', () => {
       // implement
     });
 
-    test.todo('toJSON', () => {
-      // implement
+    test('toJSON', () => {
+      const array = new Float32Array([1, 2, 3, 7, 8, 9]);
+      const instance = new InstancedInterleavedBuffer(array, 3);
+      instance.uuid = '67247c5e-822e-4ddf-929f-d3082629d644';
+      instance.array.buffer._uuid = '8a7a4b09-dddb-42a2-8ecd-ff4c1a400ae3';
+
+      const data = {};
+
+      expect(instance.toJSON(data)).toMatchInlineSnapshot(`
+        {
+          "buffer": "8a7a4b09-dddb-42a2-8ecd-ff4c1a400ae3",
+          "isInstancedInterleavedBuffer": true,
+          "meshPerAttribute": 1,
+          "stride": 3,
+          "type": "Float32Array",
+          "uuid": "67247c5e-822e-4ddf-929f-d3082629d644",
+        }
+      `);
     });
   });
 });
