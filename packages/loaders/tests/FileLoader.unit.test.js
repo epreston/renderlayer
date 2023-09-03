1d2796beb96f8b21ef31ba29ee407246c2ecec40
Ed Preston <epreston@prestonsoft.com>
2023-09-04 05:46:55 +1000

    Update FileLoader.unit.test.js
    add unit test

--- packages/loaders/tests/FileLoader.unit.test.js
+++ packages/loaders/tests/FileLoader.unit.test.js
@@ -1,7 +1,14 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+// @vitest-environment jsdom
+
+import { describe, expect, it, test, vi } from 'vitest';
+import { resolveAfter, flushPromises } from './test-helpers.js';
 
 import { Loader } from '../src/Loader.js';
 import { FileLoader } from '../src/FileLoader.js';
+
+// will be intercepted by msw, not a real url
+const DataTestFile = 'http://renderlayer.org/test/json/data.json';
+const MissingTestFile = 'http://renderlayer.org/test/json/missing.json';
 
 describe('Loaders', () => {
   describe('FileLoader', () => {
@@ -15,8 +22,35 @@ describe('Loaders', () => {
       expect(object).toBeDefined();
     });
 
-    test.todo('load', () => {
-      // implement
+    test('load', async () => {
+      const onLoad = vi.fn();
+      const onProgress = vi.fn(); // unused
+      const onError = vi.fn();
+
+      const object = new FileLoader();
+
+      // --------------------
+      // good file
+      object.load(DataTestFile, onLoad, onProgress, onError);
+
+      // allow time for fetch and async code to compete before asserts
+      // await resolveAfter(100);
+      await flushPromises();
+      expect(onLoad).toHaveBeenCalled();
+      expect(onError).not.toHaveBeenCalled();
+
+      vi.clearAllMocks();
+
+      // --------------------
+      // bad file
+      object.load(MissingTestFile, onLoad, onProgress, onError);
+
+      // allow time for fetch and async code to compete before asserts
+      // await resolveAfter(100);
+      await flushPromises();
+      expect(onLoad).not.toHaveBeenCalled();
+      expect(onError).toHaveBeenCalled();
+      expect('missing test file').toHaveBeenWarned();
     });
 
     test.todo('setResponseType', () => {
