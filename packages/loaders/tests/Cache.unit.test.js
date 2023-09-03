563a752093d6f85baeae1d0eda684a40d1316455
Ed Preston <epreston@prestonsoft.com>
2023-09-04 06:43:48 +1000

    Update Cache.unit.test.js
    add unit tests

--- packages/loaders/tests/Cache.unit.test.js
+++ packages/loaders/tests/Cache.unit.test.js
@@ -1,9 +1,12 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
 
 import { Cache } from '../src/Cache.js';
 
 describe('Loaders', () => {
   describe('Cache', () => {
+    const testKey = '/test/file.json';
+    const testFile = { name: 'test' };
+
     test('enabled', () => {
       const actual = Cache.enabled;
       const expected = false;
@@ -16,23 +19,85 @@ describe('Loaders', () => {
       expect(actual).toEqual(expected);
     });
 
-    test.todo('add', () => {
-      // function ( key, file )
-      // implement
+    test('add', () => {
+      Cache.enabled = false;
+      Cache.add(testKey, testFile);
+
+      expect(Cache.files).toMatchInlineSnapshot('{}');
+
+      Cache.enabled = true;
+      Cache.add(testKey, testFile);
+
+      expect(Cache.files).toMatchInlineSnapshot(`
+        {
+          "/test/file.json": {
+            "name": "test",
+          },
+        }
+      `);
     });
 
-    test.todo('get', () => {
-      // function ( key )
-      // implement
+    test('get', () => {
+      Cache.enabled = true;
+      Cache.add(testKey, testFile);
+
+      expect(Cache.files).toMatchInlineSnapshot(`
+        {
+          "/test/file.json": {
+            "name": "test",
+          },
+        }
+      `);
+
+      const file = Cache.get(testKey);
+      expect(file).toStrictEqual(testFile);
+
+      Cache.enabled = false;
+
+      expect(Cache.files).toMatchInlineSnapshot(`
+        {
+          "/test/file.json": {
+            "name": "test",
+          },
+        }
+      `);
+
+      const disabledNotFound = Cache.get(testKey);
+      expect(disabledNotFound).toBeUndefined();
     });
 
-    test.todo('remove', () => {
-      // function ( key )
-      // implement
+    test('remove', () => {
+      Cache.enabled = true;
+      Cache.add(testKey, testFile);
+
+      expect(Cache.files).toMatchInlineSnapshot(`
+        {
+          "/test/file.json": {
+            "name": "test",
+          },
+        }
+      `);
+
+      Cache.enabled = false;
+      Cache.remove(testKey);
+
+      // clear works even when disabled
+      expect(Cache.files).toMatchInlineSnapshot('{}');
     });
 
-    test.todo('clear', () => {
-      // implement
+    test('clear', () => {
+      Cache.enabled = true;
+      Cache.clear();
+
+      expect(Cache.files).toMatchInlineSnapshot('{}');
+
+      Cache.enabled = true;
+      Cache.add(testKey, testFile);
+      Cache.enabled = false;
+      Cache.clear();
+
+      // clear works even when disabled
+      expect(Cache.files).toMatchInlineSnapshot('{}');
     });
   });
 });
