ff8b400fd987cc6ab78b00494594d3ff5dee60b4
Ed Preston <epreston@prestonsoft.com>
2023-09-04 05:16:25 +1000

    Update TextureLoader.unit.test.js
    add notes

--- packages/loaders/tests/TextureLoader.unit.test.js
+++ packages/loaders/tests/TextureLoader.unit.test.js
@@ -1,7 +1,16 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+// Replace -AT- to enable: -AT-vitest-environment jsdom
+
+import { describe, expect, it, test, vi } from 'vitest';
+import { resolveAfter, flushPromises } from './test-helpers.js';
+
+import { Texture } from '@renderlayer/textures';
 
 import { Loader } from '../src/Loader.js';
 import { TextureLoader } from '../src/TextureLoader.js';
+
+// will be intercepted by msw, not a real url
+const UVTestFile = 'http://renderlayer.org/test/jpeg/uvcheck.jpg';
+const MissingTestFile = 'http://renderlayer.org/test/jpeg/missing.jpg';
 
 describe('Loaders', () => {
   describe('TextureLoader', () => {
@@ -15,8 +24,52 @@ describe('Loaders', () => {
       expect(object).toBeInstanceOf(Loader);
     });
 
-    test.todo('load', () => {
-      // implement
+    test.todo('load', async () => {
+      /**
+       * The following code will work when running this test in a jsdom
+       * context (see comment at top of file), provided that the "canvas"
+       * package is installed and jsdom is configured to make resources
+       * 'usable' with the following in the vitest.config.js file.
+       *
+       * environmentOptions: {
+       *   jsdom: {
+       *     resources: 'usable'
+       *   }
+       * },
+       *
+       * Test execution will fail when running the whole suite but will
+       * succeed when the file is run in isolation. Disabling until a
+       * better alternative is found.
+       */
+
+      const onLoad = vi.fn();
+      const onProgress = vi.fn(); // unused
+      const onError = vi.fn();
+
+      const object = new TextureLoader();
+
+      // --------------------
+      // good file
+      const texture = object.load(UVTestFile, onLoad, onProgress, onError);
+      expect(texture).toBeInstanceOf(Texture); // empty provided immediately
+
+      // allow time for fetch and async code to compete before asserts
+      await resolveAfter(100);
+      expect(onLoad).toHaveBeenCalled();
+      expect(onError).not.toHaveBeenCalled();
+
+      vi.clearAllMocks();
+
+      // --------------------
+      // bad file
+      const missingTexture = object.load(MissingTestFile, onLoad, onProgress, onError);
+      expect(missingTexture).toBeInstanceOf(Texture); // empty provided immediately
+
+      // allow time for fetch and async code to compete before asserts
+      await resolveAfter(100);
+      expect(onLoad).not.toHaveBeenCalled();
+      expect(onError).toHaveBeenCalled();
+      expect('missing test file').toHaveBeenWarned();
     });
   });
 });
