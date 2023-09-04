16c4168a8af25d3674393733dc8258d3cd4ce5fc
Ed Preston <epreston@prestonsoft.com>
2023-09-04 11:05:19 +1000

    Update LoaderUtils.unit.test.js
    add unit tests

--- packages/loaders/tests/LoaderUtils.unit.test.js
+++ packages/loaders/tests/LoaderUtils.unit.test.js
@@ -1,4 +1,6 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+// @vitest-environment jsdom
+
+import { describe, expect, it, test, vi } from 'vitest';
 
 import { LoaderUtils } from '../src/LoaderUtils.js';
 
@@ -20,9 +22,71 @@ describe('Loaders', () => {
       expect(LoaderUtils.extractUrlBase('/model.glb')).toEqual('/');
     });
 
-    test.todo('resolveURL', () => {
-      // static resolveURL( url, path )
-      // implement
+    // ----------------------
+    // Valid URL / URI Passthrough
+    // ----------------------
+
+    test.each([
+      'http://renderlayer.org', // Absolute URL
+      'https://renderlayer.org', // Absolute URL
+      '//renderlayer.org', // Absolute URL
+      'data:,Hello%2C%20World%21', // Data URI
+      'data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==', // Data URI
+      'data:text/html,%3Ch1%3EHello%2C%20World%21%3C%2Fh1%3E', // Data URI
+      'blob:http://example.com/550e8400-e29b-41d4-a716-446655440000', // Blob URI
+      'blob:https://javascript.info/1e67e00e-860d-40a5-89ae-6ab0cbee6273', //Blob URI
+      'file:///C:/ProgramData/chocolatey/logs/chocolatey.log' // File URL - Patch
+    ])('resolveURL -> %s', (url) => {
+      const path = 'http://renderlayer.org';
+      expect(LoaderUtils.resolveURL(url, path)).toBe(url);
+    });
+
+    // ----------------------
+    // Invalid URL / URI returns empty string
+    // ----------------------
+
+    // prettier-ignore
+    test.each([
+      '',
+      1,
+      { test: 'test' },
+    ])('resolveURL ≠ %s', (invalidURL) => {
+      const path = 'http://renderlayer.org';
+      expect(LoaderUtils.resolveURL(invalidURL, path)).toBe('');
+    });
+
+    // ----------------------
+    // Relative URL - with path
+    // ----------------------
+
+    // prettier-ignore
+    test.each([
+      ['/', 'http://renderlayer.org/'],
+      ['test.jpg', 'http://renderlayer.org/test.jpg'],
+      ['./test.jpg', 'http://renderlayer.org/test.jpg'],
+      ['/test.jpg', 'http://renderlayer.org/test.jpg'],
+      ['/assets/test.jpg', 'http://renderlayer.org/assets/test.jpg'],
+    ])('resolveURL : %s -> %s', (test, expected) => {
+      const path = 'http://renderlayer.org';
+      expect(LoaderUtils.resolveURL(test, path)).toBe(expected);
+    });
+
+    // ----------------------
+    // Relative URL - no path
+    // ----------------------
+
+    const withPathInference = true;
+
+    // prettier-ignore
+    test.each([
+      ['/', withPathInference ? 'http://localhost:3000/' : ''],
+      ['test.jpg', withPathInference ? 'http://localhost:3000/test.jpg' : ''],
+      ['./test.jpg', withPathInference ? 'http://localhost:3000/test.jpg' : ''],
+      ['/test.jpg', withPathInference ? 'http://localhost:3000/test.jpg' : ''],
+      ['/assets/test.jpg', withPathInference ? 'http://localhost:3000/assets/test.jpg' : ''],
+    ])('resolveURL = %s -> \'%s\'', (test, expected) => {
+      // const path = 'http://renderlayer.org';
+      expect(LoaderUtils.resolveURL(test)).toBe(expected);
     });
   });
 });
