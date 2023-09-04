141bd1aa6c6b95d147534f3fcf0e52632c7c2af4
Ed Preston <epreston@prestonsoft.com>
2023-09-05 00:06:05 +1000

    Update MaterialLoader.unit.test.js
    add unit test

--- packages/loaders/tests/MaterialLoader.unit.test.js
+++ packages/loaders/tests/MaterialLoader.unit.test.js
@@ -1,7 +1,15 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+// @vitest-environment jsdom
+
+import { describe, expect, it, test, vi } from 'vitest';
+import { resolveAfter, flushPromises } from './test-helpers.js';
 
 import { Loader } from '../src/Loader.js';
 import { MaterialLoader } from '../src/MaterialLoader.js';
+
+// will be intercepted by msw, not a real url
+const MatTestFile = 'http://renderlayer.org/test/json/material.json';
+const ShaderMatTestFile = 'http://renderlayer.org/test/json/shadermaterial.json';
+const MissingTestFile = 'http://renderlayer.org/test/json/missing.json';
 
 describe('Loaders', () => {
   describe('MaterialLoader', () => {
@@ -21,8 +29,47 @@ describe('Loaders', () => {
       expect(actual).toEqual(expected);
     });
 
-    test.todo('load', () => {
-      // implement
+    test('load', async () => {
+      const onLoad = vi.fn();
+      const onProgress = vi.fn(); // unused
+      const onError = vi.fn();
+
+      const object = new MaterialLoader();
+
+      // --------------------
+      // MeshBasicMaterial file
+      object.load(MatTestFile, onLoad, onProgress, onError);
+
+      // allow time for fetch and async code to compete before asserts
+      // await resolveAfter(100);
+      await flushPromises();
+      expect(onLoad).toHaveBeenCalled();
+      expect(onError).not.toHaveBeenCalled();
+
+      vi.clearAllMocks();
+
+      // --------------------
+      // ShaderMaterial file
+      object.load(ShaderMatTestFile, onLoad, onProgress, onError);
+
+      // allow time for fetch and async code to compete before asserts
+      // await resolveAfter(100);
+      await flushPromises();
+      expect(onLoad).toHaveBeenCalled();
+      expect(onError).not.toHaveBeenCalled();
+
+      vi.clearAllMocks();
+
+      // --------------------
+      // bad file
+      object.load(MissingTestFile, onLoad, onProgress, onError);
+
+      // allow time for fetch and async code to compete before asserts
+      // await resolveAfter(100);
+      await flushPromises();
+      expect(onLoad).not.toHaveBeenCalled();
+      expect(onError).toHaveBeenCalled();
+      expect('missing test file').toHaveBeenWarned();
     });
 
     test.todo('parse', () => {
