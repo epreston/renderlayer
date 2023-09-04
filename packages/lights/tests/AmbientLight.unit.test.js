9fe68a6e1ce20abbc853c15e6af81a4ec32d737f
Ed Preston <epreston@prestonsoft.com>
2023-09-04 23:03:39 +1000

    Update AmbientLight.unit.test.js
    enable ObjectLoader check

--- packages/lights/tests/AmbientLight.unit.test.js
+++ packages/lights/tests/AmbientLight.unit.test.js
@@ -1,5 +1,7 @@
 import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
 
+import { ObjectLoader } from '@renderlayer/loaders';
+
 import { Light } from '../src/Light.js';
 import { AmbientLight } from '../src/AmbientLight.js';
 
@@ -92,9 +94,9 @@ describe('Lights', () => {
 
       expect(object.id).toBeUndefined();
 
-      // const loader = new ObjectLoader();
-      // const outputLight = loader.parse(json);
-      // expect(outputLight).toEqual(light);
+      const loader = new ObjectLoader();
+      const outputLight = loader.parse(json);
+      expect(outputLight).toEqual(light);
     });
   });
 });
