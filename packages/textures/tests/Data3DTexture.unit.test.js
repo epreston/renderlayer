3070ddf0c2efae28172aeb71734fe80babeacb16
Ed Preston <epreston@prestonsoft.com>
2023-09-05 20:20:46 +1000

    Update Data3DTexture.unit.test.js
    add unit tests

--- packages/textures/tests/Data3DTexture.unit.test.js
+++ packages/textures/tests/Data3DTexture.unit.test.js
@@ -1,5 +1,7 @@
 import { describe, expect, it, test, vi } from 'vitest';
 
+import { ClampToEdgeWrapping, NearestFilter } from '@renderlayer/shared';
+
 import { Texture } from '../src/Texture.js';
 import { Data3DTexture } from '../src/Data3DTexture.js';
 
@@ -20,32 +22,45 @@ describe('Textures', () => {
       expect(object.isData3DTexture).toBeTruthy();
     });
 
-    test.todo('image', () => {
-      // implement
+    test('image', () => {
+      const object = new Data3DTexture();
+
+      const data = object.image;
+      expect(data).not.toBeNull();
+
+      const newData = { data: 'test', width: 4, height: 4, depth: 1 };
+      object.image = newData;
+      expect(object.image).toBe(newData);
     });
 
-    test.todo('magFilter', () => {
-      // implement
+    test('magFilter', () => {
+      const object = new Data3DTexture();
+      expect(object.magFilter).toBe(NearestFilter);
     });
 
-    test.todo('minFilter', () => {
-      // implement
+    test('minFilter', () => {
+      const object = new Data3DTexture();
+      expect(object.magFilter).toBe(NearestFilter);
     });
 
-    test.todo('wrapR', () => {
-      // implement
+    test('wrapR', () => {
+      const object = new Data3DTexture();
+      expect(object.wrapR).toBe(ClampToEdgeWrapping);
     });
 
-    test.todo('generateMipmaps', () => {
-      // implement
+    test('generateMipmaps', () => {
+      const object = new Data3DTexture();
+      expect(object.generateMipmaps).toBe(false);
     });
 
-    test.todo('flipY', () => {
-      // implement
+    test('flipY', () => {
+      const object = new Data3DTexture();
+      expect(object.flipY).toBe(false);
     });
 
-    test.todo('unpackAlignment', () => {
-      // implement
+    test('unpackAlignment', () => {
+      const object = new Data3DTexture();
+      expect(object.unpackAlignment).toBe(1);
     });
   });
 });
