b71807865c1dfb4c31d87b1fb715a26bc96c96be
Ed Preston <epreston@prestonsoft.com>
2023-09-05 20:09:03 +1000

    Update DataArrayTexture.unit.test.js
    add unit tests

--- packages/textures/tests/DataArrayTexture.unit.test.js
+++ packages/textures/tests/DataArrayTexture.unit.test.js
@@ -1,4 +1,6 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
+
+import { ClampToEdgeWrapping, NearestFilter } from '@renderlayer/shared';
 
 import { Texture } from '../src/Texture.js';
 import { DataArrayTexture } from '../src/DataArrayTexture.js';
@@ -15,37 +17,50 @@ describe('Textures', () => {
       expect(object).toBeInstanceOf(Texture);
     });
 
-    test.todo('image', () => {
-      // implement
-    });
-
-    test.todo('magFilter', () => {
-      // implement
-    });
-
-    test.todo('minFilter', () => {
-      // implement
-    });
-
-    test.todo('wrapR', () => {
-      // implement
-    });
-
-    test.todo('generateMipmaps', () => {
-      // implement
-    });
-
-    test.todo('flipY', () => {
-      // implement
-    });
-
-    test.todo('unpackAlignment', () => {
-      // implement
-    });
-
     test('isDataArrayTexture', () => {
       const object = new DataArrayTexture();
       expect(object.isDataArrayTexture).toBeTruthy();
+    });
+
+    test('image', () => {
+      const object = new DataArrayTexture();
+
+      const data = object.image;
+      expect(data).not.toBeNull();
+
+      const newData = { data: 'test', width: 4, height: 4, depth: 1 };
+      object.image = newData;
+      expect(object.image).toBe(newData);
+    });
+
+    test('magFilter', () => {
+      const object = new DataArrayTexture();
+      expect(object.magFilter).toBe(NearestFilter);
+    });
+
+    test('minFilter', () => {
+      const object = new DataArrayTexture();
+      expect(object.minFilter).toBe(NearestFilter);
+    });
+
+    test('wrapR', () => {
+      const object = new DataArrayTexture();
+      expect(object.wrapR).toBe(ClampToEdgeWrapping);
+    });
+
+    test('generateMipmaps', () => {
+      const object = new DataArrayTexture();
+      expect(object.generateMipmaps).toBe(false);
+    });
+
+    test('flipY', () => {
+      const object = new DataArrayTexture();
+      expect(object.flipY).toBe(false);
+    });
+
+    test('unpackAlignment', () => {
+      const object = new DataArrayTexture();
+      expect(object.unpackAlignment).toBe(1);
     });
   });
 });
