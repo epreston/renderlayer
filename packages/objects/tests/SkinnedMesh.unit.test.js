4c652ab9470dce10ca54aa2d5bf2929f8b2cb65a
Ed Preston <epreston@prestonsoft.com>
2023-09-06 04:07:27 +1000

    Update SkinnedMesh.unit.test.js
    add unit tests

--- packages/objects/tests/SkinnedMesh.unit.test.js
+++ packages/objects/tests/SkinnedMesh.unit.test.js
@@ -1,8 +1,12 @@
 import { describe, expect, it, test, vi } from 'vitest';
 
-import { Matrix4 } from '@renderlayer/math';
-import { Object3D } from '@renderlayer/core';
+import { Object3D, Raycaster } from '@renderlayer/core';
+import { Matrix4, Vector3 } from '@renderlayer/math';
+
+import { skinnedMeshFactory } from './skinnedMeshHelpers.js';
+
 import { Mesh } from '../src/Mesh.js';
+import { Skeleton } from '../src/Skeleton.js';
 import { SkinnedMesh } from '../src/SkinnedMesh.js';
 
 describe('Objects', () => {
@@ -14,7 +18,6 @@ describe('Objects', () => {
 
     test('extends', () => {
       const skinnedMesh = new SkinnedMesh();
-
       expect(skinnedMesh).toBeInstanceOf(Object3D);
       expect(skinnedMesh).toBeInstanceOf(Mesh);
     });
@@ -50,6 +53,66 @@ describe('Objects', () => {
       expect(bindMatrixInverse.equals(new Matrix4().identity())).toBeTruthy();
     });
 
+    test('skeleton', () => {
+      const object = new SkinnedMesh();
+      expect(object.skeleton).toBeNull();
+    });
+
+    test('boundingBox', () => {
+      const object = new SkinnedMesh();
+      expect(object.boundingBox).toBeNull();
+    });
+
+    test('boundingSphere', () => {
+      const object = new SkinnedMesh();
+      expect(object.boundingSphere).toBeNull();
+    });
+
+    test('computeBoundingBox', () => {
+      const { mesh } = skinnedMeshFactory();
+
+      expect(mesh.boundingBox).toBeNull();
+
+      mesh.computeBoundingBox();
+
+      expect(mesh.boundingBox).toBeDefined();
+      expect(mesh.boundingBox).toMatchInlineSnapshot(`
+        Box3 {
+          "isBox3": true,
+          "max": Vector3 {
+            "x": 5,
+            "y": 16,
+            "z": 5,
+          },
+          "min": Vector3 {
+            "x": -5,
+            "y": -16,
+            "z": -5,
+          },
+        }
+      `);
+    });
+
+    test('computeBoundingSphere', () => {
+      const { mesh } = skinnedMeshFactory();
+
+      expect(mesh.boundingSphere).toBeNull();
+
+      mesh.computeBoundingSphere();
+
+      expect(mesh.boundingSphere).toBeDefined();
+      expect(mesh.boundingSphere).toMatchInlineSnapshot(`
+        Sphere {
+          "center": Vector3 {
+            "x": 0.6982977624676568,
+            "y": 3.5662052113218454,
+            "z": 1.3585288233260149,
+          },
+          "radius": 20.60857993529236,
+        }
+      `);
+    });
+
     test('copy', () => {
       const src = new SkinnedMesh();
       const dst = new SkinnedMesh();
@@ -59,28 +122,69 @@ describe('Objects', () => {
       expect(dst.bindMode).toBe(src.bindMode);
       expect(dst.bindMatrix.equals(src.bindMatrix)).toBeTruthy();
       expect(dst.bindMatrixInverse.equals(src.bindMatrixInverse)).toBeTruthy();
-
       expect(dst.skeleton).toBe(src.skeleton);
+
+      expect(dst).not.toBe(src);
     });
 
-    test.todo('bind', () => {
-      // implement
+    test('raycast', () => {
+      const { mesh } = skinnedMeshFactory();
+
+      const raycaster = new Raycaster();
+      const intersects = [];
+
+      mesh.raycast(raycaster, intersects);
+
+      expect(intersects.length).toBe(0);
     });
 
-    test.todo('pose', () => {
-      // implement
+    test('bind', () => {
+      const { mesh, bones } = skinnedMeshFactory();
+      const skeleton = new Skeleton(bones);
+
+      mesh.add(bones[0]);
+      mesh.bind(skeleton);
+
+      expect(mesh.skeleton).toBe(skeleton);
     });
 
-    test.todo('normalizeSkinWeights', () => {
-      // implement
+    test('pose', () => {
+      const { mesh } = skinnedMeshFactory();
+      mesh.pose();
+
+      // assets required
     });
 
-    test.todo('updateMatrixWorld', () => {
-      // implement
+    test('normalizeSkinWeights', () => {
+      const { mesh } = skinnedMeshFactory();
+      mesh.normalizeSkinWeights();
+
+      // assets required
     });
 
-    test.todo('applyBoneTransform', () => {
-      // implement
+    test('updateMatrixWorld', () => {
+      const { mesh } = skinnedMeshFactory();
+      mesh.bindMode = 'detached';
+
+      mesh.updateMatrixWorld();
+
+      // assets required
+    });
+
+    test('applyBoneTransform', () => {
+      const { mesh, geometry } = skinnedMeshFactory();
+      const positionAttribute = geometry.getAttribute('position');
+      const vertex = new Vector3().fromBufferAttribute(positionAttribute, 3);
+
+      mesh.applyBoneTransform(3, vertex);
+
+      expect(vertex).toMatchInlineSnapshot(`
+        Vector3 {
+          "x": 3.535533905029297,
+          "y": 16,
+          "z": -3.535533905029297,
+        }
+      `);
     });
   });
 });
