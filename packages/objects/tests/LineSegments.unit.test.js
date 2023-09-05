f7557cc3527757e7730277d596e164e1ca0f7d21
Ed Preston <epreston@prestonsoft.com>
2023-09-06 07:20:26 +1000

    Update LineSegments.unit.test.js
    add unit test

--- packages/objects/tests/LineSegments.unit.test.js
+++ packages/objects/tests/LineSegments.unit.test.js
@@ -2,6 +2,7 @@ import { describe, expect, it, test, vi } from 'vitest';
 
 import { BufferGeometry } from '@renderlayer/buffers';
 import { Object3D } from '@renderlayer/core';
+import { ObjectLoader } from '@renderlayer/loaders';
 import { Vector3 } from '@renderlayer/math';
 
 import { Line } from '../src/Line.js';
@@ -45,6 +46,23 @@ describe('Objects', () => {
       object.computeLineDistances();
 
       // EP: asserts required
+    });
+
+    test('from ObjectLoader', () => {
+      const points = [];
+      points.push(new Vector3(-10, 0, 0));
+      points.push(new Vector3(0, 10, 0));
+      points.push(new Vector3(10, 0, 0));
+
+      const geometry = new BufferGeometry().setFromPoints(points);
+      const object = new LineSegments(geometry);
+
+      const json = object.toJSON();
+      const loader = new ObjectLoader();
+      const outputObject = loader.parse(json);
+
+      // EP: toJSON returns BufferAttribute instead of Float32BufferAttribute ?
+      expect(outputObject.toJSON()).toStrictEqual(object.toJSON());
     });
   });
 });
