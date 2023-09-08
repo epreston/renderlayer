2c998ece7cba0a3877f6bcafb2f6f1aefe8cc0f8
Ed Preston <epreston@prestonsoft.com>
2023-09-09 09:26:34 +1000

    Update WebGLShaderCache.unit.test.js
    cleanup

--- packages/webgl/tests/WebGLShaderCache.unit.test.js
+++ packages/webgl/tests/WebGLShaderCache.unit.test.js
@@ -2,46 +2,48 @@ import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
 
 import { WebGLShaderCache } from '../src/WebGLShaderCache.js';
 
-describe('WebGLShaderCache', () => {
-  let instance;
+describe('WebGL', () => {
+  describe('WebGLShaderCache', () => {
+    let instance;
 
-  beforeAll(() => {
-    instance = new WebGLShaderCache();
+    beforeAll(() => {
+      instance = new WebGLShaderCache();
+    });
+
+    // beforeEach(() => {
+    //   instance = new WebGLShaderCache();
+    // });
+
+    test('constructor', () => {
+      expect(instance).toBeInstanceOf(WebGLShaderCache);
+    });
+
+    test.todo('update()', () => {
+      // instance.update(material);
+    });
+
+    test.todo('remove()', () => {
+      // instance.remove(material);
+    });
+
+    test.todo('getVertexShaderID()', () => {
+      // instance.getVertexShaderID(material);
+    });
+
+    test.todo('getFragmentShaderID()', () => {
+      // instance.getFragmentShaderID(material);
+    });
+
+    test.todo('dispose()', () => {
+      // instance.dispose();
+    });
+
+    // test.todo('_getShaderCacheForMaterial()', () => {
+    //   // instance._getShaderCacheForMaterial(material);
+    // });
+
+    // test.todo('_getShaderStage()', () => {
+    //   // instance._getShaderStage(code);
+    // });
   });
-
-  // beforeEach(() => {
-  //   instance = new WebGLShaderCache();
-  // });
-
-  test('constructor', () => {
-    expect(instance).toBeInstanceOf(WebGLShaderCache);
-  });
-
-  test.todo('update()', () => {
-    // instance.update(material);
-  });
-
-  test.todo('remove()', () => {
-    // instance.remove(material);
-  });
-
-  test.todo('getVertexShaderID()', () => {
-    // instance.getVertexShaderID(material);
-  });
-
-  test.todo('getFragmentShaderID()', () => {
-    // instance.getFragmentShaderID(material);
-  });
-
-  test.todo('dispose()', () => {
-    // instance.dispose();
-  });
-
-  // test.todo('_getShaderCacheForMaterial()', () => {
-  //   // instance._getShaderCacheForMaterial(material);
-  // });
-
-  // test.todo('_getShaderStage()', () => {
-  //   // instance._getShaderStage(code);
-  // });
 });
