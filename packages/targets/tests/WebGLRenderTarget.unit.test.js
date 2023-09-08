d948f51bfd921d4c3ac6be5200da0f2bcc6d145d
Ed Preston <epreston@prestonsoft.com>
2023-09-09 08:44:58 +1000

    Update WebGLRenderTarget.unit.test.js
    cleanup

--- packages/targets/tests/WebGLRenderTarget.unit.test.js
+++ packages/targets/tests/WebGLRenderTarget.unit.test.js
@@ -3,7 +3,7 @@ import { describe, expect, it, test, vi } from 'vitest';
 import { RenderTarget } from '../src/RenderTarget.js';
 import { WebGLRenderTarget } from '../src/WebGLRenderTarget.js';
 
-describe('Renderers', () => {
+describe('Targets', () => {
   describe('WebGLRenderTarget', () => {
     test('constructor', () => {
       const object = new WebGLRenderTarget();
