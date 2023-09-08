0755c4a2d9cbb5c1367b286432408f5197ee1b27
Ed Preston <epreston@prestonsoft.com>
2023-09-08 17:36:27 +1000

    Update PropertyBinding.js
    cleanup

--- packages/animation/src/PropertyBinding.js
+++ packages/animation/src/PropertyBinding.js
@@ -338,9 +338,7 @@ class PropertyBinding {
 
     // ensure there is a value node
     if (!targetObject) {
-      console.error(
-        'PropertyBinding: Trying to update node for track: ' + this.path + " but it wasn't found."
-      );
+      console.error('PropertyBinding: No target node found for track: ' + this.path + '.');
       return;
     }
 
