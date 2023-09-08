aa5da0ad4a6109b7c09008f9ab3d5346a4729000
Ed Preston <epreston@prestonsoft.com>
2023-09-09 09:26:27 +1000

    Update WebGLShader.unit.test.js
    cleanup

--- packages/webgl/tests/WebGLShader.unit.test.js
+++ packages/webgl/tests/WebGLShader.unit.test.js
@@ -1,4 +1,4 @@
-import { beforeAll, beforeEach, describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, test, vi } from 'vitest';
 
 import { WebGLShader } from '../src/WebGLShader.js';
 
