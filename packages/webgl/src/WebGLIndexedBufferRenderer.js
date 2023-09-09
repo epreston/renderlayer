d5e678de4205a11a0b8f190449001e6e31918dd3
Ed Preston <epreston@prestonsoft.com>
2023-09-10 08:13:21 +1000

    Update WebGLIndexedBufferRenderer.js
    convert to class

--- packages/webgl/src/WebGLIndexedBufferRenderer.js
+++ packages/webgl/src/WebGLIndexedBufferRenderer.js
@@ -1,37 +1,39 @@
-function WebGLIndexedBufferRenderer(gl, extensions, info, capabilities) {
-  let mode;
+class WebGLIndexedBufferRenderer {
+  constructor(gl, extensions, info, capabilities) {
+    let mode;
 
-  function setMode(value) {
-    mode = value;
+    function setMode(value) {
+      mode = value;
+    }
+
+    let type, bytesPerElement;
+
+    function setIndex(value) {
+      type = value.type;
+      bytesPerElement = value.bytesPerElement;
+    }
+
+    function render(start, count) {
+      gl.drawElements(mode, count, type, start * bytesPerElement);
+
+      info.update(count, mode, 1);
+    }
+
+    function renderInstances(start, count, primcount) {
+      if (primcount === 0) return;
+
+      gl.drawElementsInstanced(mode, count, type, start * bytesPerElement, primcount);
+
+      info.update(count, mode, primcount);
+    }
+
+    //
+
+    this.setMode = setMode;
+    this.setIndex = setIndex;
+    this.render = render;
+    this.renderInstances = renderInstances;
   }
-
-  let type, bytesPerElement;
-
-  function setIndex(value) {
-    type = value.type;
-    bytesPerElement = value.bytesPerElement;
-  }
-
-  function render(start, count) {
-    gl.drawElements(mode, count, type, start * bytesPerElement);
-
-    info.update(count, mode, 1);
-  }
-
-  function renderInstances(start, count, primcount) {
-    if (primcount === 0) return;
-
-    gl.drawElementsInstanced(mode, count, type, start * bytesPerElement, primcount);
-
-    info.update(count, mode, primcount);
-  }
-
-  //
-
-  this.setMode = setMode;
-  this.setIndex = setIndex;
-  this.render = render;
-  this.renderInstances = renderInstances;
 }
 
 export { WebGLIndexedBufferRenderer };
