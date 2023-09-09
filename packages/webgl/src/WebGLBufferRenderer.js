f269feb12105246f1bb15bda7ae88c1ee03be592
Ed Preston <epreston@prestonsoft.com>
2023-09-10 07:44:08 +1000

    Update WebGLBufferRenderer.js
    cleanup

--- packages/webgl/src/WebGLBufferRenderer.js
+++ packages/webgl/src/WebGLBufferRenderer.js
@@ -16,25 +16,6 @@ function WebGLBufferRenderer(gl, extensions, info, capabilities) {
   function renderInstances(start, count, primcount) {
     if (primcount === 0) return;
 
-    // let extension, methodName;
-
-    // if (isWebGL2) {
-    // extension = gl;
-    // methodName = 'drawArraysInstanced';
-    // } else {
-    //   extension = extensions.get('ANGLE_instanced_arrays');
-    //   methodName = 'drawArraysInstancedANGLE';
-
-    //   if (extension === null) {
-    //     console.error(
-    //       'WebGLBufferRenderer: using InstancedBufferGeometry but hardware does not support extension ANGLE_instanced_arrays.'
-    //     );
-    //     return;
-    //   }
-    // }
-
-    // extension[methodName](mode, start, count, primcount);
-
     gl.drawArraysInstanced(mode, start, count, primcount);
 
     info.update(count, mode, primcount);
