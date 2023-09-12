0dd59ccdfe76c7b47d8631bda5b309d2335d9a6d
Ed Preston <epreston@prestonsoft.com>
2023-09-13 05:18:16 +1000

    Update vite.config.js
    run validation on source for instant feedback

--- apps/validation/vite.config.js
+++ apps/validation/vite.config.js
@@ -1,8 +1,13 @@
+import { entries as renderLayerEntries } from '../../scripts/aliases.js';
+
 // https://vitejs.dev/config/
 export default {
-  // server: {
-  //   open: '/index.html'
-  // },
+  resolve: {
+    // run validation on source for instant feedback
+    alias: {
+      ...renderLayerEntries
+    }
+  },
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.hdr'],
   build: {
     target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
