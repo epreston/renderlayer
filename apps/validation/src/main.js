5cf131625e066ca007193f00fe6ab5710d33dad6
Ed Preston <epreston@prestonsoft.com>
2023-09-14 01:23:42 +1000

    validation 0.0.2
    test lighting models

--- apps/validation/src/main.js
+++ apps/validation/src/main.js
@@ -2,19 +2,45 @@
 
 // will return url, see vite.config.js and this app's global.d.ts
 import gblFile from './assets/spriggen-opt-compress.glb';
+import hdrFile from './skybox/paul_lobe_haus_2k.hdr';
+
+import frontz from './images/sunset0-front+z.png';
+import backz from './images/sunset1-back-z.png';
+import leftx from './images/sunset2-left+x.png';
+import rightx from './images/sunset3-right-x.png';
+import upy from './images/sunset4-up+y.png';
+import downy from './images/sunset5-down-y.png';
 
 import * as RL from 'renderlayer';
 
 import { DRACOLoader } from '@renderlayer/draco';
 import { GLTFLoader } from '@renderlayer/gltf';
+import { RGBELoader } from '@renderlayer/extras';
 
-const decoderPath = 'https://www.gstatic.com/draco/versioned/decoders/1.5.6/';
+// hdr
+
+const rgbeLoader = new RGBELoader();
+const hdrTexture = rgbeLoader.load(hdrFile, function (texture) {
+  texture.mapping = RL.EquirectangularReflectionMapping;
+
+  // scene.background = texture;
+  // scene.environment = texture;
+});
+
+// cubemap environment
+
+const cubeLoader = new RL.CubeTextureLoader();
+const cubeTexture = cubeLoader.load([leftx, rightx, upy, downy, frontz, backz]);
 
 // scene
 
 const scene = new RL.Scene();
-const light = new RL.AmbientLight(0xffffff, 1 * Math.PI);
+const light = new RL.AmbientLight(0xffffff, 1.0 * Math.PI);
+light.visible = false;
 scene.add(light);
+
+// scene.background = cubeTexture;
+scene.environment = hdrTexture;
 
 // load
 
@@ -40,7 +66,9 @@ const onLoad = (gltf) => {
 const onProgress = (status) => console.log((status.loaded / status.total) * 100 + '% loaded');
 const onError = (error) => console.log(error);
 
-// glTF with DRACO support
+// load glTF with DRACO support
+
+const decoderPath = 'https://www.gstatic.com/draco/versioned/decoders/1.5.6/';
 
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath(decoderPath);
@@ -58,6 +86,7 @@ camera.position.set(0, 1.5, 2);
 // renderer
 
 const renderer = new RL.WebGLRenderer({ antialias: true });
+
 renderer.setClearColor(0x3d3d3d);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animation);
@@ -82,6 +111,7 @@ function animation(time) {
 
   const timeElapsed = (time - previousRAF) * 0.001;
 
+  controls.update(); // not strictly required
   mixer.update(timeElapsed);
   renderer.render(scene, camera);
 
@@ -95,4 +125,46 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// keyboard
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case ' ':
+      mixer.stopAllAction();
+      break;
+
+    case 'c':
+      scene.environment = cubeTexture;
+      break;
+
+    case 'h':
+      scene.environment = hdrTexture;
+      break;
+
+    case 'l':
+      light.visible = light.visible ? false : true;
+      break;
+
+    case 'e':
+      scene.environment = scene.environment ? null : hdrTexture;
+      break;
+
+    case 'b':
+      scene.background = scene.background ? null : scene.environment;
+      break;
+
+    case 'f':
+      scene.backgroundBlurriness = scene.backgroundBlurriness > 0 ? 0 : 0.1;
+      break;
+
+    case 'r':
+      camera.position.set(0, 1.5, 2);
+      controls.target.set(0, 1, 0);
+      break;
+
+    default:
+      break;
+  }
 });
