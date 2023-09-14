c474486cb97f9a07b1ca2bc082b3f44e7a06a948
Ed Preston <epreston@prestonsoft.com>
2023-09-14 21:05:11 +1000

    Update meta.js
    update meta command

--- scripts/meta.js
+++ scripts/meta.js
@@ -93,7 +93,7 @@ async function check(target) {
   console.log(target);
 
   await build({
-    tsconfig: path.resolve('jsconfig.json'),
+    tsconfig: path.resolve('tsconfig.json'),
     minify: false,
     target: 'es2022',
 
