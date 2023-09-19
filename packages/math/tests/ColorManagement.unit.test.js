71c883c347ceb959bd82ae88b74f25ac1d213fa5
Ed Preston <epreston@prestonsoft.com>
2023-09-19 17:10:24 +1000

    Update ColorManagement.unit.test.js
    add unit tests

--- packages/math/tests/ColorManagement.unit.test.js
+++ packages/math/tests/ColorManagement.unit.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, it, test, vi } from 'vitest';
 
-import { ColorManagement } from '../src/ColorManagement.js';
+import { ColorManagement, SRGBToLinear, LinearToSRGB } from '../src/ColorManagement.js';
 
 describe('Maths', () => {
   describe('ColorManagement', () => {
@@ -28,12 +28,24 @@ describe('Maths', () => {
       // implement
     });
 
-    test.todo('SRGBToLinear', () => {
-      // implement
+    test.each([
+      [0.0, 0.0],
+      [0.25, 0.05087609],
+      [0.5, 0.21404114],
+      [0.75, 0.52252155],
+      [1.0, 1.0]
+    ])('SRGBToLinear(%i) -> %i', (srgb, linear) => {
+      expect(SRGBToLinear(srgb)).toBeCloseTo(linear, 8);
     });
 
-    test.todo('LinearToSRGB', () => {
-      // implement
+    test.each([
+      [0.0, 0.0],
+      [0.05087609, 0.25000606],
+      [0.21404114, 0.5000057],
+      [0.52252155, 0.75000348],
+      [1.0, 1.0]
+    ])('LinearToSRGB(%i) -> %i', (linear, srgb) => {
+      expect(LinearToSRGB(linear)).toBeCloseTo(srgb, 8);
     });
   });
 });
