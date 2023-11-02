b244f8712e610cbe4d91b373197abd2b9e7d5ab9
Ed Preston <epreston@prestonsoft.com>
2023-11-03 04:37:24 +1100

    cleanup vitest config
    simplify vitest config for clarity

--- vitest.config.js
+++ vitest.config.js
@@ -1,37 +1,44 @@
 // vitest.config.js
 
+// import types for vitest config autocomplete
 /// <reference types="vitest" />
 
-import { mergeConfig } from 'vite';
 import { configDefaults, defineConfig } from 'vitest/config';
+import { entries } from './scripts/aliases.js';
 
-import viteConfig from './vite.config.js';
-
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      // globals: true,
-      // disable threads on GH actions to speed it up
-      threads: !process.env.GITHUB_ACTIONS,
-      setupFiles: ['./scripts/vitest-setup.js'],
-      // includeSource: ['src/**/*.{js,ts}'], // enable inline tests
-      // environmentOptions: {
-      //   jsdom: {
-      //     resources: 'usable'
-      //   }
-      // },
-      coverage: {
-        provider: 'v8',
-        reporter: ['text-summary', 'html'],
-        exclude: [
-          ...configDefaults.coverage.exclude,
-          // mostly entries that skew coverage reports
-          'scripts/mocks',
-          'packages/shaders/src/ShaderChunk/**',
-          'packages/shaders/src/ShaderLib/**'
-        ]
-      }
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    __DEV__: true,
+    __TEST__: true,
+    __VERSION__: '"test"',
+    __BROWSER__: false,
+    __GLOBAL__: false,
+    __ESM_BUNDLER__: true,
+    __ESM_BROWSER__: false,
+    __NODE_JS__: true
+  },
+  resolve: {
+    alias: entries
+  },
+  test: {
+    // globals: true,
+    // disable threads on GH actions to speed it up
+    threads: !process.env.GITHUB_ACTIONS,
+    setupFiles: ['./scripts/vitest-setup.js'],
+    sequence: {
+      hooks: 'list'
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text-summary', 'html'],
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        // mostly entries that skew coverage reports
+        'scripts/mocks',
+        'packages/shaders/src/ShaderChunk/**',
+        'packages/shaders/src/ShaderLib/**'
+      ]
     }
-  })
-);
+  }
+});
