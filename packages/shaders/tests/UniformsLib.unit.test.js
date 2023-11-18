fcb7b126482ff6a205bb1898de9d5ff3385eeb8e
Ed Preston <epreston@prestonsoft.com>
2023-11-18 16:36:04 +1100

    Update UniformsLib.unit.test.js
    cleanup

--- packages/shaders/tests/UniformsLib.unit.test.js
+++ packages/shaders/tests/UniformsLib.unit.test.js
@@ -5,7 +5,7 @@ import { UniformsLib } from '../src/UniformsLib.js';
 describe('Shaders', () => {
   describe('UniformsLib', () => {
     test('constructor', () => {
-      expect(UniformsLib, 'UniformsLib is defined.').toBeTruthy();
+      expect(UniformsLib).toBeDefined();
     });
   });
 });
