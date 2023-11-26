9f3818c9c6d3ea7f1eedbba7bcd31e9371b123e7
Ed Preston <epreston@prestonsoft.com>
2023-11-27 00:14:54 +1100

    Update LinearInterpolant.unit.test.js
    add unit test

--- packages/interpolants/tests/LinearInterpolant.unit.test.js
+++ packages/interpolants/tests/LinearInterpolant.unit.test.js
@@ -3,6 +3,15 @@ import { describe, expect, it, test, vi } from 'vitest';
 import { Interpolant } from '../src/Interpolant.js';
 import { LinearInterpolant } from '../src/LinearInterpolant.js';
 
+function createLinearInterpolant() {
+  const positions = new Float32Array([1, 11, 2, 22, 3, 33]);
+  const values = new Float32Array([1, 11, 2, 22, 3, 33]);
+  const size = 1;
+  const result = new Float32Array(1);
+
+  return new LinearInterpolant(positions, values, size, result);
+}
+
 describe('Interpolants', () => {
   describe('LinearInterpolant', () => {
     test('constructor', () => {
@@ -16,11 +25,15 @@ describe('Interpolants', () => {
       expect(object).toBeInstanceOf(Interpolant);
     });
 
-    test.todo('interpolate_', () => {
+    test('interpolate_', () => {
       // private
-      // interpolate_( i1, t0, t, t1 )
+      const object = createLinearInterpolant();
+      const evalResult = object.evaluate(0.5);
+      expect(evalResult.length).toBe(1);
+      expect(evalResult[0]).toBe(1);
+
       // return equal to base class Interpolant.resultBuffer after call
-      // implement
+      expect(evalResult).toEqual(object.resultBuffer);
     });
   });
 });
