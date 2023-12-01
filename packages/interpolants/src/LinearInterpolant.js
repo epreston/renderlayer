029da69af30fc2898fada2f982ba0d389918dd26
Ed Preston <epreston@prestonsoft.com>
2023-12-01 14:58:54 +1100

    Update LinearInterpolant.js
    cleanup

--- packages/interpolants/src/LinearInterpolant.js
+++ packages/interpolants/src/LinearInterpolant.js
@@ -6,13 +6,13 @@ class LinearInterpolant extends Interpolant {
   }
 
   interpolate_(i1, t0, t, t1) {
-    const result = this.resultBuffer,
-      values = this.sampleValues,
-      stride = this.valueSize,
-      offset1 = i1 * stride,
-      offset0 = offset1 - stride,
-      weight1 = (t - t0) / (t1 - t0),
-      weight0 = 1 - weight1;
+    const result = this.resultBuffer;
+    const values = this.sampleValues;
+    const stride = this.valueSize;
+    const offset1 = i1 * stride;
+    const offset0 = offset1 - stride;
+    const weight1 = (t - t0) / (t1 - t0);
+    const weight0 = 1 - weight1;
 
     for (let i = 0; i !== stride; ++i) {
       result[i] = values[offset0 + i] * weight0 + values[offset1 + i] * weight1;
