40980f85954f7acc393787eafd3183af0d7f421a
Ed Preston <epreston@prestonsoft.com>
2023-12-03 13:14:43 +1100

    Update WebGLBufferRenderer.js
    refactor

--- packages/webgl/src/WebGLBufferRenderer.js
+++ packages/webgl/src/WebGLBufferRenderer.js
@@ -1,31 +1,25 @@
 class WebGLBufferRenderer {
-  /** @param { WebGL2RenderingContext} gl */
+  /** @param {WebGL2RenderingContext} gl */
   constructor(gl, extensions, info, capabilities) {
-    let mode;
+    this._gl = gl;
+    this._info = info;
 
-    function setMode(value) {
-      mode = value;
-    }
+    this._mode = null;
+  }
 
-    function render(start, count) {
-      gl.drawArrays(mode, start, count);
+  setMode(value) {
+    this._mode = value;
+  }
 
-      info.update(count, mode, 1);
-    }
+  render(start, count) {
+    this._gl.drawArrays(this._mode, start, count);
+    this._info.update(count, this._mode, 1);
+  }
 
-    function renderInstances(start, count, primcount) {
-      if (primcount === 0) return;
-
-      gl.drawArraysInstanced(mode, start, count, primcount);
-
-      info.update(count, mode, primcount);
-    }
-
-    //
-
-    this.setMode = setMode;
-    this.render = render;
-    this.renderInstances = renderInstances;
+  renderInstances(start, count, primcount) {
+    if (primcount === 0) return;
+    this._gl.drawArraysInstanced(this._mode, start, count, primcount);
+    this._info.update(count, this._mode, primcount);
   }
 }
 
