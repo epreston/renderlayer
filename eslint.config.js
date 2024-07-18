d0a7f8b2640076ea7791c24886f1a4d2a0864333
Ed Preston <epreston@prestonsoft.com>
2024-07-19 04:59:00 +1000

    Update eslint.config.js
    not using pettier plugin for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,4 @@
 import sharedConfig from '@epreston/eslint-config';
 
 /** @type { import('eslint').Linter.FlatConfig[] } */
-export default [
-  ...sharedConfig,
-  {
-    name: 'project/rules',
-    rules: {
-      'prettier/prettier': 'off'
-    }
-  }
-];
+export default [...sharedConfig];
