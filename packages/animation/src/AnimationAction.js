5cfdf139bc42786e5a0a29ca8768185ec0faccf9
Ed Preston <epreston@prestonsoft.com>
2026-01-19 17:00:30 +1100

    Update AnimationAction.js
    class fields

--- packages/animation/src/AnimationAction.js
+++ packages/animation/src/AnimationAction.js
@@ -10,6 +10,49 @@ import {
 } from '@renderlayer/shared';
 
 class AnimationAction {
+  _mixer;
+  _clip;
+  _localRoot = null;
+  blendMode;
+
+  _interpolantSettings;
+  _interpolants; // bound by the mixer
+
+  _propertyBindings; // inside: PropertyMixer (managed by the mixer)
+
+  _cacheIndex = null; // for the memory manager
+  _byClipCacheIndex = null; // for the memory manager
+
+  _timeScaleInterpolant = null;
+  _weightInterpolant = null;
+
+  loop = LoopRepeat;
+  _loopCount = -1;
+
+  // global mixer time when the action is to be started
+  // it's set back to 'null' upon start of the action
+  _startTime = null;
+
+  // scaled local time of the action
+  // gets clamped or wrapped to 0..clip.duration according to loop
+  time = 0;
+
+  timeScale = 1;
+  _effectiveTimeScale = 1;
+
+  weight = 1;
+  _effectiveWeight = 1;
+
+  repetitions = Infinity; // no. of repetitions when looping
+
+  paused = false; // true -> zero effective time scale
+  enabled = true; // false -> zero effective weight
+
+  clampWhenFinished = false; // keep feeding the last frame?
+
+  zeroSlopeAtStart = true; // for smooth interpolation w/o separate
+  zeroSlopeAtEnd = true; // clips for start, loop and end
+
   constructor(mixer, clip, localRoot = null, blendMode = clip.blendMode) {
     this._mixer = mixer;
     this._clip = clip;
@@ -32,44 +75,10 @@ class AnimationAction {
     }
 
     this._interpolantSettings = interpolantSettings;
-
     this._interpolants = interpolants; // bound by the mixer
 
     // inside: PropertyMixer (managed by the mixer)
     this._propertyBindings = new Array(nTracks);
-
-    this._cacheIndex = null; // for the memory manager
-    this._byClipCacheIndex = null; // for the memory manager
-
-    this._timeScaleInterpolant = null;
-    this._weightInterpolant = null;
-
-    this.loop = LoopRepeat;
-    this._loopCount = -1;
-
-    // global mixer time when the action is to be started
-    // it's set back to 'null' upon start of the action
-    this._startTime = null;
-
-    // scaled local time of the action
-    // gets clamped or wrapped to 0..clip.duration according to loop
-    this.time = 0;
-
-    this.timeScale = 1;
-    this._effectiveTimeScale = 1;
-
-    this.weight = 1;
-    this._effectiveWeight = 1;
-
-    this.repetitions = Infinity; // no. of repetitions when looping
-
-    this.paused = false; // true -> zero effective time scale
-    this.enabled = true; // false -> zero effective weight
-
-    this.clampWhenFinished = false; // keep feeding the last frame?
-
-    this.zeroSlopeAtStart = true; // for smooth interpolation w/o separate
-    this.zeroSlopeAtEnd = true; // clips for start, loop and end
   }
 
   // State & Scheduling
