48d537e5804788b63959df206df27930b89d8bc8
Ed Preston <epreston@prestonsoft.com>
2026-01-20 07:18:14 +1100

    Update StringKeyframeTrack.js
    class fields

--- packages/keyframes/src/StringKeyframeTrack.js
+++ packages/keyframes/src/StringKeyframeTrack.js
@@ -4,12 +4,12 @@ import { KeyframeTrack } from './KeyframeTrack.js';
 /**
  * A Track that interpolates Strings
  */
-class StringKeyframeTrack extends KeyframeTrack {}
-
-StringKeyframeTrack.prototype.ValueTypeName = 'string';
-StringKeyframeTrack.prototype.ValueBufferType = Array;
-StringKeyframeTrack.prototype.DefaultInterpolation = InterpolateDiscrete;
-StringKeyframeTrack.prototype.InterpolantFactoryMethodLinear = undefined;
-StringKeyframeTrack.prototype.InterpolantFactoryMethodSmooth = undefined;
+class StringKeyframeTrack extends KeyframeTrack {
+  ValueTypeName = 'string';
+  ValueBufferType = Array;
+  DefaultInterpolation = InterpolateDiscrete;
+  InterpolantFactoryMethodLinear = undefined;
+  InterpolantFactoryMethodSmooth = undefined;
+}
 
 export { StringKeyframeTrack };
