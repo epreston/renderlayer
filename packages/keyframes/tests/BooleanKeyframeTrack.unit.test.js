93d2ede85c1427cfdc5c434d07c81810153cc35d
Ed Preston <epreston@prestonsoft.com>
2026-01-20 07:41:24 +1100

    Update BooleanKeyframeTrack.unit.test.js
    add unit tests

--- packages/keyframes/tests/BooleanKeyframeTrack.unit.test.js
+++ packages/keyframes/tests/BooleanKeyframeTrack.unit.test.js
@@ -1,5 +1,6 @@
 import { describe, expect, it, test, vi } from 'vitest';
 
+import { InterpolateDiscrete } from '@renderlayer/shared';
 import { KeyframeTrack } from '../src/KeyframeTrack.js';
 import { BooleanKeyframeTrack } from '../src/BooleanKeyframeTrack.js';
 
@@ -9,7 +10,7 @@ describe('Keyframes', () => {
       name: '.visible',
       times: [0, 1],
       values: [true, false],
-      interpolation: BooleanKeyframeTrack.DefaultInterpolation
+      interpolation: InterpolateDiscrete
     };
 
     test('constructor', () => {
@@ -30,6 +31,32 @@ describe('Keyframes', () => {
     test('extends', () => {
       const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
       expect(object).toBeInstanceOf(KeyframeTrack);
+      expect(object.name).toBe(parameters.name);
+    });
+
+    test('ValueTypeName', () => {
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object.ValueTypeName).toBe('bool');
+    });
+
+    test('ValueBufferType', () => {
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object.ValueBufferType).toBe(Array);
+    });
+
+    test('DefaultInterpolation', () => {
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object.DefaultInterpolation).toBe(InterpolateDiscrete);
+    });
+
+    test('InterpolantFactoryMethodLinear', () => {
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object.InterpolantFactoryMethodLinear).toBeUndefined();
+    });
+
+    test('InterpolantFactoryMethodSmooth', () => {
+      const object = new BooleanKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      expect(object.InterpolantFactoryMethodSmooth).toBeUndefined();
     });
   });
 });
