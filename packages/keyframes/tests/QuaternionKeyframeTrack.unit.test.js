403832f3baa0c939a5cd4cb11468b473fc1937c9
Ed Preston <epreston@prestonsoft.com>
2026-01-20 14:37:51 +1100

    Update QuaternionKeyframeTrack.unit.test.js
    cleanup

--- packages/keyframes/tests/QuaternionKeyframeTrack.unit.test.js
+++ packages/keyframes/tests/QuaternionKeyframeTrack.unit.test.js
@@ -6,7 +6,7 @@ import { QuaternionKeyframeTrack } from '../src/QuaternionKeyframeTrack.js';
 
 describe('Keyframes', () => {
   describe('QuaternionKeyframeTrack', () => {
-    const parameters = {
+    const _parameters = {
       name: '.rotation',
       times: [0],
       values: [0.5, 0.5, 0.5, 1],
@@ -16,55 +16,55 @@ describe('Keyframes', () => {
     test('constructor', () => {
       // name, times, values
       const object = new QuaternionKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
       );
       expect(object).toBeDefined();
 
       // name, times, values, interpolation
       const object_all = new QuaternionKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values,
-        parameters.interpolation
+        _parameters.name,
+        _parameters.times,
+        _parameters.values,
+        _parameters.interpolation
       );
       expect(object_all).toBeDefined();
     });
 
     test('extends', () => {
       const object = new QuaternionKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
       );
       expect(object).toBeInstanceOf(KeyframeTrack);
-      expect(object.name).toBe(parameters.name);
+      expect(object.name).toBe(_parameters.name);
     });
 
     test('ValueTypeName', () => {
       const object = new QuaternionKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
       );
       expect(object.ValueTypeName).toBe('quaternion');
     });
 
     test('InterpolantFactoryMethodLinear', () => {
       const object = new QuaternionKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
       );
       expect(object.InterpolantFactoryMethodLinear).toBeDefined();
     });
 
     test('InterpolantFactoryMethodSmooth', () => {
       const object = new QuaternionKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
       );
       expect(object.InterpolantFactoryMethodSmooth).toBeUndefined();
     });
