bd8ec0b329b224dd6d0f7e7d1eafeaca8f448729
Ed Preston <epreston@prestonsoft.com>
2026-01-20 14:37:43 +1100

    Update NumberKeyframeTrack.unit.test.js
    cleanup

--- packages/keyframes/tests/NumberKeyframeTrack.unit.test.js
+++ packages/keyframes/tests/NumberKeyframeTrack.unit.test.js
@@ -6,7 +6,7 @@ import { NumberKeyframeTrack } from '../src/NumberKeyframeTrack.js';
 
 describe('Keyframes', () => {
   describe('NumberKeyframeTrack', () => {
-    const parameters = {
+    const _parameters = {
       name: '.material.opacity',
       times: [0, 1],
       values: [0, 0.5],
@@ -15,27 +15,39 @@ describe('Keyframes', () => {
 
     test('constructor', () => {
       // name, times, values
-      const object = new NumberKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new NumberKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object).toBeDefined();
 
       // name, times, values, interpolation
       const object_all = new NumberKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values,
-        parameters.interpolation
+        _parameters.name,
+        _parameters.times,
+        _parameters.values,
+        _parameters.interpolation
       );
       expect(object_all).toBeDefined();
     });
 
     test('extends', () => {
-      const object = new NumberKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new NumberKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object).toBeInstanceOf(KeyframeTrack);
-      expect(object.name).toBe(parameters.name);
+      expect(object.name).toBe(_parameters.name);
     });
 
     test('ValueTypeName', () => {
-      const object = new NumberKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new NumberKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.ValueTypeName).toBe('number');
     });
   });
