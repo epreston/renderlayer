27f50f57d2f152cef1eae52ab6051ff09c8d5bb6
Ed Preston <epreston@prestonsoft.com>
2026-01-20 14:37:35 +1100

    Update ColorKeyframeTrack.unit.test.js
    cleanup

--- packages/keyframes/tests/ColorKeyframeTrack.unit.test.js
+++ packages/keyframes/tests/ColorKeyframeTrack.unit.test.js
@@ -6,7 +6,7 @@ import { ColorKeyframeTrack } from '../src/ColorKeyframeTrack.js';
 
 describe('Keyframes', () => {
   describe('ColorKeyframeTrack', () => {
-    const parameters = {
+    const _parameters = {
       name: '.material.diffuse',
       times: [0, 1],
       values: [0, 0.5, 1.0],
@@ -15,27 +15,39 @@ describe('Keyframes', () => {
 
     test('constructor', () => {
       // name, times, values
-      const object = new ColorKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new ColorKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object).toBeDefined();
 
       // name, times, values, interpolation
       const object_all = new ColorKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values,
-        parameters.interpolation
+        _parameters.name,
+        _parameters.times,
+        _parameters.values,
+        _parameters.interpolation
       );
       expect(object_all).toBeDefined();
     });
 
     test('extends', () => {
-      const object = new ColorKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new ColorKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object).toBeInstanceOf(KeyframeTrack);
-      expect(object.name).toBe(parameters.name);
+      expect(object.name).toBe(_parameters.name);
     });
 
     test('ValueTypeName', () => {
-      const object = new ColorKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new ColorKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.ValueTypeName).toBe('color');
     });
   });
