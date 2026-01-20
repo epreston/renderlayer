58af4320a993dbaec9edb0e15d56137224fa57a3
Ed Preston <epreston@prestonsoft.com>
2026-01-20 14:37:55 +1100

    Update StringKeyframeTrack.unit.test.js
    cleanup

--- packages/keyframes/tests/StringKeyframeTrack.unit.test.js
+++ packages/keyframes/tests/StringKeyframeTrack.unit.test.js
@@ -6,7 +6,7 @@ import { StringKeyframeTrack } from '../src/StringKeyframeTrack.js';
 
 describe('Keyframes', () => {
   describe('StringKeyframeTrack', () => {
-    const parameters = {
+    const _parameters = {
       name: '.name',
       times: [0, 1],
       values: ['foo', 'bar'],
@@ -15,47 +15,75 @@ describe('Keyframes', () => {
 
     test('constructor', () => {
       // name, times, values
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object).toBeDefined();
 
       // name, times, values, interpolation
       const object_all = new StringKeyframeTrack(
-        parameters.name,
-        parameters.times,
-        parameters.values,
-        parameters.interpolation
+        _parameters.name,
+        _parameters.times,
+        _parameters.values,
+        _parameters.interpolation
       );
       expect(object_all).toBeDefined();
     });
 
     test('extends', () => {
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object).toBeInstanceOf(KeyframeTrack);
-      expect(object.name).toBe(parameters.name);
+      expect(object.name).toBe(_parameters.name);
     });
 
     test('ValueTypeName', () => {
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.ValueTypeName).toBe('string');
     });
 
     test('ValueBufferType', () => {
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.ValueBufferType).toBe(Array);
     });
 
     test('DefaultInterpolation', () => {
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.DefaultInterpolation).toBe(InterpolateDiscrete);
     });
 
     test('InterpolantFactoryMethodLinear', () => {
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.InterpolantFactoryMethodLinear).toBeUndefined();
     });
 
     test('InterpolantFactoryMethodSmooth', () => {
-      const object = new StringKeyframeTrack(parameters.name, parameters.times, parameters.values);
+      const object = new StringKeyframeTrack(
+        _parameters.name,
+        _parameters.times,
+        _parameters.values
+      );
       expect(object.InterpolantFactoryMethodSmooth).toBeUndefined();
     });
   });
