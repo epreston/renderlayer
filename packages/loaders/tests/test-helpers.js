2d60d1f9b804039c799039b1f16ca44d9137ae71
Ed Preston <epreston@prestonsoft.com>
2026-01-20 14:56:00 +1100

    Update test-helpers.js
    cleanup

--- packages/loaders/tests/test-helpers.js
+++ packages/loaders/tests/test-helpers.js
@@ -3,7 +3,9 @@ export function resolveAfter(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// @ts-ignore - ensure code works in browser and node
 const scheduler = typeof setImmediate === 'function' ? setImmediate : setTimeout;
+
 // Credit to: https://github.com/kentor/flush-promises
 export function flushPromises() {
   return new Promise(function (resolve) {
