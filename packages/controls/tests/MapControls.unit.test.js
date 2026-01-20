6f9e73394980051e1eb796800fbfdaf4f251d398
Ed Preston <epreston@prestonsoft.com>
2026-01-20 14:07:01 +1100

    Update MapControls.unit.test.js
    cleanup

--- packages/controls/tests/MapControls.unit.test.js
+++ packages/controls/tests/MapControls.unit.test.js
@@ -10,23 +10,23 @@ import { MapControls } from '../src/MapControls.js';
 
 describe('Controls', () => {
   describe('MapControls', () => {
-    const mockDomElement = {
+    const _mockDomElement = {
       style: vi.fn(),
       addEventListener: vi.fn(),
       removeEventListener: vi.fn()
     };
 
     // prettier-ignore
-    const near = 1, far = 3, aspect = 16 / 9, fov = 50;
-    const camera = new PerspectiveCamera(fov, aspect, near, far);
+    const _near = 1, far = 3, aspect = 16 / 9, fov = 50;
+    const _camera = new PerspectiveCamera(fov, aspect, _near, far);
 
     test('constructor', () => {
-      const object = new MapControls(camera, mockDomElement);
+      const object = new MapControls(_camera, _mockDomElement);
       expect(object).toBeDefined();
     });
 
     test('extends', () => {
-      const object = new MapControls(camera, mockDomElement);
+      const object = new MapControls(_camera, _mockDomElement);
       expect(object).toBeInstanceOf(OrbitControls);
       expect(object).toBeInstanceOf(EventDispatcher);
     });
