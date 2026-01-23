b5df67b0b08755db66de46c5305b080f125fa537
Ed Preston <epreston@prestonsoft.com>
2026-01-24 00:42:15 +1100

    Update OrbitControls.unit.test.js
    add unit tests

--- packages/controls/tests/OrbitControls.unit.test.js
+++ packages/controls/tests/OrbitControls.unit.test.js
@@ -16,8 +16,8 @@ describe('Controls', () => {
     };
 
     // prettier-ignore
-    const _near = 1, far = 3, aspect = 16 / 9, fov = 60;
-    const _camera = new PerspectiveCamera(fov, aspect, _near, far);
+    const _near = 1, _far = 3, _aspect = 16 / 9, _fov = 60;
+    const _camera = new PerspectiveCamera(_fov, _aspect, _near, _far);
 
     test('constructor', () => {
       const object = new OrbitControls(_camera, _mockDomElement);
@@ -27,6 +27,146 @@ describe('Controls', () => {
     test('extends', () => {
       const object = new OrbitControls(_camera, _mockDomElement);
       expect(object).toBeInstanceOf(EventDispatcher);
+    });
+
+    test('object', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.object).toBeDefined();
+    });
+
+    test('domElement', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.domElement).toBeDefined();
+    });
+
+    test('enabled', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.enabled).toBeTruthy();
+    });
+
+    test('target', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.target).toBeDefined();
+    });
+
+    test('minDistance', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.minDistance).toBe(0);
+    });
+
+    test('maxDistance', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.maxDistance).toBe(Infinity);
+    });
+
+    test('minZoom', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.minZoom).toBe(0);
+    });
+
+    test('maxZoom', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.maxZoom).toBe(Infinity);
+    });
+
+    test('minPolarAngle', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.minPolarAngle).toBe(0);
+    });
+
+    test('maxPolarAngle', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.maxPolarAngle).toBe(Math.PI);
+    });
+
+    test('minAzimuthAngle', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.minAzimuthAngle).toBe(-Infinity);
+    });
+
+    test('maxAzimuthAngle', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.maxAzimuthAngle).toBe(Infinity);
+    });
+
+    test('enableDamping', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.enableDamping).toBeFalsy();
+    });
+
+    test('dampingFactor', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.dampingFactor).toBeCloseTo(0.05);
+    });
+
+    test('enableZoom', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.enableZoom).toBeTruthy();
+    });
+
+    test('zoomSpeed', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.zoomSpeed).toBeCloseTo(1.0);
+    });
+
+    test('enableRotate', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.enableRotate).toBeTruthy();
+    });
+
+    test('rotateSpeed', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.rotateSpeed).toBeCloseTo(1.0);
+    });
+
+    test('enablePan', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.enablePan).toBeTruthy();
+    });
+
+    test('panSpeed', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.panSpeed).toBeCloseTo(1.0);
+    });
+
+    test('screenSpacePanning', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.screenSpacePanning).toBeTruthy();
+    });
+
+    test('keyPanSpeed', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.keyPanSpeed).toBeCloseTo(7.0);
+    });
+
+    test('zoomToCursor', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.zoomToCursor).toBeFalsy();
+    });
+
+    test('autoRotate', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.autoRotate).toBeFalsy();
+    });
+
+    test('autoRotateSpeed', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.autoRotateSpeed).toBeCloseTo(2.0);
+    });
+
+    test('keys', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.keys).toBeDefined();
+    });
+
+    test('mouseButtons', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.mouseButtons).toBeDefined();
+    });
+
+    test('touches', () => {
+      const object = new OrbitControls(_camera, _mockDomElement);
+      expect(object.touches).toBeDefined();
     });
   });
 });
