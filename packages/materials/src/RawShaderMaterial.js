c1506998e0eea0ada6c443a409c29511f77cb57b
Ed Preston <epreston@prestonsoft.com>
2026-01-26 16:34:03 +1100

    Update RawShaderMaterial.js
    class fields

--- packages/materials/src/RawShaderMaterial.js
+++ packages/materials/src/RawShaderMaterial.js
@@ -1,10 +1,10 @@
 import { ShaderMaterial } from './ShaderMaterial.js';
 
 class RawShaderMaterial extends ShaderMaterial {
+  type = 'RawShaderMaterial';
+
   constructor(parameters) {
     super(parameters);
-
-    this.type = 'RawShaderMaterial';
   }
 
   get isRawShaderMaterial() {
