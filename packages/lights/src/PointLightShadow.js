73af5adebfb24870f2f6c3c6b279d6f30d617f6d
Ed Preston <epreston@prestonsoft.com>
2026-01-26 15:27:41 +1100

    Update PointLightShadow.js
    class fields

--- packages/lights/src/PointLightShadow.js
+++ packages/lights/src/PointLightShadow.js
@@ -2,63 +2,58 @@ import { PerspectiveCamera } from '@renderlayer/cameras';
 import { Matrix4, Vector2, Vector3, Vector4 } from '@renderlayer/math';
 import { LightShadow } from './LightShadow.js';
 
-const _projScreenMatrix = /*@__PURE__*/ new Matrix4();
-const _lightPositionWorld = /*@__PURE__*/ new Vector3();
-const _lookTarget = /*@__PURE__*/ new Vector3();
-
 class PointLightShadow extends LightShadow {
+  _frameExtents = new Vector2(4, 2);
+  _viewportCount = 6;
+
+  _viewports = [
+    // These viewports map a cube-map onto a 2D texture with the
+    // following orientation:
+    //
+    //  xzXZ
+    //   y Y
+    //
+    // X - Positive x direction
+    // x - Negative x direction
+    // Y - Positive y direction
+    // y - Negative y direction
+    // Z - Positive z direction
+    // z - Negative z direction
+
+    // positive X
+    new Vector4(2, 1, 1, 1),
+    // negative X
+    new Vector4(0, 1, 1, 1),
+    // positive Z
+    new Vector4(3, 1, 1, 1),
+    // negative Z
+    new Vector4(1, 1, 1, 1),
+    // positive Y
+    new Vector4(3, 0, 1, 1),
+    // negative Y
+    new Vector4(1, 0, 1, 1)
+  ];
+
+  _cubeDirections = [
+    new Vector3(1, 0, 0),
+    new Vector3(-1, 0, 0),
+    new Vector3(0, 0, 1),
+    new Vector3(0, 0, -1),
+    new Vector3(0, 1, 0),
+    new Vector3(0, -1, 0)
+  ];
+
+  _cubeUps = [
+    new Vector3(0, 1, 0),
+    new Vector3(0, 1, 0),
+    new Vector3(0, 1, 0),
+    new Vector3(0, 1, 0),
+    new Vector3(0, 0, 1),
+    new Vector3(0, 0, -1)
+  ];
+
   constructor() {
     super(new PerspectiveCamera(90, 1, 0.5, 500));
-
-    this._frameExtents = new Vector2(4, 2);
-
-    this._viewportCount = 6;
-
-    this._viewports = [
-      // These viewports map a cube-map onto a 2D texture with the
-      // following orientation:
-      //
-      //  xzXZ
-      //   y Y
-      //
-      // X - Positive x direction
-      // x - Negative x direction
-      // Y - Positive y direction
-      // y - Negative y direction
-      // Z - Positive z direction
-      // z - Negative z direction
-
-      // positive X
-      new Vector4(2, 1, 1, 1),
-      // negative X
-      new Vector4(0, 1, 1, 1),
-      // positive Z
-      new Vector4(3, 1, 1, 1),
-      // negative Z
-      new Vector4(1, 1, 1, 1),
-      // positive Y
-      new Vector4(3, 0, 1, 1),
-      // negative Y
-      new Vector4(1, 0, 1, 1)
-    ];
-
-    this._cubeDirections = [
-      new Vector3(1, 0, 0),
-      new Vector3(-1, 0, 0),
-      new Vector3(0, 0, 1),
-      new Vector3(0, 0, -1),
-      new Vector3(0, 1, 0),
-      new Vector3(0, -1, 0)
-    ];
-
-    this._cubeUps = [
-      new Vector3(0, 1, 0),
-      new Vector3(0, 1, 0),
-      new Vector3(0, 1, 0),
-      new Vector3(0, 1, 0),
-      new Vector3(0, 0, 1),
-      new Vector3(0, 0, -1)
-    ];
   }
 
   get isPointLightShadow() {
@@ -95,5 +90,9 @@ class PointLightShadow extends LightShadow {
     this._frustum.setFromProjectionMatrix(_projScreenMatrix);
   }
 }
+
+const _projScreenMatrix = /*@__PURE__*/ new Matrix4();
+const _lightPositionWorld = /*@__PURE__*/ new Vector3();
+const _lookTarget = /*@__PURE__*/ new Vector3();
 
 export { PointLightShadow };
