2cdd89a0ee56517710f6fc95e5c413c5870320f7
Ed Preston <epreston@prestonsoft.com>
2026-01-27 16:11:55 +1100

    Update CatmullRomCurve3.js
    cleanup

--- packages/curves/src/CatmullRomCurve3.js
+++ packages/curves/src/CatmullRomCurve3.js
@@ -10,70 +10,6 @@ import { Curve } from './core/Curve.js';
  * curve.tension is used for Catmull-Rom which defaults to 0.5
  */
 
-/*
-Based on an optimized c++ solution in
- - http://stackoverflow.com/questions/9489736/catmull-rom-curve-with-no-cusps-and-no-self-intersections/
- - http://ideone.com/NoEbVM
-
-This CubicPoly class could be used for reusing some variables and calculations,
-but for curve use, it could be possible inlined and flatten into a single function call
-which can be placed in CurveUtils.
-*/
-
-// fixme: convert to class
-function CubicPoly() {
-  let c0 = 0;
-  let c1 = 0;
-  let c2 = 0;
-  let c3 = 0;
-
-  /*
-   * Compute coefficients for a cubic polynomial
-   *   p(s) = c0 + c1*s + c2*s^2 + c3*s^3
-   * such that
-   *   p(0) = x0, p(1) = x1
-   *  and
-   *   p'(0) = t0, p'(1) = t1.
-   */
-  function init(x0, x1, t0, t1) {
-    c0 = x0;
-    c1 = t0;
-    c2 = -3 * x0 + 3 * x1 - 2 * t0 - t1;
-    c3 = 2 * x0 - 2 * x1 + t0 + t1;
-  }
-
-  return {
-    initCatmullRom(x0, x1, x2, x3, tension) {
-      init(x1, x2, tension * (x2 - x0), tension * (x3 - x1));
-    },
-
-    initNonuniformCatmullRom(x0, x1, x2, x3, dt0, dt1, dt2) {
-      // compute tangents when parameterized in [t1,t2]
-      let t1 = (x1 - x0) / dt0 - (x2 - x0) / (dt0 + dt1) + (x2 - x1) / dt1;
-      let t2 = (x2 - x1) / dt1 - (x3 - x1) / (dt1 + dt2) + (x3 - x2) / dt2;
-
-      // rescale tangents for parametrization in [0,1]
-      t1 *= dt1;
-      t2 *= dt1;
-
-      init(x1, x2, t1, t2);
-    },
-
-    calc(t) {
-      const t2 = t * t;
-      const t3 = t2 * t;
-      return c0 + c1 * t + c2 * t2 + c3 * t3;
-    }
-  };
-}
-
-//
-
-const tmp = /*@__PURE__*/ new Vector3();
-const px = /*@__PURE__*/ new CubicPoly();
-const py = /*@__PURE__*/ new CubicPoly();
-const pz = /*@__PURE__*/ new CubicPoly();
-
 class CatmullRomCurve3 extends Curve {
   type = 'CatmullRomCurve3';
 
@@ -119,8 +55,8 @@ class CatmullRomCurve3 extends Curve {
       p0 = points[(intPoint - 1) % l];
     } else {
       // extrapolate first point
-      tmp.subVectors(points[0], points[1]).add(points[0]);
-      p0 = tmp;
+      _tmp.subVectors(points[0], points[1]).add(points[0]);
+      p0 = _tmp;
     }
 
     const p1 = points[intPoint % l];
@@ -130,8 +66,8 @@ class CatmullRomCurve3 extends Curve {
       p3 = points[(intPoint + 2) % l];
     } else {
       // extrapolate last point
-      tmp.subVectors(points[l - 1], points[l - 2]).add(points[l - 1]);
-      p3 = tmp;
+      _tmp.subVectors(points[l - 1], points[l - 2]).add(points[l - 1]);
+      p3 = _tmp;
     }
 
     if (this.curveType === 'centripetal' || this.curveType === 'chordal') {
@@ -146,16 +82,16 @@ class CatmullRomCurve3 extends Curve {
       if (dt0 < 1e-4) dt0 = dt1;
       if (dt2 < 1e-4) dt2 = dt1;
 
-      px.initNonuniformCatmullRom(p0.x, p1.x, p2.x, p3.x, dt0, dt1, dt2);
-      py.initNonuniformCatmullRom(p0.y, p1.y, p2.y, p3.y, dt0, dt1, dt2);
-      pz.initNonuniformCatmullRom(p0.z, p1.z, p2.z, p3.z, dt0, dt1, dt2);
+      _px.initNonuniformCatmullRom(p0.x, p1.x, p2.x, p3.x, dt0, dt1, dt2);
+      _py.initNonuniformCatmullRom(p0.y, p1.y, p2.y, p3.y, dt0, dt1, dt2);
+      _pz.initNonuniformCatmullRom(p0.z, p1.z, p2.z, p3.z, dt0, dt1, dt2);
     } else if (this.curveType === 'catmullrom') {
-      px.initCatmullRom(p0.x, p1.x, p2.x, p3.x, this.tension);
-      py.initCatmullRom(p0.y, p1.y, p2.y, p3.y, this.tension);
-      pz.initCatmullRom(p0.z, p1.z, p2.z, p3.z, this.tension);
+      _px.initCatmullRom(p0.x, p1.x, p2.x, p3.x, this.tension);
+      _py.initCatmullRom(p0.y, p1.y, p2.y, p3.y, this.tension);
+      _pz.initCatmullRom(p0.z, p1.z, p2.z, p3.z, this.tension);
     }
 
-    point.set(px.calc(weight), py.calc(weight), pz.calc(weight));
+    point.set(_px.calc(weight), _py.calc(weight), _pz.calc(weight));
 
     return point;
   }
@@ -212,5 +148,64 @@ class CatmullRomCurve3 extends Curve {
     return this;
   }
 }
+
+/*
+Based on an optimized c++ solution in
+ - http://stackoverflow.com/questions/9489736/catmull-rom-curve-with-no-cusps-and-no-self-intersections/
+ - http://ideone.com/NoEbVM
+
+This CubicPoly class could be used for reusing some variables and calculations,
+but for curve use, it could be possible inlined and flatten into a single function call
+which can be placed in CurveUtils.
+*/
+
+class CubicPoly {
+  #c0 = 0;
+  #c1 = 0;
+  #c2 = 0;
+  #c3 = 0;
+
+  /*
+   * Compute coefficients for a cubic polynomial
+   *   p(s) = c0 + c1*s + c2*s^2 + c3*s^3
+   * such that
+   *   p(0) = x0, p(1) = x1
+   * and
+   *   p'(0) = t0, p'(1) = t1.
+   */
+  #init(x0, x1, t0, t1) {
+    this.#c0 = x0;
+    this.#c1 = t0;
+    this.#c2 = -3 * x0 + 3 * x1 - 2 * t0 - t1;
+    this.#c3 = 2 * x0 - 2 * x1 + t0 + t1;
+  }
+
+  initCatmullRom(x0, x1, x2, x3, tension) {
+    this.#init(x1, x2, tension * (x2 - x0), tension * (x3 - x1));
+  }
+
+  initNonuniformCatmullRom(x0, x1, x2, x3, dt0, dt1, dt2) {
+    // compute tangents when parameterized in [t1,t2]
+    let t1 = (x1 - x0) / dt0 - (x2 - x0) / (dt0 + dt1) + (x2 - x1) / dt1;
+    let t2 = (x2 - x1) / dt1 - (x3 - x1) / (dt1 + dt2) + (x3 - x2) / dt2;
+
+    // rescale tangents for parametrization in [0,1]
+    t1 *= dt1;
+    t2 *= dt1;
+
+    this.#init(x1, x2, t1, t2);
+  }
+
+  calc(t) {
+    const t2 = t * t;
+    const t3 = t2 * t;
+    return this.#c0 + this.#c1 * t + this.#c2 * t2 + this.#c3 * t3;
+  }
+}
+
+const _tmp = /*@__PURE__*/ new Vector3();
+const _px = /*@__PURE__*/ new CubicPoly();
+const _py = /*@__PURE__*/ new CubicPoly();
+const _pz = /*@__PURE__*/ new CubicPoly();
 
 export { CatmullRomCurve3 };
