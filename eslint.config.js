e5d279feca7b286f9ffc0e306a0f17be1d361bd7
Ed Preston <epreston@prestonsoft.com>
2026-01-30 18:05:25 +1100

    Update eslint.config.js
    exclude lib folders from linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,17 @@
 import sharedConfig from '@epreston/eslint-config';
 
 /** @type { import('eslint').Linter.Config[] } */
-export default [...sharedConfig];
+export default [
+  ...sharedConfig,
+  {
+    // ignores must be completely separate from other rules
+    name: 'project/ignores',
+    ignores: ['**/packages/*/lib']
+  }
+  // {
+  //   name: 'project/rules',
+  //   rules: {
+  //     'no-console': 'off',
+  //   }
+  // }
+];
