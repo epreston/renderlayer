5f516ff62bce2497c7643673da17a3c11d9fdac9
Ed Preston <epreston@prestonsoft.com>
2026-02-01 20:04:25 +1100

    Update vite.config.js
    configure web server "appType" that runs during testing
    
    disabling history fallback will make sure a "404 not found" response is returned on an incorrect URL instead of the default page, "index.html".

--- apps/shaders/vite.config.js
+++ apps/shaders/vite.config.js
@@ -2,6 +2,7 @@ import { entries as renderLayerEntries } from '../../scripts/aliases.js';
 
 // https://vitejs.dev/config/
 export default {
+  appType: 'mpa', // disable history fallback
   resolve: {
     // run validation on source for instant feedback
     alias: {
