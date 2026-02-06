2323491ebd436317f19e89a7b5961f765d82bea3
Ed Preston <epreston@prestonsoft.com>
2026-02-07 00:36:13 +1100

    Update GLTFTextureAVIFExtension.js
    class fields

--- packages/gltf/src/extensions/GLTFTextureAVIFExtension.js
+++ packages/gltf/src/extensions/GLTFTextureAVIFExtension.js
@@ -10,10 +10,12 @@ import { EXTENSIONS } from './EXTENSIONS';
  * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Vendor/EXT_texture_avif
  */
 export class GLTFTextureAVIFExtension {
+  parser;
+  name = EXTENSIONS.EXT_TEXTURE_AVIF;
+
   /** @param {GLTFParser} parser  */
   constructor(parser) {
     this.parser = parser;
-    this.name = EXTENSIONS.EXT_TEXTURE_AVIF;
   }
 
   loadTexture(textureIndex) {
