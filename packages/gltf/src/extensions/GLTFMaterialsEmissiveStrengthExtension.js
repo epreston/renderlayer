3c51d8de0c10a1eab7666de9ef30a4e052de4fc8
Ed Preston <epreston@prestonsoft.com>
2026-02-07 00:47:41 +1100

    Update GLTFMaterialsEmissiveStrengthExtension.js
    class fields

--- packages/gltf/src/extensions/GLTFMaterialsEmissiveStrengthExtension.js
+++ packages/gltf/src/extensions/GLTFMaterialsEmissiveStrengthExtension.js
@@ -10,10 +10,12 @@ import { EXTENSIONS, getMaterialExtension } from './EXTENSIONS';
  * Specification: https://github.com/KhronosGroup/glTF/blob/5768b3ce0ef32bc39cdf1bef10b948586635ead3/extensions/2.0/Khronos/KHR_materials_emissive_strength/README.md
  */
 export class GLTFMaterialsEmissiveStrengthExtension {
+  parser;
+  name = EXTENSIONS.KHR_MATERIALS_EMISSIVE_STRENGTH;
+
   /** @param {GLTFParser} parser  */
   constructor(parser) {
     this.parser = parser;
-    this.name = EXTENSIONS.KHR_MATERIALS_EMISSIVE_STRENGTH;
   }
 
   extendMaterialParams(materialIndex, materialParams) {
