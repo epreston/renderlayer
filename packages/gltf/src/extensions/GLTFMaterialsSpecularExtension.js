11d7e4c92056d109267f8841c52fd912bd6fcfe0
Ed Preston <epreston@prestonsoft.com>
2026-02-07 00:38:56 +1100

    Update GLTFMaterialsSpecularExtension.js
    class fields

--- packages/gltf/src/extensions/GLTFMaterialsSpecularExtension.js
+++ packages/gltf/src/extensions/GLTFMaterialsSpecularExtension.js
@@ -14,10 +14,12 @@ import { EXTENSIONS, getMaterialExtension } from './EXTENSIONS';
  * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_specular
  */
 export class GLTFMaterialsSpecularExtension {
+  parser;
+  name = EXTENSIONS.KHR_MATERIALS_SPECULAR;
+
   /** @param {GLTFParser} parser  */
   constructor(parser) {
     this.parser = parser;
-    this.name = EXTENSIONS.KHR_MATERIALS_SPECULAR;
   }
 
   getMaterialType(materialIndex) {
