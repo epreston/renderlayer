1d786c151e48edb7ad57e2e1c44f56974070c413
Ed Preston <epreston@prestonsoft.com>
2026-02-07 00:36:33 +1100

    Update GLTFMeshoptCompression.js
    class fields

--- packages/gltf/src/extensions/GLTFMeshoptCompression.js
+++ packages/gltf/src/extensions/GLTFMeshoptCompression.js
@@ -10,6 +10,9 @@ import { EXTENSIONS } from './EXTENSIONS';
  * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Vendor/EXT_meshopt_compression
  */
 export class GLTFMeshoptCompression {
+  name;
+  parser;
+
   /** @param {GLTFParser} parser  */
   constructor(parser, name = EXTENSIONS.KHR_MESHOPT_COMPRESSION) {
     this.name = name;
