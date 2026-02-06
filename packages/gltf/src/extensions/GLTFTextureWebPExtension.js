3f4baa61b40c960d37d5cedf84ba0568a2540e56
Ed Preston <epreston@prestonsoft.com>
2026-02-07 00:28:44 +1100

    Update GLTFTextureWebPExtension.js
    class fields

--- packages/gltf/src/extensions/GLTFTextureWebPExtension.js
+++ packages/gltf/src/extensions/GLTFTextureWebPExtension.js
@@ -10,10 +10,12 @@ import { EXTENSIONS } from './EXTENSIONS';
  * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Vendor/EXT_texture_webp
  */
 export class GLTFTextureWebPExtension {
+  parser;
+  name = EXTENSIONS.EXT_TEXTURE_WEBP;
+
   /** @param {GLTFParser} parser  */
   constructor(parser) {
     this.parser = parser;
-    this.name = EXTENSIONS.EXT_TEXTURE_WEBP;
   }
 
   loadTexture(textureIndex) {
