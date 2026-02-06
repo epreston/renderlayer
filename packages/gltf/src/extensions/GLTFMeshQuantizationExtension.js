b289bd3a10d858f82162879384d4c884a259f971
Ed Preston <epreston@prestonsoft.com>
2026-02-07 00:36:23 +1100

    Update GLTFMeshQuantizationExtension.js
    class fields

--- packages/gltf/src/extensions/GLTFMeshQuantizationExtension.js
+++ packages/gltf/src/extensions/GLTFMeshQuantizationExtension.js
@@ -6,7 +6,5 @@ import { EXTENSIONS } from './EXTENSIONS';
  * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_mesh_quantization
  */
 export class GLTFMeshQuantizationExtension {
-  constructor() {
-    this.name = EXTENSIONS.KHR_MESH_QUANTIZATION;
-  }
+  name = EXTENSIONS.KHR_MESH_QUANTIZATION;
 }
