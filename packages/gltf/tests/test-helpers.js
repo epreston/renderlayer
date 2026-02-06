c6333ca4f3fb61be3ece88e2fabef8d28de400e7
Ed Preston <epreston@prestonsoft.com>
2026-02-06 21:57:29 +1100

    Update test-helpers.js
    rework

--- packages/gltf/tests/test-helpers.js
+++ packages/gltf/tests/test-helpers.js
@@ -1,12 +1,12 @@
+// @ts-ignore
+const scheduler = typeof setImmediate === 'function' ? setImmediate : setTimeout;
+
 export function resolveAfter(ms) {
   // introduces a delay
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => scheduler(resolve, ms));
 }
 
-const scheduler = typeof setImmediate === 'function' ? setImmediate : setTimeout;
 // Credit to: https://github.com/kentor/flush-promises
 export function flushPromises() {
-  return new Promise(function (resolve) {
-    scheduler(resolve, 0);
-  });
+  return resolveAfter(0);
 }
