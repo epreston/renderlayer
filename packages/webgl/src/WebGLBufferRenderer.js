4a0c4efff938c1d8f2a63c06593847b4c4a4f2a2
Ed Preston <epreston@prestonsoft.com>
2026-02-10 04:39:59 +1100

    Update WebGLBufferRenderer.js
    port multidraw

--- packages/webgl/src/WebGLBufferRenderer.js
+++ packages/webgl/src/WebGLBufferRenderer.js
@@ -5,6 +5,7 @@
 class WebGLBufferRenderer {
   #gl;
   #info;
+  #extensions;
 
   #mode = null;
 
@@ -16,6 +17,7 @@ class WebGLBufferRenderer {
   constructor(gl, extensions, info) {
     this.#gl = gl;
     this.#info = info;
+    this.#extensions = extensions;
   }
 
   setMode(value) {
@@ -31,6 +33,50 @@ class WebGLBufferRenderer {
     if (primcount === 0) return;
     this.#gl.drawArraysInstanced(this.#mode, start, count, primcount);
     this.#info.update(count, this.#mode, primcount);
+  }
+
+  renderMultiDraw(starts, counts, drawCount) {
+    if (drawCount === 0) return;
+
+    const extension = this.#extensions.get('WEBGL_multi_draw');
+    extension.multiDrawArraysWEBGL(this.#mode, starts, 0, counts, 0, drawCount);
+
+    let elementCount = 0;
+    for (let i = 0; i < drawCount; i++) {
+      elementCount += counts[i];
+    }
+
+    this.#info.update(elementCount, this.#mode, 1);
+  }
+
+  renderMultiDrawInstances(starts, counts, drawCount, primcount) {
+    if (drawCount === 0) return;
+
+    const extension = this.#extensions.get('WEBGL_multi_draw');
+
+    if (extension === null) {
+      for (let i = 0; i < starts.length; i++) {
+        this.renderInstances(starts[i], counts[i], primcount[i]);
+      }
+    } else {
+      extension.multiDrawArraysInstancedWEBGL(
+        this.#mode,
+        starts,
+        0,
+        counts,
+        0,
+        primcount,
+        0,
+        drawCount
+      );
+
+      let elementCount = 0;
+      for (let i = 0; i < drawCount; i++) {
+        elementCount += counts[i] * primcount[i];
+      }
+
+      this.#info.update(elementCount, this.#mode, 1);
+    }
   }
 }
 
